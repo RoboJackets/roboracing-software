a356350b322bfc4efe5d759c22331c9c6c10a2cc
Eric Huang <ehuang4@gmail.com>
2014-05-15 19:17:37 -0400

    Change msg DriveCommand to MotorCommand

--- iarrc_motor_relay/src/iarrc_motor_relay_node.cpp
+++ iarrc_motor_relay/src/iarrc_motor_relay_node.cpp
@@ -1,13 +1,13 @@
 #include <ros/ros.h>
 #include <ros/publisher.h>
-#include <iarrc_msgs/DriveCommand.h>
+#include <iarrc_msgs/MotorCommand.h>
 #include <boost/asio.hpp>
 #include <string>
 
-iarrc_msgs::DriveCommand cmd;
+iarrc_msgs::MotorCommand cmd;
 bool new_cmd = false;
 
-void DriveCommandCB(const iarrc_msgs::DriveCommand::ConstPtr& msg)
+void MotorCommandCB(const iarrc_msgs::MotorCommand::ConstPtr& msg)
 {
 	cmd = *msg;
 	new_cmd = true;
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
     // 
     std::string drive_command_topic;
     nhp.param(std::string("drive_command_topic"), drive_command_topic, std::string("/drive_command_topic"));
-    ros::Subscriber drive_command_sub = nh.subscribe(drive_command_topic, 1, DriveCommandCB);
+    ros::Subscriber drive_command_sub = nh.subscribe(drive_command_topic, 1, MotorCommandCB);
 
     ROS_INFO_STREAM("Drive Command topic = " << drive_command_topic);
 
@@ -40,24 +40,13 @@ int main(int argc, char** argv)
 
 		// if(new_cmd) {
 		if(true) {
-
-			std::stringstream ss;
-
-			ROS_INFO("Sending command: servo=%d, motor=%d", cmd.servo_position, cmd.motor_speed);
-
-			ss << (char)181 << (char)(cmd.motor_speed + 90) << (char)(cmd.servo_position + 90) << (char)182;
+			ROS_INFO("Sending command: servo=%d, motor=%d", cmd.angle, cmd.speed);
 
 			char m[4];
 			m[0] = 181;
-			m[1] = (char)(cmd.motor_speed + 90);
-			m[2] = (char)(cmd.servo_position + 90);
+			m[1] = (char)(cmd.speed + 90);
+			m[2] = (char)(cmd.angle + 90);
 			m[3] = 182;
-
-			std::string msg = ss.str();
-			for(int i=0; i < 4; i++) {
-				if(m[i] != msg[i])
-					ROS_INFO("WTF");
-			}
 
 			try {
 				boost::asio::write(serial, boost::asio::buffer(m, 4));
--- iarrc_tutorials/src/nodes/iarrc_joystick.cpp
+++ iarrc_tutorials/src/nodes/iarrc_joystick.cpp
@@ -1,64 +1,51 @@
 #include <ros/ros.h>
 #include <ros/publisher.h>
-#include <std_msgs/Float32.h>
-#include <string>
-#include <iarrc_tutorials/Empty.h>
 #include <sensor_msgs/Joy.h>
-#include <linux/joystick.h> // may not be necessary
-#include <iarrc_msgs/DriveCommand.h>
+#include <iarrc_msgs/MotorCommand.h>
+#include <string>
 
-// TODO:
-// + Access more realistic data structures with ROS headers and such
-// 
+ros::Publisher command_publisher; // Publishes the motor commands
+int angle_max;
+int speed_max;
 
-// Global variables
-ros::Publisher command_publisher;
-
-void FloatCommandCB(const sensor_msgs::Joy::ConstPtr& scan_in) {
-    // Always be careful!
-    if(!scan_in) {
-        ROS_ERROR("Received void input pointer in FloatCommandCB.");
+void JoystickCB(const sensor_msgs::Joy::ConstPtr& msg) {
+    if(!msg) {
+        ROS_ERROR("Received void input pointer in JoystickCB.");
+        return;
     }
-    //ROS_INFO("Axes 0: %f !", scan_in->axes[0]); // print axes[1]
-    //ROS_INFO("Button 1: %d !", scan_in->buttons[1]); // print button[1]
-    iarrc_msgs::DriveCommand StateVehicle; 
-    if ((scan_in->buttons[1])>0){ //if the x button is pushed then accelerate to the speed.
-        StateVehicle.motor_speed = 10;
-    }else{
-        StateVehicle.motor_speed = 0;
-    }
-    if((scan_in->axes[0])>0){ // if pressed left on d-pad
-        StateVehicle.servo_position = -10;
-    }else if ((scan_in->axes[0])<0){ //if pressed right on d-pad
-        StateVehicle.servo_position = 10;
-    }else{
-        StateVehicle.servo_position = 0;
-    }
-    ROS_INFO("motor_speed: %d !", StateVehicle.motor_speed); // print axes[1]
-    ROS_INFO("servo_position: %d !", StateVehicle.servo_position); // print button[1]
 
-    command_publisher.publish(StateVehicle);
+    iarrc_msgs::MotorCommand command;
+    command.angle = angle_max * msg->axes[0];
+    command.speed = speed_max * msg->axes[3];
+
+    // ROS_INFO_STREAM("Sending motor command:\n" << command);
+
+    command_publisher.publish(command);
 }
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "iarrc_laser_scan");
+    ros::init(argc, argv, "iarrc_joystick");
     ros::NodeHandle nh;
     ros::NodeHandle nhp("~");
-   
-    std::string scan_topic;    
 
-    nhp.param(std::string("joy"), scan_topic, std::string("/joy"));
-   
-    ros::Subscriber float_command_sub = nh.subscribe(scan_topic, 1, FloatCommandCB);
+    // Subscribe to joystick topic
+    std::string joystick_topic;
+    nhp.param(std::string("joystick_topic"), joystick_topic, std::string("/joy"));
+    ros::Subscriber float_command_sub = nh.subscribe(joystick_topic, 1, JoystickCB);
 
-    command_publisher = nh.advertise<iarrc_msgs::DriveCommand>("/iarrc/drive_command", 1);
-    
-    ROS_INFO_STREAM("IARRC scan_topic: " << scan_topic);
-   
-	ROS_INFO("IARRC tutorial node ready.");
+    // Convert joystick commands into motor commands on this topic
+    std::string motor_topic;
+    nhp.param(std::string("motor_topic"), joystick_topic, std::string("/iarrc/motor_command"));
+    command_publisher = nh.advertise<iarrc_msgs::MotorCommand>(motor_topic, 1);
+
+    // Driving limits
+    nhp.param(std::string("angle_max"), angle_max, 10);
+    nhp.param(std::string("speed_max"), speed_max, 10);
+
+	ROS_INFO("IARRC joystick node ready.");
 	ros::spin();
-	ROS_INFO("Shutting down IARRC tutorial node.");
+	ROS_INFO("Shutting down IARRC joystick node.");
 
     return 0;
 }
