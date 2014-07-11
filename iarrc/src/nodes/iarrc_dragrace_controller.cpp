a2355255055bb64c2b79a7437d7578c7d17871f0
Ben Nuttle <bennuttle@gmail.com>
2014-07-11 01:01:49 -0400

    working stoplight detection yay

--- iarrc/src/nodes/iarrc_dragrace_controller.cpp
+++ iarrc/src/nodes/iarrc_dragrace_controller.cpp
@@ -13,8 +13,8 @@
 
 ros::Publisher speed_publisher;
 
-ros::NodeHandle nh;
-ros::NodeHandle nhp("~");
+ros::NodeHandle *nh;
+ros::NodeHandle *nhp;
 
 int drive_speed;
 std::string wall_detect_topic;
@@ -30,21 +30,27 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "iarrc_dragrace_controller");
 
+	nh = new ros::NodeHandle();
+	nhp = new ros::NodeHandle("~");
+
 	std::string stoplight_topic;
-    	nhp.param(std::string("stoplight_topic"), stoplight_topic, std::string("/light_change"));
-    	stoplight_subscriber = nh.subscribe(stoplight_topic, 1, stoplightCB);
+    	nhp->param(std::string("stoplight_topic"), stoplight_topic, std::string("/light_change"));
+    	stoplight_subscriber = nh->subscribe(stoplight_topic, 1, stoplightCB);
 
-	nhp.param(std::string("wall_detect_topic"), wall_detect_topic, std::string("/wall_detect"));
+	nhp->param(std::string("wall_detect_topic"), wall_detect_topic, std::string("/wall_detect"));
 
-	nhp.param(std::string("drive_speed"), drive_speed, 8);
+	nhp->param(std::string("drive_speed"), drive_speed, 8);
 
 	std::string speed_topic;
-    	nhp.param(std::string("speed_topic"), speed_topic, std::string("/speed"));
-    	speed_publisher = nh.advertise<iarrc_msgs::iarrc_speed>(speed_topic, 1);
+    	nhp->param(std::string("speed_topic"), speed_topic, std::string("/speed"));
+    	speed_publisher = nh->advertise<iarrc_msgs::iarrc_speed>(speed_topic, 1);
 	racecar_set_speed(0);
 
 	ROS_INFO("IARRC dragrace_controller node ready.");
 	ros::spin();
+
+	delete nh;
+	delete nhp;
 	ROS_INFO("Shutting down IARRC dragrace_controller node.");
 
     	return 0;
@@ -52,13 +58,15 @@ int main(int argc, char** argv)
 
 void stoplightCB(std_msgs::Bool)
 {
+	ROS_INFO("Drag Race Controller got stoplight.");
 	racecar_set_speed(drive_speed);
 	stoplight_subscriber.shutdown();
-	wall_detect_subscriber = nh.subscribe(wall_detect_topic, 1, wallDetectCB);
+	wall_detect_subscriber = nh->subscribe(wall_detect_topic, 1, wallDetectCB);
 }
 
 void wallDetectCB(std_msgs::Bool)
 {
+	ROS_INFO("Drag Race Controller got wall.");
 	racecar_set_speed(0);
 	wall_detect_subscriber.shutdown();
 	exit(0);
