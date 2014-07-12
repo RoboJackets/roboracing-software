0c377a9c714b84de9f7c240cd2ab13bbc285d219
Matthew Barulic <matthew.barulic@gmail.com>
2014-07-11 21:09:16 -0400

    Imports constants and drag_race_steerer.

---
+++ iarrc/src/nodes/constants.hpp
@@ -0,0 +1,16 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace constants {
+
+// center-to-center in m
+double wheel_base = 0.26;
+
+// axel-to-axel in m
+double chassis_length = 0.325;
+
+double steering_degress_per_pwm = 1.5;
+
+}
+
+#endif // CONSTANTS_HPP
