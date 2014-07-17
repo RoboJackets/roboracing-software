d4aff01f2f4e6fc14754f9986f2fc0021c1b94e1
Claire Bergman <claire@Claire.(none)>
2014-07-16 22:21:38 -0400

    Re-made the stoplight detection code

--- iarrc/src/nodes/stoplight_watcher.cpp
+++ iarrc/src/nodes/stoplight_watcher.cpp
@@ -17,13 +17,22 @@ sensor_msgs::Image rosimage;
 int last_diff = 0;
 
 Mat lastFrame;
+bool red;
+
+int lowS = 100;
+int highS = 200;
+int lowV =150;
+int highV = 255;
+int lowH = 0;
+int highH = 40;
+
 
 // ROS image callback
 void ImageCB(const sensor_msgs::Image::ConstPtr& msg) { 
 	cv_bridge::CvImagePtr cv_ptr;
-	Mat blurImg;
-	Mat grayscaleImg;
 	Mat circlesImg;
+
+    int diff;
 
 	// Convert ROS to OpenCV
 	try {
@@ -32,74 +41,54 @@ void ImageCB(const sensor_msgs::Image::ConstPtr& msg) {
 		ROS_ERROR("CV-Bridge error: %s", e.what());
 		return;
 	}
+    int width = cv_ptr->image.cols;
+    int height = cv_ptr->image.rows;
 
-	GaussianBlur(cv_ptr->image,circlesImg,Size(3,3), 0 ,0);
-	
-	Point circCenter(497, 208);
-	int circRadius = 13;
-	
-	//circle(circlesImg, circCenter, circRadius, Scalar(255,255,0));
-	
-	/*vector<Mat> channels;
-	split(circlesImg, channels);
-	for(int i = 0; i < 3; i++)
-	{
-		Rect ROI(i*circlesImg.cols/3,0,circlesImg.cols/3,circlesImg.rows);
-		Size size(circlesImg.cols/3, circlesImg.rows);
-		resize(channels[i], channels[i](ROI), size);
-		ROI = Rect(((i+1)%3)*circlesImg.cols/3,0,circlesImg.cols/3,circlesImg.rows);
-		channels[i](ROI) = Mat::zeros(size, CV_8UC1);
-		ROI = Rect(((i+2)%3)*circlesImg.cols/3,0,circlesImg.cols/3,circlesImg.rows);
-		channels[i](ROI) = Mat::zeros(size, CV_8UC1);
-	}
-	merge(channels, circlesImg);*/
-	cvtColor(circlesImg, circlesImg, CV_BGR2GRAY);
-	
-	circlesImg(Rect(circlesImg.cols/3,0,circlesImg.cols/3,circlesImg.rows)) *= 0;
-	
-	if(!lastFrame.data)
-		circlesImg.copyTo(lastFrame);
-	
-	Mat diff = Mat::ones(lastFrame.rows, lastFrame.cols, CV_8UC1);
-	for(int r = 0; r < diff.rows; r++)
-	{
-		uchar* dRow = diff.ptr(r);
-		uchar* cRow = circlesImg.ptr(r);
-		uchar* lRow = lastFrame.ptr(r);
-		for(int c = 0; c < diff.cols; c++) {
-			dRow[c] = 127 + ( ( cRow[c] - lRow[c] ) / 2 );
-		}
-	}
-	
-	diff(Rect(0,diff.rows-30,diff.cols,30)) *= 0;
-	
-	for(int r = 0; r < diff.rows-30; r++)
-	{
-		uchar* row = diff.ptr(r);
-		uchar* row2 = diff.ptr(r+30);
-		for(int c = 0; c < diff.cols; c++) {
-			if(row[c] < 100 && row2[c] > 154) {
-				row[c] = 255;
-			} else {
-				row[c] = 0;
-			}
-		}
-	}
-	
-	lastFrame = circlesImg;
-	
-	int diffSum = sum(diff)[0]/255;
-	
-	if(diffSum > 10 && diffSum + last_diff > 185) {
-		ROS_INFO("Stoplight Change Detected");
-		std_msgs::Bool b;
-		b.data = true;
-		bool_pub.publish(b);
-	}
-	last_diff = diffSum;
+    Rect trafficRect(0,height*3/8, width, height/4);
+    cv_ptr->image(trafficRect).copyTo(circlesImg);
 
-	cv_ptr->image=diff;
-	cv_ptr->encoding="mono8";
+
+    cvtColor(circlesImg, circlesImg, CV_BGR2HSV);
+
+    Mat threshImg(height, width, circlesImg.type());
+
+    inRange(circlesImg, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), circlesImg);
+	
+  //  equalizeHist(circlesImg, circlesImg);
+
+    Mat finalImg = Mat::zeros(height, width, circlesImg.type());
+    circlesImg.copyTo(finalImg(trafficRect));
+
+    if (!lastFrame.data){
+        circlesImg.copyTo(lastFrame);
+        red = true;
+    }
+    else{
+        CvScalar cvs = sum(abs(lastFrame - circlesImg));
+        diff = cvs.val[0];
+
+
+        if (diff > 300 && !red){
+            ROS_INFO("Stoplight Change Detected");
+            std_msgs::Bool b;
+            b.data = true;
+            bool_pub.publish(b);
+            red = true;
+        }
+
+        if (diff < 500 && red){
+            red = false;
+            lowH = 30;
+            highH = 120;
+        }
+
+        lastFrame = circlesImg;
+    }
+
+   // cvtColor(finalImg, finalImg, CV_HSV2GRAY);
+
+    cv_ptr->image=finalImg;
+    cv_ptr->encoding="mono8";
 	cv_ptr->toImageMsg(rosimage);
 	img_pub.publish(rosimage);
 }
