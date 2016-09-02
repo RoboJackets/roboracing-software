5334e1395000102a2151f263696b61b392cdf2c4
Matthew Barulic <matthew.barulic@gmail.com>
2016-09-02 11:28:34 -0400

    Fixes message timing bug in videoconverter.

--- sandbox/videoconverter/videoconverter.cpp
+++ sandbox/videoconverter/videoconverter.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv) {
 	ros::init(argc, argv, "videoconverter");
 	ros::NodeHandle handle;
 
-	VideoCapture vid("/home/sastorer/IARRC/SparkFun AVC 2016 Course Preview - Ground Competition.mp4");
+	VideoCapture vid("/home/matt/Desktop/SparkFun AVC 2016 Course Preview - Ground Competition.mp4");
 	if(!vid.isOpened()) {
 		cout << "You done fucked up. Fix your video." << endl;
 		return 0;
@@ -19,29 +19,42 @@ int main(int argc, char** argv) {
 
 	Mat frame;
 	rosbag::Bag bag;
-	bag.open("bagvid.bag", rosbag::bagmode::Write);
+	bag.open("/media/matt/DATA/bagvid.bag", rosbag::bagmode::Write);
 
 	//namedWindow("window", WINDOW_NORMAL);
 	ros::Rate rate(30);
 
 	bag.setCompression(rosbag::compression::BZ2);
 
+  auto time = ros::Time::now();
+
+  auto frameDuration = ros::Duration(1.0/30.0);
+
+  auto frameCount = 0;
+
 	while(vid.grab()) {
 		vid.retrieve(frame);
 		cv_bridge::CvImage cvimage;
 		cvimage.image = frame;
 		cvimage.encoding = "bgr8";
-		cvimage.header.stamp = ros::Time::now();
+		cvimage.header.stamp = time;
 
 		sensor_msgs::Image rawimg;
 		cvimage.toImageMsg(rawimg);
 
-		bag.write("camera/image_raw", ros::Time::now(), rawimg);
+		bag.write("camera/image_raw", time, rawimg);
 
 		/*imshow("window", frame);
 		if(waitKey(1) == ' ') {
 			break;
 		}*/
+
+    time += frameDuration;
+    frameCount++;
+    
+    if(frameCount % 10 == 0) {
+      cout << "Frame: " << frameCount << "\n";
+    }
 
 		rate.sleep();
 	}
