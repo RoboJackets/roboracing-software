ec412cb56b95f7cb217b23782f3926aae71efa59
sastorer <sstorer3@gatech.edu>
2016-09-06 03:49:50 -0400

    Iterate through the rows and columns and perform comparison operation. Added mask todo.

--- avc/src/color_detector_avc.cpp
+++ avc/src/color_detector_avc.cpp
@@ -10,6 +10,8 @@ using namespace std;
 using namespace cv;
 using namespace ros;
 
+using uchar = unsigned char;
+
 //img size: 480 x 640
 
 Publisher img_pub;
@@ -19,7 +21,26 @@ Mat mask;
 Mat detectGrey(const Mat& image){
 	Mat frame;
 	image.copyTo(frame);
-	return frame;
+
+	Mat output(image.rows, image.cols, CV_8UC3);
+
+		for(int r = 0; r < frame.rows; r++){
+			uchar* row = frame.ptr<uchar>(r);
+			uchar* out_row = output.ptr<uchar>(r);
+			for(int c = 0; c < frame.cols * frame.channels(); c += frame.channels()){
+				uchar& B = row[c];
+				uchar& G = row[c + 1];
+				uchar& R = row[c + 2];
+
+				if(B == G && G == R){
+					out_row[c] = out_row[c + 1] = out_row[c + 2] = 0;
+				} else {
+					out_row[c] = out_row[c + 1] = out_row[c + 2] = 255;
+				}
+			}
+	}
+
+	return output;
 } 
 
 void ImageCB(const sensor_msgs::ImageConstPtr& msg) {
@@ -35,7 +56,7 @@ void ImageCB(const sensor_msgs::ImageConstPtr& msg) {
 		return;
 	}
 	
-	//applying 
+	//applying mask and detectGrey() function to image
 	frame = cv_ptr->image;
     frame = frame.mul(mask);
     Mat grey_img = detectGrey(frame);
@@ -56,6 +77,26 @@ int main(int argc, char** argv){
 	init(argc, argv, "color_detector_avc");
 
 	NodeHandle nh;
+
+	/* TODO: Make this code not process the top half of the image
+			 using masks and things.
+
+	vector<Mat> mask_hsegments = {
+	    Mat(40,205,CV_8UC3, CV_RGB(1,1,1)),
+	    Mat::zeros(40,230,CV_8UC3),
+	    Mat(40,205,CV_8UC3, CV_RGB(1,1,1))
+	};
+
+	hconcat(mask_hsegments, mask_horizontal);
+	vector<Mat> mask_segments = {
+	    Mat::zeros(200,640,CV_8UC3),
+	    Mat(200,640,CV_8UC3, CV_RGB(1,1,1)),
+	    mask_horizontal,
+	    Mat::zeros(40,640,CV_8UC3)
+	};
+
+	vconcat(mask_segments, mask);
+	*/
 
 	Subscriber img_saver_sub = nh.subscribe("/ps3_eye/image_raw", 1, ImageCB);
 	
