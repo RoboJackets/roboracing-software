1d6cee623345abe67cd5ebe5be7adbd728f5338e
sastorer <sstorer3@gatech.edu>
2016-09-16 23:41:48 -0400

    Detecting curbs with color

--- avc/src/color_detector_avc.cpp
+++ avc/src/color_detector_avc.cpp
@@ -16,6 +16,8 @@ using uchar = unsigned char;
 Publisher img_pub;
 Mat mask;
 
+/* DETECTS ROAD
+
 Mat detectGrey(const Mat& image){
 	Mat frame;
 	image.copyTo(frame);
@@ -38,14 +40,42 @@ Mat detectGrey(const Mat& image){
 			}
 	}
 	return output;
-} 
+}
+*/
+
+//DETECTS CURBS
+Mat detectCurb(const Mat& image){
+	Mat frame;
+	image.copyTo(frame);
+
+	Mat curb(image.rows, image.cols, CV_8UC3);
+
+		for(int r = 0; r < frame.rows; r++){
+			uchar* row = frame.ptr<uchar>(r);
+			uchar* curb_row = curb.ptr<uchar>(r);
+			for(int c = 0; c < frame.cols * frame.channels(); c += frame.channels()){
+				uchar B = row[c];
+				uchar G = row[c + 1];
+				uchar R = row[c + 2];
+
+				if(((B < 100 && B > 90) && (G < 100 && G > 90) && (R < 100 && R > 90))
+					|| ((B < 215 && B > 200) && (G < 215 && G > 200) && (R < 215 && R > 200))){
+					curb_row[c] = curb_row[c + 1] = curb_row[c + 2] = 0;
+				} else {
+					curb_row[c] = curb_row[c + 1] = curb_row[c + 2] = 255;
+				}
+			}
+		}
+
+	return curb;
+}
 
 void ImageCB(const sensor_msgs::ImageConstPtr& msg) {
 
     cv_bridge::CvImagePtr cv_ptr;
 	Mat frame;
 	Mat output;
-	
+
 	try {
 		cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
 	} catch (cv_bridge::Exception& e) {
@@ -56,7 +86,7 @@ void ImageCB(const sensor_msgs::ImageConstPtr& msg) {
 	//applying mask and detectGrey() function to image
 	frame = cv_ptr->image;
     frame = frame.mul(mask);
-    Mat grey_img = detectGrey(frame);
+    Mat grey_img = detectCurb(frame);
     output = grey_img;
 
     sensor_msgs::Image outmsg;
@@ -88,9 +118,9 @@ int main(int argc, char** argv){
 	vconcat(mask_segments, mask);
 
 	Subscriber img_saver_sub = nh.subscribe("/camera/image_raw", 1, ImageCB);
-	
+
 	img_pub = nh.advertise<sensor_msgs::Image>(string("/colors_img"), 1);
-        
+
     spin();
 
 	return 0;
