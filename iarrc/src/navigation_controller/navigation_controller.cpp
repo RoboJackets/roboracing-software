e234176c45fb0cc02fe4b9629fe8e941b06884cd
Evan Bretl <evanbretl4@gmail.com>
2017-02-19 18:49:21 -0500

    fixes errors in navigation_controller

--- iarrc/src/navigation_controller/navigation_controller.cpp
+++ iarrc/src/navigation_controller/navigation_controller.cpp
@@ -2,10 +2,12 @@
 #include <rr_platform/speed.h>
 #include <rr_platform/steering.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Int8.h>
 
-constexpr WAITING_FOR_START = 0;
-constexpr RUNNING_PLANNER = 1;
-constexpr FINISHED = 2;
+const int WAITING_FOR_START = 0;
+const int RUNNING_PLANNER = 1;
+const int FINISHED = 2;
+int REQ_FINISH_LINE_CROSSES;
 
 ros::Publisher steerPub;
 ros::Publisher speedPub;
@@ -14,7 +16,7 @@ int state;
 double planSpeed, speed;
 double planSteering, steering;
 bool raceStarted;
-bool raceEnded;
+int finishLineCrosses;
 
 void updateState() {
     switch(state) {
@@ -28,7 +30,7 @@ void updateState() {
         case RUNNING_PLANNER:
             speed = planSpeed;
             steering = planSteering;
-            if(raceEnded) {
+            if(finishLineCrosses >= REQ_FINISH_LINE_CROSSES) {
                 state = FINISHED;
             }
             break;
@@ -54,8 +56,8 @@ void startLightCB(const std_msgs::Bool::ConstPtr &bool_msg) {
     raceStarted = bool_msg->data;
 }
 
-void finishLineCB(const std_msgs::Bool::ConstPtr &bool_msg) {
-    raceEnded = bool_msg->data;
+void finishLineCB(const std_msgs::Int8::ConstPtr &int_msg) {
+    finishLineCrosses = int_msg->data;
 }
 
 int main(int argc, char** argv) {
@@ -63,9 +65,12 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle nh;
 
+    nh.getParam("req_finish_line_crosses", REQ_FINISH_LINE_CROSSES);
+
     auto planSpeedSub = nh.subscribe("plan/speed", 1, planSpeedCB);
     auto planSteerSub = nh.subscribe("plan/steering", 1, planSteerCB);
     auto startLightSub = nh.subscribe("green_light", 1, startLightCB);
+    auto finishLineSub = nh.subscribe("finish_line_crosses", 1, finishLineCB);
 
     speedPub = nh.advertise<rr_platform::speed>("speed", 1);
     steerPub = nh.advertise<rr_platform::steering>("steering", 1);
@@ -73,9 +78,11 @@ int main(int argc, char** argv) {
     state = WAITING_FOR_START;
     planSpeed = 0.0;
     planSteering = 0.0;
+    finishLineCrosses = 0;
 
     ros::Rate rate(30.0);
     while(ros::ok()) {
+        ros::spinOnce();
         updateState();
 
         rr_platform::speed speedMsg;
@@ -84,7 +91,7 @@ int main(int argc, char** argv) {
 
         rr_platform::steering steerMsg;
         steerMsg.angle = steering;
-        steerMsg.publish(steerMsg);
+        steerPub.publish(steerMsg);
 
         rate.sleep();
     }
