19a1d619082341b136981f382f072783a4f57cb0
Matthew Barulic <matthew.barulic@gmail.com>
2017-03-12 19:20:27 -0400

    Adds LED test code to MPU9250 node, and add script to automatically enable Tuchuck board LEDs at startup. #62

--- rr_platform/src/mpu9250_for_joule/main.cpp
+++ rr_platform/src/mpu9250_for_joule/main.cpp
@@ -11,15 +11,25 @@ int main(int argc, char **argv) {
 
     ros::init(argc, argv,  "mpu9250_for_joule");
 
-    mraa::I2c imu{0};
+    ros::NodeHandle handle;
 
-    imu.address(MPU9250_ADDRESS);
+//    mraa::I2c imu{0};
+//
+//    imu.address(MPU9250_ADDRESS);
+//
+//    auto whoAmIVal = imu.readReg(MPU9250_WHO_AM_I_REGISTER);
+//
+//    if(whoAmIVal != MPU9250_WHO_AM_I_VALUE) {
+//        ROS_ERROR_STREAM("Recieved unexpected value (" << whoAmIVal << ") from IMU's \"Who Am I\" register.");
+//        ros::shutdown();
+//        return 1;
+//    }
 
-    auto whoAmIVal = imu.readReg(MPU9250_WHO_AM_I_REGISTER);
+    mraa::Gpio led(100);
 
-    if(whoAmIVal != MPU9250_WHO_AM_I_VALUE) {
-        ROS_ERROR_STREAM("Recieved unexpected value (" << whoAmIVal << ") from IMU's \"Who Am I\" register.");
-        ros::shutdown();
+    auto response = led.dir(mraa::DIR_OUT);
+    if(response != mraa::SUCCESS) {
+        ROS_ERROR_STREAM(response);
         return 1;
     }
 
@@ -27,7 +37,10 @@ int main(int argc, char **argv) {
 
     while(ros::ok()) {
 
-
+        led.write(1);
+        sleep(1);
+        led.write(0);
+        sleep(1);
 
         rate.sleep();
     }
