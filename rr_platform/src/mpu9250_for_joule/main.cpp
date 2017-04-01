977d403a92f33d89a399c03436bf51f1613ed65b
Matthew Barulic <matthew.barulic@gmail.com>
2017-04-01 15:48:12 -0400

    Implements first stage of communicating with MPU9250 via mraa. Successfully reading sensor data that seams reasonable. A lot of work left to clean it up and pull data into ROS. Issue #62

--- rr_platform/src/mpu9250_for_joule/main.cpp
+++ rr_platform/src/mpu9250_for_joule/main.cpp
@@ -1,49 +1,95 @@
+#include <signal.h>
 #include <ros/ros.h>
 #include <mraa.hpp>
+#include <iostream>
+#include "mpu9250.h"
 
 using namespace std;
 
 constexpr uint8_t MPU9250_ADDRESS = 0b1101000;
 constexpr uint8_t MPU9250_WHO_AM_I_REGISTER = 117;
 constexpr uint8_t MPU9250_WHO_AM_I_VALUE = 0x71;
+constexpr uint8_t MPU9250_ACCEL_XOUT_H_REG = 59;
+constexpr uint8_t MPU9250_ACCEL_XOUT_L_REG = 60;
+
+volatile bool running = true;
+
+void signal_handler(int) {
+    running = false;
+}
+
+void set_SIGINT_handler() {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = signal_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+}
 
 int main(int argc, char **argv) {
 
-    ros::init(argc, argv,  "mpu9250_for_joule");
+    set_SIGINT_handler();
 
-    ros::NodeHandle handle;
+//    ros::init(argc, argv,  "mpu9250_for_joule");
 
-//    mraa::I2c imu{0};
-//
-//    imu.address(MPU9250_ADDRESS);
-//
-//    auto whoAmIVal = imu.readReg(MPU9250_WHO_AM_I_REGISTER);
-//
-//    if(whoAmIVal != MPU9250_WHO_AM_I_VALUE) {
-//        ROS_ERROR_STREAM("Recieved unexpected value (" << whoAmIVal << ") from IMU's \"Who Am I\" register.");
-//        ros::shutdown();
-//        return 1;
-//    }
+//    ros::NodeHandle handle;
 
-    mraa::Gpio led(100);
+/*
+ * TODO look at https://communities.intel.com/thread/110551
+ */
 
-    auto response = led.dir(mraa::DIR_OUT);
-    if(response != mraa::SUCCESS) {
-        ROS_ERROR_STREAM(response);
+    MPU9250 imu;
+    
+    if(!imu.whoAmIPassed()) {
+        cerr << "Failed to read Who-Am-I value from IMU." << endl;
         return 1;
     }
+    
+    array<float, 6> selfTestResults;
+    imu.runSelfTest(selfTestResults);
+    
+    if(any_of(selfTestResults.begin(), selfTestResults.end(),
+           [](const auto &e){ return e > 14.0; })) {
+        cerr << "WARNING: SelfTest results outside of acceptable range." << endl;
+        for(const auto &val : selfTestResults) {
+            cerr << val << ", ";
+        }
+        cerr << endl;
+    }
+    
+    imu.calibrate();
 
-    ros::Rate rate{10}; // TODO set this higher if possible
+    imu.initialize();
 
-    while(ros::ok()) {
+    imu.initializeMagnetometer();
 
-        led.write(1);
-        sleep(1);
-        led.write(0);
-        sleep(1);
+    imu.getAres();
+    imu.getGres();
+    imu.getMres();
 
-        rate.sleep();
+    array<int16_t, 3> accel_data;
+    array<int16_t, 3> gyro_data;
+    array<int16_t, 3> mag_data;
+
+    array<float, 3> accel_real;
+    array<float, 3> gyro_real;
+    array<float, 3> mag_real;
+
+    while(running) {
+        imu.readAccelData(accel_data);
+        imu.readGyroData(gyro_data);
+
+        transform(accel_data.begin(), accel_data.end(), accel_real.begin(),
+                  [&imu](const auto &e){ return e * imu.aRes;});
+
+        transform(gyro_data.begin(), gyro_data.end(), gyro_real.begin(),
+                  [&imu](const auto &e){ return e * imu.gRes;});
+
+        cout << accel_real[0] << " " << accel_real[1] << " " << accel_real[2] << "\t" << gyro_real[0] << " " << gyro_real[1] << " " << gyro_real[2] << endl;
+
+        usleep(500'000);
     }
 
     return 0;
 }
+
