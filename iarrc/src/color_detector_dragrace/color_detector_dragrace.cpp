365b6232c4afe90ff72c803ebf3b26b84e723ccc
Matthew Barulic <matthew.barulic@gmail.com>
2017-06-03 01:45:39 -0400

    Begins reducing computational load of IARRC vision code. Takes drag race color detector from 30% cpu usage down to 7%. Still need to polish up the work and do the same for the circuit race color detector.

--- iarrc/src/color_detector_dragrace/color_detector_dragrace.cpp
+++ iarrc/src/color_detector_dragrace/color_detector_dragrace.cpp
@@ -12,9 +12,9 @@ using uchar = unsigned char;
 
 Publisher img_pub;
 
-// image size: 480 x 640
+Rect mask;
 
-Mat mask;
+Mat erosion_kernel;
 
 Mat findWhiteLines(const Mat& image) {
     Mat frame;
@@ -136,39 +136,72 @@ Mat findBlueLines(const Mat& image) {
     return output;
 }
 
+inline bool is_blue(const uchar &H, const uchar &S) {
+    return ( abs(H - 108) < 5 ) && ( S > 50 );
+}
+
+inline bool is_orange(const uchar &H, const uchar &S, const uchar &V) {
+    return ( abs(H - 15) < 5 ) && ( S > 70 ) && ( V > 40 );
+}
+
+inline bool is_yellow(const uchar &H, const uchar &S, const uchar &V) {
+    return false;
+    // return ( abs(H - 30) < 0 ) && ( S > 50 ) && ( V > 50 );
+    // No yellow lines to be detected in drag race (<0 never true)
+}
+
+inline bool is_white(const uchar &H, const uchar &S, const uchar &V) {
+    return false; 
+    // return blue > 220 && green > 220 && red > 220;
+    // TODO convert to HSV
+}
+
 void ImageCB(const sensor_msgs::ImageConstPtr& msg) {
 
-    cv_bridge::CvImagePtr cv_ptr;
-	Mat frame;
-	Mat output;
+    cv_bridge::CvImageConstPtr cv_ptr;
 	
 	try {
-		cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+		cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
 	} catch (cv_bridge::Exception& e) {
 		ROS_ERROR("CV-Bridge error: %s", e.what());
 		return;
 	}
 	
-	frame = cv_ptr->image;
+	const Mat &frame = cv_ptr->image;
+	
+	Mat output = Mat::zeros(frame.rows, frame.cols, CV_8UC3);
+	
+	const Mat &frame_masked = frame(mask);
+	
+	Mat output_masked = output(mask);
+	
+	for(int r = 0; r < frame_masked.rows; r++) {
+	    const uchar *row = frame_masked.ptr<uchar>(r);
+	    uchar *out_row = output_masked.ptr<uchar>(r);
+	    for(int c = 0; c < frame_masked.cols * frame_masked.channels(); c += frame_masked.channels()) {
+	        const uchar &H = row[c];
+	        const uchar &S = row[c+1];
+	        const uchar &V = row[c+2];
+	        
+	        if(is_orange(H,S,V)) {
+	            out_row[c] = 0;
+                out_row[c+1] = 127;
+                out_row[c+2] = 255;
+	        } else if(is_yellow(H,S,V)) {
+	            out_row[c] = 0;
+                out_row[c+1] = out_row[c+2] = 255;
+	        } else if(is_blue(H,S)) {
+	            out_row[c] = 255;
+	            out_row[c+1] = out_row[c+2] = 0;
+	        } if(is_white(H,S,V)) {
+	            out_row[c] = out_row[c+1] = out_row[c+2] = 255;
+	        }
+	    }
+	}
 
-    frame = frame.mul(mask);
+    //erode(output_masked, output_masked, erosion_kernel);
 
-    Mat white_lines = findWhiteLines(frame);
-
-    Mat blue_lines = findBlueLines(frame);
-
-    Mat yellow_lines = findYellowLines(frame);
-
-    Mat orange_blobs = findOrange(frame);
-
-    output = white_lines + blue_lines + yellow_lines + orange_blobs;
-
-    sensor_msgs::Image outmsg;
-
-    cv_ptr->image = output;
-	cv_ptr->encoding = "bgr8";
-	cv_ptr->toImageMsg(outmsg);
-	img_pub.publish(outmsg);
+	img_pub.publish(cv_bridge::CvImage{std_msgs::Header(), "bgr8", output}.toImageMsg());
 }
 
 int main(int argc, char** argv) {
@@ -176,16 +209,23 @@ int main(int argc, char** argv) {
     init(argc, argv, "color_detector_dragrace");
 
     NodeHandle nh;
+    
     //drag race
-    vector<Mat> mask_segments = {
-        Mat::zeros(120,640,CV_8UC3),
-        Mat(310,640,CV_8UC3, CV_RGB(1,1,1)),
-        Mat::zeros(50,640,CV_8UC3)
-    };
+    
+    //vector<Mat> mask_segments = {
+    //    Mat::zeros(120,640,CV_8UC3),
+    //    Mat(310,640,CV_8UC3, CV_RGB(1,1,1)),
+    //    Mat::zeros(50,640,CV_8UC3)
+    //};
+    //vconcat(mask_segments, mask);
+    
+    mask = Rect(0, 120, 640, 310); // x, y, w, h
+    
+    auto kernel_size = 3;
+    erosion_kernel = getStructuringElement(MORPH_CROSS, Size(kernel_size, kernel_size));
 
-    vconcat(mask_segments, mask);
 
-    ros::Subscriber img_saver_sub = nh.subscribe("/ps3_eye/image_raw", 1, ImageCB);
+    ros::Subscriber img_saver_sub = nh.subscribe("/camera/image_rect", 1, ImageCB);
 	
 	img_pub = nh.advertise<sensor_msgs::Image>(string("/colors_img"), 1);
         
