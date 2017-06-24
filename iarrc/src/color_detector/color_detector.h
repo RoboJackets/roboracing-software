5d09de2fa1bfc79dd7d0d5743889b4d59e8df2eb
Sahit Chintalapudi <chsahit@gmail.com>
2017-06-23 21:17:52 -0400

    stopped using namespace in header

--- iarrc/src/color_detector/color_detector.h
+++ iarrc/src/color_detector/color_detector.h
@@ -6,25 +6,18 @@
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 
-using namespace std;
-using namespace ros;
-using namespace cv;
-
 namespace iarrc {
     class color_detector: public nodelet::Nodelet {
-
         private:
-            Publisher img_pub;
-            Rect mask;
-            Mat erosion_kernel;
+            ros::Publisher img_pub;
+            cv::Rect mask;
+            cv::Mat erosion_kernel;
             void ImageCB(const sensor_msgs::ImageConstPtr &msg);
             inline bool is_blue(const uchar &H, const uchar &S);
             inline bool is_orange(const uchar &H, const uchar &S, const uchar &V);
             inline bool is_yellow(const uchar &H, const uchar &S, const uchar &V);
             inline bool is_white(const uchar &H, const uchar &S, const uchar &V);
             virtual void onInit();
-
-
     };
 
 }
