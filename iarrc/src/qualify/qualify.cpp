5ace29280e7068575fdb7030921d7bbfffbc330f
Sahit Chintalapudi <chsahit@gmail.com>
2017-07-10 18:51:30 -0400

    initial orientation control implementation

--- iarrc/src/qualify/qualify.cpp
+++ iarrc/src/qualify/qualify.cpp
@@ -2,11 +2,24 @@
 #include <rr_platform/speed.h>
 #include <rr_platform/steering.h>
 #include <rr_platform/chassis_state.h>
+#include <sensor_msgs/Imu.h>
+#include <tf/transform_datatypes.h>
 
 ros::Publisher speedPub;
 ros::Publisher steeringPub;
 
+double currentYaw = 0;
+const double kP = 1;
+
 bool go = false;
+
+void imuCallback(const sensor_msgs::ImuConstPtr& msg) {
+    tf::Quaternion orientationQuat;
+    tf::quaternionMsgToTF(msg->orientation, orientationQuat);
+    double roll, pitch, yaw;
+    tf::Matrix3x3(orientationQuat).getRPY(roll, pitch, yaw);
+    currentYaw = yaw;
+}
 
 void chassisCB(const rr_platform::chassis_stateConstPtr& msg) {
     go = msg->mux_automatic;
@@ -20,7 +33,7 @@ void publishSpeed(const float &desiredSpeed) {
     ROS_INFO("published");
     rr_platform::steering steeringMsg;
     steeringMsg.header.stamp = ros::Time::now();
-    steeringMsg.angle = 0.0;
+    steeringMsg.angle = -currentYaw * kP;
     steeringPub.publish(steeringMsg);
 }
 
@@ -29,7 +42,8 @@ int main(int argc, char* argv[]) {
     ros::NodeHandle nh;
     speedPub = nh.advertise<rr_platform::speed>("/speed", 1);
     steeringPub = nh.advertise<rr_platform::steering>("/steering", 1);
-    ros::Subscriber  chassis_sub = nh.subscribe("/chassis_state", 1, chassisCB);
+    ros::Subscriber chassisSub = nh.subscribe("/chassis_state", 1, chassisCB);
+    ros::Subscriber imuSub = nh.subscribe("/imu/data_raw", 1, imuCallback);
     while (!go && ros::ok()) {
         ros::spinOnce();
     }
