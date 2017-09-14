4b08eb8da95c895fa6f638948e6da5c9ec17ae36
Evan Bretl <evanbretl4@gmail.com>
2017-09-13 20:41:21 -0400

    sets realsense dynamic configuration from launch file
    works decently outdoors

--- rr_platform/launch/bigoli_camera_dynconfig.sh
+++ rr_platform/launch/bigoli_camera_dynconfig.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # There is no sleeping/waiting in launch files, so
-# unfortunately we're left with this
+# unfortunately we're left with this. The dynamic
+# reconfigure settings should be applied after all
+# the defaults have been applied
 
 sleep 10
 roslaunch rr_platform bigoli_camera_dynconfig.launch
