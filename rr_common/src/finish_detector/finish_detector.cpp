5781965f6ebeac6280be548dea67473916351775
Sahit Chintalapudi <chsahit@gmail.com>
2017-10-08 20:11:18 -0400

    finish detector compiles

---
+++ rr_common/src/finish_detector/finish_detector.cpp
@@ -0,0 +1,134 @@
+/*
+ * Modified version of old speed_controller node which now
+ * publishes the number of times the robot has crossed the
+ * finish line
+ */
+
+#include "finish_detector.h"
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <climits>
+#include <rr_platform/speed.h>
+#include <std_msgs/Int8.h>
+#include <pluginlib/class_list_macros.h> 
+
+using namespace std;
+using namespace cv;
+using namespace ros;
+
+namespace rr_common {
+using uchar = unsigned char;
+
+Publisher debug_pub;
+
+#define HIGH 1
+#define LOW 0
+
+int state = LOW;
+
+int number_of_crosses = 0;
+
+int finish_detector::getWidth(const Mat& image) {
+
+    int width = 0;
+
+    bool in_line = false;
+    int start;
+
+    for(int r = 0; r < image.rows; r++) {
+        auto row = image.ptr<uchar>(r);
+        for(int c = 0; c < image.cols; c++) {
+            if(!in_line && row[c]) {
+                in_line = true;
+                start = c;
+            } else if(in_line && !row[c]) {
+                in_line = false;
+                auto my_width = c - start;
+                width = max(width, my_width);
+            }
+        }
+    }
+    return width;
+}
+
+void finish_detector::ImageCB(const sensor_msgs::ImageConstPtr& msg) {
+    cv_bridge::CvImagePtr cv_ptr;
+	Mat frame;
+	Mat output;
+	
+	try {
+		cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+	} catch (cv_bridge::Exception& e) {
+		ROS_ERROR("CV-Bridge error: %s", e.what());
+		return;
+	}
+	
+	frame = cv_ptr->image;
+
+    for(int r = 0; r < frame.rows; r++) {
+        unsigned char* row = frame.ptr<unsigned char>(r);
+        for(int c = 0; c < frame.cols * frame.channels(); c+= frame.channels()) {
+            auto& blue = row[c];
+            auto& green = row[c+1];
+            auto& red = row[c+2];
+            if(blue == 255 && green == 0 && red == 0) {
+                blue = green = red = 255;
+            } else if(blue != 0 || green != 0 || red != 0) {
+                blue = green = red = 0;
+            }
+        }
+    }
+    cvtColor(frame, frame, CV_BGR2GRAY);
+
+    Rect ROI(0,270,640,50);
+
+    frame = frame(ROI);
+
+    auto count = countNonZero(frame);
+
+    auto width = finish_detector::getWidth(frame);
+
+    if(state == LOW && count > 1000 && width > 240) {
+        state = HIGH;
+    } else if(state == HIGH && count < 1000) {
+        // We crossed the line!
+        state = LOW;
+        number_of_crosses++;
+	ROS_INFO_STREAM("Finish line crossed - " << to_string(number_of_crosses));
+    }
+
+    sensor_msgs::Image outmsg;
+    cv_ptr->image = frame;
+	cv_ptr->encoding = "mono8";
+	cv_ptr->toImageMsg(outmsg);
+	debug_pub.publish(outmsg);
+}
+
+void finish_detector::onInit() {
+    
+    NodeHandle nh;
+    NodeHandle nhp("~");
+    image_transport::ImageTransport it(nh);
+
+    string img_topic;
+    nhp.getParam("img_topic", img_topic);
+    ROS_INFO("Finish line watching %s", img_topic.c_str());
+
+    img_saver_sub = it.subscribe(img_topic, 1, &finish_detector::ImageCB, this);
+
+    crosses_pub = nh.advertise<std_msgs::Int8>("finish_line_crosses", 1);
+    debug_pub = nhp.advertise<sensor_msgs::Image>("finish_line_debug_img", 1);
+
+    Rate rate(30);
+    while(ros::ok()) {
+        std_msgs::Int8 intmsg;
+        intmsg.data = number_of_crosses;
+        crosses_pub.publish(intmsg);
+   
+        spinOnce();
+        rate.sleep();
+    }
+
+}
+}
+PLUGINLIB_EXPORT_CLASS(rr_common::finish_detector, nodelet::Nodelet)
---
+++ rr_common/src/finish_detector/finish_detector.h
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <nodelet/nodelet.h>
+#include <image_transport/image_transport.h>
+#include <ros/publisher.h>
+#include <opencv2/opencv.hpp>
+
+namespace rr_common {
+class finish_detector : public nodelet::Nodelet {
+private:
+    ros::Publisher crosses_pub;
+    image_transport::Subscriber img_saver_sub;
+
+    int getWidth(const cv::Mat& image);
+    void ImageCB(const sensor_msgs::ImageConstPtr &msg);
+
+    virtual void onInit();
+};
+
+}
+
+
