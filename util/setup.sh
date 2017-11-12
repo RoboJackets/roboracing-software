1037539004b0a75d56bbd3c84c578452e013a767
Matthew Barulic <matthew.barulic@gmail.com>
2017-11-12 14:46:20 -0500

    Adds debugging lines to setup.sh for CircleCI. Attempting to debug CI failure from rviz dependency.

--- util/setup.sh
+++ util/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # A small script to automate installation of dependencies
-
+# This enables "unofficial Bash strict mode", which Jay recommended for debugging
+set -euo pipefail
+IFS=$'\n\t'
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/../.."
+
+echo "WHERE IS THIS RUN?"
 
 catkin_make || true
 sudo apt-get update
