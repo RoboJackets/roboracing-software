6d7fde08811978e67d5ea794e966381f41f139dc
Matthew Barulic <matthew.barulic@gmail.com>
2017-11-12 14:50:07 -0500

    Removes strict mode from setup.sh as our script actually depends on a failing catkin build.

--- util/setup.sh
+++ util/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 # A small script to automate installation of dependencies
-# This enables "unofficial Bash strict mode", which Jay recommended for debugging
-set -euo pipefail
-IFS=$'\n\t'
-
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
