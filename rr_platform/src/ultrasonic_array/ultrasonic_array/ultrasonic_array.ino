9d51c724630274884f451d86e1e9165619b70de5
Btdubs <Brianco1998@gmail.com>
2017-11-13 21:22:58 -0500

    Fixed Arduino read. Tested and works. Prints distances in meters to serial. Will need updating to work with MUX

---
+++ rr_platform/src/ultrasonic_array/ultrasonic_array/ultrasonic_array.ino
@@ -0,0 +1,60 @@
+#define NUM_SENSORS 2
+#define BAUD_RATE 9600
+#define SPEED_OF_SOUND 2941.2 //microseconds per meter!
+
+//Firmware for sensor array with HC-SR04 ultrasonic sensors
+//#TODO: write a version of ping that does not use delay. Increment through two at a time, need to write your own "pulseIn"
+//#TODO: https://www.bananarobotics.com/shop/HC-SR04-Ultrasonic-Distance-Sensor for info on sensor
+
+int trigPins[] = {2, 8}; //trigger pins in order of left to right
+int echoPins[] = {3, 9}; //echo pins in order of left to right
+double distances[NUM_SENSORS];
+
+
+double ping(int trigPin, int echoPin) {
+  //outputs distance (in cm currently; #TODO change that?)
+
+  long duration;
+  double distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); //May be unnecessary #TODO
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = ((double)duration / 2.0) / SPEED_OF_SOUND; //Distance = (Time x SpeedOfSound) / 2. The "2" is in the formula because the sound has to travel back and forth
+  return distance;
+
+}
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    pinMode(trigPins[i], OUTPUT);
+    pinMode(echoPins[i], INPUT);
+  }
+
+}
+
+void loop() {
+
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    distances[i] = ping(trigPins[i], echoPins[i]);
+    delay(5); //neccessary because echo from other sensor can interfere if lowre
+  }
+
+
+  //output distances to serial
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    Serial.print(distances[i]);
+    if (i != NUM_SENSORS - 1) {
+      Serial.print(",");
+    }
+  }
+  Serial.println();
+
+  delay(200); //may want to lessen this #TODO
+
+}
