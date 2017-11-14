45bb4d3af5d5f0fcd771898e27a5152cc53681c1
Btdubs <Brianco1998@gmail.com>
2017-11-13 23:48:39 -0500

    More fixing compile errors.

--- rr_platform/src/ultrasonic_array/ultrasonic_array_to_pcl.cpp
+++ rr_platform/src/ultrasonic_array/ultrasonic_array_to_pcl.cpp
@@ -2,6 +2,7 @@
 #include <ros/publisher.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
 #include <tf/transform_listener.h>
 #include <boost/asio.hpp>
 #include <boost/algorithm/string.hpp> //#TODO: is this the right import?
@@ -36,13 +37,13 @@ string readLine(boost::asio::serial_port &port) { //#TODO: ensure this reads the
     }
 }
 
-vector<float> parseLine(string line) {
-  vector<float> dist;
+vector<double> parseLine(string line) {
+  vector<double> dist;
   vector<string> strs;
   boost::split(strs, line, boost::is_any_of(","));
 
   for (int i = 0; i < strs.size(); i++) {
-    dist[i] = stof(strs[i]); //convert string to float
+    dist[i] = atof(strs[i].c_str()); //convert string to double
   }
 
   return dist;
@@ -58,9 +59,9 @@ void drawSemiCircle(pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointXYZ point, 
   float currentAngle = 3 * PI / 2; //#TODO: shift if necssary? Should provide semicircle with outside facing the robot
 
   for (int i = 0; i < numPoints; i++) {
-    int x = radius * (cos(currentAngle)) + point.x;
-    int y = -radius * (sin(currentAngle)) + point.y; //- because urdf standards of left is +y
-    pcl::PointXYZ newPoint = new pcl::PointXYZ(x, y, 0.0);
+    float x = radius * (cos(currentAngle)) + point.x;
+    float y = -radius * (sin(currentAngle)) + point.y; //- because urdf standards of left is +y
+    pcl::PointXYZ newPoint(x, y, 0.0);
     cloud.push_back(newPoint);
     currentAngle -= angle;
   }
@@ -71,7 +72,10 @@ void drawSemiCircle(pcl::PointCloud<pcl::PointXYZ> &cloud, pcl::PointXYZ point, 
 
 
 ros::Publisher pub;
-
+string sensor_base_link;
+string sensor_link;
+float rate;
+int baud_rate;
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "ultrasonic_array");
@@ -83,10 +87,10 @@ int main(int argc, char** argv) {
 
 // Serial port setup
     string serial_port_name;
-    nhp.param(string("serial_port"), serial_port_name, string("/dev/ttyACM0")); //#TODO: launch file
+    nhp.param(string("serial_port_name"), serial_port_name, string("/dev/ttyACM0")); //#TODO: launch file
     nhp.param(string("sensor_base_link"), sensor_base_link, string("ultrasonic_array_base"));
     nhp.param(string("sensor_link"), sensor_link, string("ultrasonic_"));
-    nhp.param(string("rate"), rate, 10.0); //#TODO
+    nhp.param(string("rate"), rate, 10.0f); //#TODO
     boost::asio::io_service io_service;
     boost::asio::serial_port serial(io_service, serial_port_name);
     serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
@@ -102,7 +106,7 @@ int main(int argc, char** argv) {
   //#####
     vector<pcl::PointCloud<pcl::PointXYZ>> clouds;
     tf::TransformListener tf_listener;
-    tf::Transform tf_transform;
+    tf::StampedTransform tf_transform;
     pcl::PointCloud<pcl::PointXYZ> compiled_cloud;
 
     //######
@@ -112,24 +116,24 @@ int main(int argc, char** argv) {
   while(ros::ok() && serial.is_open()) {
     ros::spinOnce();
 
-    string line = readLine();
-    vector<float> distances = parseLine(line);
+    string line = readLine(serial);
+    vector<double> distances = parseLine(line);
 
     for (int i = 0; i < NUM_SENSORS; i++) {
-      pcl::PointXYZ point = new PointXYZ(distances[i], 0.0, 0.0);
+      pcl::PointXYZ point(distances[i], 0.0, 0.0);
       clouds[i].push_back(point); //add point given
-      drawSemiCircle(cloud[i], points, RADIUS, NUM_POINTS); //numPoints is number of points including the one we added line above!
+      drawSemiCircle(clouds[i], point, RADIUS, NUM_POINTS); //numPoints is number of points including the one we added line above!
 
 
       if(tf_listener.waitForTransform(sensor_base_link, sensor_link + to_string(i), ros::Time(0), ros::Duration(3.0))) {
-        tf_listener.lookupTransform(sensor_base_link, sensor_link + to_string(i), ros::Time(0), transform);
+        tf_listener.lookupTransform(sensor_base_link, sensor_link + to_string(i), ros::Time(0), tf_transform);
         //transform cloud to base_link frame
         //pcl::transformPointCloud(base_link, cloud[i], cloud[i], listener); #TODO: can we use this and get rid of the listener if above?? I don't understand the co
-        pcl::transformPointCloud(cloud[i], cloud[i], transform);
+        pcl_ros::transformPointCloud(clouds[i], clouds[i], tf_transform);
       }
 
       //add point clouds to the output one
-      pcl::concatenatePointCloud(cloud[i], compiled_cloud, compiled_cloud);
+      pcl::concatenatePointCloud(clouds[i], compiled_cloud, compiled_cloud);
     }
 
 
