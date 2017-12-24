44c1f2fef6959a728b7a1e75e05be9c96614b439
Sahit Chintalapudi <chsahit@gmail.com>
2017-12-23 22:30:03 -0500

    data has the right shape

--- util/train.py
+++ util/train.py
@@ -2,12 +2,19 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras import backend
+from keras.datasets import mnist
 
 def defineCategory(steering):
     categories = [-0.25, -0.1, 0, 0.1, 0.25]
     differences = [abs(steering - category) for category in categories]
     oneHot = [1 if i == category else 0 for i in range(len(categories))]
     return oneHot
+
+def flatten(data):
+    dataFlattened = np.zeros((len(data), 240, 320, 3))
+    for i in range(len(data)):
+        dataFlattened[i] = data[i]
+    return dataFlattened
 
 #get our data
 data = np.load('data.npy')
@@ -17,9 +24,11 @@ testData = data[1500:]
 print backend.image_data_format()
 
 #format our data
-xTrain  = trainData[:,0]
-xTrain.reshape(1500, 240, 320, 3)
-xTest = testData[:,0]
+xTrain = flatten(trainData[:,0])
+#xTrain.reshape(1500, 240, 320, 3)
+
+print xTrain.shape
+xTest = flatten(testData[:,0])
 xTrain = xTrain.astype('float32')
 xTest  = xTest.astype('float32')
 xTrain /= 255
