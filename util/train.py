3eb4229c0e0f107ad351d88041b46afc160dd1cb
Evan Bretl <evanbretl4@gmail.com>
2017-12-26 23:02:04 -0600

    discriminates between finer categories

--- util/train.py
+++ util/train.py
@@ -5,31 +5,32 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.datasets import mnist
 import cv2
+import collections
+import random
+
+#get our data
+data = np.load('data.npy')
+np.random.shuffle(data)
+n_train = int(len(data) * 0.7)
+trainData = data[:n_train]
+testData = data[n_train:]
+
+input_shape = (60, 80, 3)
+batch_size = 128
+epochs = 12
+categories = [-0.1, -0.05, 0, 0.05, 0.1]
 
 def defineCategory(steering):
-    categories = [-0.25, -0.1, 0, 0.1, 0.25]
     differences = [abs(steering - category) for category in categories]
     category = np.argmin(differences)
     oneHot = [1 if i == category else 0 for i in range(len(categories))]
     return oneHot
 
 def format_data(data):
-    dataFlattened = np.zeros((len(data), 60, 80, 3))
+    data2 = np.zeros((len(data), 60, 80, 3))
     for i in range(len(data)):
-        dataFlattened[i] = cv2.resize(data[i], None, fx=0.25, fy=0.25)
-    return dataFlattened
-
-#get our data
-data = np.load('data.npy')
-np.random.shuffle(data)
-n_train = int(len(data) * 0.8)
-trainData = data[:n_train]
-testData = data[n_train:]
-
-input_shape = (60, 80, 3)
-num_classes = 5
-batch_size = 64
-epochs = 100
+        data2[i] = cv2.resize(data[i], None, fx=0.25, fy=0.25)
+    return data2
 
 #format our data
 xTrain = format_data(trainData[:,0])
@@ -43,8 +44,17 @@ xTest /= 255
 yTrain = np.array([defineCategory(steer) for steer in trainData[:,1]])
 yTest = np.array([defineCategory(steer) for steer in testData[:,1]])
 
-n_2 = sum(1 if np.argmax(ys)==2 else 0 for ys in yTest)
-print float(n_2) / len(yTest)
+cnt = collections.Counter()
+for x,y in zip(xTrain,yTrain):
+    i = np.argmax(y)
+    cnt[i] += 1
+print "training label counts:", cnt
+
+cnt = collections.Counter()
+for x,y in zip(xTest,yTest):
+    i = np.argmax(y)
+    cnt[i] += 1
+print "testing label counts:", cnt
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
@@ -56,13 +66,12 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(len(categories), activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               # optimizer=keras.optimizers.RMSprop(lr=0.0002),
               metrics=['accuracy'])
-
 
 model.fit(xTrain, yTrain,
           batch_size=batch_size,
