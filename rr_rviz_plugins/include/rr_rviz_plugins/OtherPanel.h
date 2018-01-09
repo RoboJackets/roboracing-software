602a8109210bd25bc4aeea42d1a5f73abf90f515
Mukund Pandiri <mpandiri3@gatech.edu>
2018-01-08 19:18:50 -0500

    This adds the new panel with the speedometer

---
+++ rr_rviz_plugins/include/rr_rviz_plugins/OtherPanel.h
@@ -0,0 +1,69 @@
+#ifndef PROJECT_OTHERPANEL_H
+#define PROJECT_OTHERPANEL_H
+
+#include <ros/ros.h>
+#include <rviz/panel.h>
+#include <rr_platform/chassis_state.h>
+#include <QLabel>
+#include <QtGui>
+#include <QPainter>
+#include <math.h>
+
+
+/*
+ * All of our panels need to be under the rr_rviz_plugins namespace.
+ */
+namespace rr_rviz_plugins {
+
+/*
+ * Each panel is a subclass of rviz::Panel
+ */
+class OtherPanel : public rviz::Panel {
+
+
+
+
+/*
+ * rviz is based on QT, so our panels need to be QObjects, which requires this macro keyword.
+ */
+Q_OBJECT
+public:
+
+    /**
+     * This is a standard QWidget constructor.
+     * @param parent The parent widget, which will be responsible for the lifetime of this widget.
+     */
+    OtherPanel(QWidget *parent = 0);
+
+protected:
+    /*
+     * stuff for painting
+     */
+    int height = 200;
+    int width = 200;
+    int maxSpeed = 60;
+    void paintEvent(QPaintEvent*);
+    /*
+     * Be sure to make any publishers / subscribers members of the class. This will keep them alive throughout
+     * the lifetime of the widget.
+     */
+    ros::Subscriber chassis_subscriber;
+
+    /**
+     * Declare any ROS callbacks you need here. Be sure to create a parameter for any UI elements you need to update.
+     * @param msg The ROS message that triggers this callback.
+     * @param label A QT label whose text we will update based on the message contents.
+     */
+    void chassis_callback(const rr_platform::chassis_stateConstPtr &msg, QLabel *speedlabel);
+
+    /**
+     * If you need to paint custom graphics on your panel, uncomment and implement the paintEvent method.
+     * You can find out more about this method here: http://doc.qt.io/qt-5/qwidget.html#paintEvent
+     */
+//    void paintEvent(QPaintEvent *event) override;
+
+};
+
+}
+
+#endif //PROJECT_OTHERPANEL_H
---
+++ rr_rviz_plugins/src/OtherPanel.cpp
@@ -0,0 +1,79 @@
+#include <rr_rviz_plugins/OtherPanel.h>
+#include <QVBoxLayout>
+#include <pluginlib/class_list_macros.h>
+
+
+/*
+ * Just as in the header, everything needs to happen in the rr_rviz_plugins namespace.
+ */
+namespace rr_rviz_plugins {
+
+
+
+OtherPanel::OtherPanel(QWidget *parent)
+  : rviz::Panel(parent) // Base class constructor
+{
+
+    // Panels are allowed to interact with NodeHandles directly just like ROS nodes.
+    ros::NodeHandle handle;
+
+    // Initialize a label for displaying some data
+    QLabel *speedlabel = new QLabel("0 m/s");
+
+
+    /* Initialize our subscriber to listen to the /speed topic.
+    * Note the use of boost::bind, which allows us to give the callback a pointer to our UI label.
+    */
+    chassis_subscriber = handle.subscribe<rr_platform::chassis_state>("/chassis_state", 1, boost::bind(&OtherPanel::chassis_callback, this, _1, speedlabel));
+
+    /* Use QT layouts to add widgets to the panel.
+    * Here, we're using a VBox layout, which allows us to stack all of our widgets vertically.
+    */
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(speedlabel);
+    setLayout(layout);
+    
+
+}
+
+void OtherPanel::paintEvent(QPaintEvent*)
+{
+ QPainter painter(this);
+ painter.drawLine(width,height, 210,0);
+ painter.drawArc(10,-200,400,400,0,-5760/2);
+
+
+ //this is for the lines every 10mph
+ painter.drawLine(0,0,20,0);
+ painter.drawLine(210-(190*.866),190/2, 210-(210*.866),105);
+ painter.drawLine(210-(190*.5),190*.866, 210-(210*.5),210*.866);
+ painter.drawLine(210,190,210,210);
+ painter.drawLine(210+(190*.5),190*.866, 210+(210*.5),210*.866);
+ painter.drawLine(210+(190*.866),190/2, 210+(210*.866),105);
+ painter.drawLine(210+190,0,420,0);
+
+}
+
+void OtherPanel::chassis_callback(const rr_platform::chassis_stateConstPtr &msg, QLabel *speedlabel) {
+    // Create the new contents of the label based on the speed message.
+    auto text = std::to_string(msg->speed_mps) + " m/s (actual)";
+    // Set the contents of the label.
+    speedlabel->setText(text.c_str());
+
+    //calculations for height and width
+    double angle = ((msg->speed_mps)/maxSpeed)*3.14159256;
+    width = 210-(200*cos(angle));
+    height = 200*sin(angle);
+    update();
+}
+
+//void ExamplePanel::paintEvent(QPaintEvent *event)  {
+//
+//}
+
+}
+
+/*
+ * IMPORTANT! This macro must be filled out correctly for your panel class.
+ */
+PLUGINLIB_EXPORT_CLASS( rr_rviz_plugins::OtherPanel, rviz::Panel)
