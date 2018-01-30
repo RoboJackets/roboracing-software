0fedb1f3f4ee0c25afae7dbf660d7fa6cd8ff984
Evan Bretl <evanbretl4@gmail.com>
2018-01-29 20:06:43 -0500

    makes nn training "work" on "real" data

--- util/neural_nets/bag_to_data.py
+++ util/neural_nets/bag_to_data.py
@@ -28,7 +28,16 @@ def convert_bag_file(bag_file_path, output_dir, examples_per_file):
     iter_obj = iter(bag.read_messages(topics=["/steering"]))
     topic2, msg, t2 = iter_obj.next()
 
-    for topic1, img, t1 in bag.read_messages(topics=['/camera/image_rect']):
+    topics = bag.get_type_and_topic_info()[1].keys()
+    if '/camera/image_rect' in topics:
+        camera_topic = '/camera/image_rect'
+    elif '/camera/image_raw' in topics:
+        camera_topic = '/camera/image_raw'
+    else:
+        print "ERROR: camera topic not recognized"
+        sys.exit(2);
+
+    for topic1, img, t1 in bag.read_messages(topics=[camera_topic]):
         try:
             cv_image = bridge.imgmsg_to_cv2(img, 'bgr8')
 
