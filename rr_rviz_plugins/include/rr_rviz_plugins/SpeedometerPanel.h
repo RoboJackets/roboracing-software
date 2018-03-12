e36887e762f362ec4eae42145ee49d451137369b
Mukund Pandiri <mpandiri3@gatech.edu>
2018-03-12 19:18:57 -0400

    removed the comments

--- rr_rviz_plugins/include/rr_rviz_plugins/SpeedometerPanel.h
+++ rr_rviz_plugins/include/rr_rviz_plugins/SpeedometerPanel.h
@@ -12,35 +12,23 @@
 #include <QString>
 
 
-/*
- * All of our panels need to be under the rr_rviz_plugins namespace.
- */
+
 namespace rr_rviz_plugins {
 
-/*
- * Each panel is a subclass of rviz::Panel
- */
+
 class SpeedometerPanel : public rviz::Panel {
 
 
 
 
-/*
- * rviz is based on QT, so our panels need to be QObjects, which requires this macro keyword.
- */
 Q_OBJECT
 public:
 
-    /**
-     * This is a standard QWidget constructor.
-     * @param parent The parent widget, which will be responsible for the lifetime of this widget.
-     */
+
     SpeedometerPanel(QWidget *parent = 0);
 
 protected:
-    /*
-     * stuff for painting
-     */
+
     int height = 200;
     int width = 200;
     int maxSpeed = 3;
@@ -48,18 +36,11 @@ protected:
     double wheelAngle = 0;	
     void paintEvent(QPaintEvent* event) override;
     void tickMarkGenerator(QPainter &painter, float speeds);
-    /*
-     * Be sure to make any publishers / subscribers members of the class. This will keep them alive throughout
-     * the lifetime of the widget.
-     */
+
     ros::Subscriber chassis_subscriber;
     ros::Subscriber steering_subscriber;
 
-    /**
-     * Declare any ROS callbacks you need here. Be sure to create a parameter for any UI elements you need to update.
-     * @param msg The ROS message that triggers this callback.
-     * @param label A QT label whose text we will update based on the message contents.
-     */
+ 
     void chassis_callback(const rr_platform::chassis_stateConstPtr &msg, QLabel *speedlabel);
 
     void steering_callback(const rr_platform::steeringConstPtr &msg); 
@@ -69,4 +50,4 @@ protected:
 
 }
 
-#endif //PROJECT_SpeedometerPanel_H
+#endif 
--- rr_rviz_plugins/src/SpeedometerPanel.cpp
+++ rr_rviz_plugins/src/SpeedometerPanel.cpp
@@ -3,33 +3,26 @@
 #include <pluginlib/class_list_macros.h>
 
 
-/*
- * Just as in the header, everything needs to happen in the rr_rviz_plugins namespace.
- */
+
 namespace rr_rviz_plugins {
 
 
 
 SpeedometerPanel::SpeedometerPanel(QWidget *parent)
-  : rviz::Panel(parent) // Base class constructor
+  : rviz::Panel(parent) 
 {
 
-    // Panels are allowed to interact with NodeHandles directly just like ROS nodes.
+
     ros::NodeHandle handle;
 
-    // Initialize a label for displaying some data
+
     QLabel *speedlabel = new QLabel("0 m/s");
 
 
-    /* Initialize our subscriber to listen to the /speed topic.
-    * Note the use of boost::bind, which allows us to give the callback a pointer to our UI label.
-    */
     chassis_subscriber = handle.subscribe<rr_platform::chassis_state>("/chassis_state", 1, boost::bind(&SpeedometerPanel::chassis_callback, this, _1, speedlabel));
     steering_subscriber = handle.subscribe<rr_platform::steering>("/steering", 1, &SpeedometerPanel::steering_callback, this); 	
 
-    /* Use QT layouts to add widgets to the panel.
-    * Here, we're using a VBox layout, which allows us to stack all of our widgets vertically.
-    */
+
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(speedlabel);
     setLayout(layout);
@@ -44,54 +37,13 @@ void SpeedometerPanel::paintEvent(QPaintEvent* event)
  painter.drawArc(70,70,400,400,0,5760/2);
  painter.drawLine(0+60,270,480,270);
 
- //this is for the ticks (every .5 m/s)
-
-
- // painter.drawLine(0+60,210+60,20+60,210+60);
- // QString *string_one = new QString("0");
- // painter.drawText(40,270,*string_one);
-
- 
-
-
- // painter.drawLine(210-(190*.866)+60,210-190/2+60, 210-(210*.866)+60,210-105+60);
- // QString *string_two = new QString("0.5");
- // painter.drawText(210-(190*.866)+60-45,210-190/2+60-15,*string_two);
-
- 
 
  for (float i = 0; i <= 3; i += 0.5) {
     tickMarkGenerator(painter, i);
  }
 
 
- // painter.drawLine(210-(190*.5)+60,210-190*.866+60, 210-(210*.5)+60,210-210*.866+60);
- // QString *string_three = new QString("1.0");
- // painter.drawText(210-(190*.4)+60-45,210-190*.5-40,*string_three);
 
-
-
- // painter.drawLine(210+60,210-190+60,210+60,210-210+60);
- // QString *string_four = new QString("1.5");
- // painter.drawText(260,47,*string_four);
-
-
-
- // painter.drawLine(210+(190*.5)+60,210-190*.866+60, 210+(210*.5)+60,210-210*.866+60);
- // QString *string_five = new QString("2.0");
- // painter.drawText(210+(190*.4)+60+25,210-190*.5-40,*string_five);
-
-
-
-
- // painter.drawLine(210+(190*.866)+60,210-190/2+60, 210+(210*.866)+60,210-105+60);
- // QString *string_six = new QString("2.5");
- // painter.drawText(210+(190*.866)+45+45,210-190/2+60-15,*string_six);
-
-
- // painter.drawLine(210+190+60,210+60,420+60,210+60);
- // QString *string_seven = new QString("3.0");
- // painter.drawText(493,270,*string_seven);
 
 
  painter.drawArc(70,400,400,400,0,5760);
@@ -102,7 +54,7 @@ void SpeedometerPanel::paintEvent(QPaintEvent* event)
 }
 
 
-//thing to make tick marks - finish this
+
 void SpeedometerPanel::tickMarkGenerator(QPainter &painter, float speeds) {
     float speed = 3.1415 - (speeds/3) * 3.1415;
     
@@ -113,17 +65,17 @@ void SpeedometerPanel::tickMarkGenerator(QPainter &painter, float speeds) {
 }
 
 void SpeedometerPanel::chassis_callback(const rr_platform::chassis_stateConstPtr &msg, QLabel *speedlabel) {
-    // Create the new contents of the label based on the speed message.
+    
     auto text = std::to_string(msg->speed_mps) + " m/s (actual)";
-    // Set the contents of the label.
+    
     speedlabel->setText(text.c_str());
 
-    //calculations for height and width
+    
     double angle = ((msg->speed_mps)/maxSpeed)*3.14159256;
     width = 60+210-(200*cos(angle));
     height = 60+210-200*sin(angle);
 	
-    //wheelAngle = 1;
+   
     
     	
     update();
@@ -135,13 +87,9 @@ void SpeedometerPanel::steering_callback(const rr_platform::steeringConstPtr &ms
    update();
 }
 
-//void ExamplePanel::paintEvent(QPaintEvent *event)  {
-//
-//}
+
 
 }
 
-/*
- * IMPORTANT! This macro must be filled out correctly for your panel class.
- */
+
 PLUGINLIB_EXPORT_CLASS( rr_rviz_plugins::SpeedometerPanel, rviz::Panel)
