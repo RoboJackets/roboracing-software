61693e24e2073696cc4c224963fa8f99aa88be39
Evan Bretl <evanbretl4@gmail.com>
2018-05-26 11:32:41 -0400

    cleans up train.py

--- util/neural_nets/train.py
+++ util/neural_nets/train.py
@@ -15,8 +15,10 @@ from params import input_shape
 
 n_examples_to_load = 8000 # if the number of training examples is below this, load more data
 batch_size = 32
-epochs = 20
-categories = [-0.2, -0.05, 0, 0.05, 0.2]
+epochs = 10
+
+categories = [0.05, 0.2]
+categories = [-x for x in reversed(categories)] + [0] + categories
 
 
 def defineCategory(steering):
@@ -29,6 +31,7 @@ def format_inputs(examples):
     data2 = np.zeros((len(examples),) + input_shape, dtype='float32')
     for i, ex in enumerate(examples):
         data2[i] = ex.get_image()
+    data2 /= 255.0
     return data2
 
 def make_model():
@@ -82,7 +85,7 @@ if __name__ == '__main__':
     print "total training examples:", n_training_examples
     print "training label counts:", cnt
 
-    def batch_generator(isTraining):
+    def batch_generator(isValidation = False):
         for epoch in range(epochs):
             exampleSetFiles = list(exampleSetFiles_const)
             random.shuffle(exampleSetFiles)
@@ -91,52 +94,33 @@ if __name__ == '__main__':
                 D = []
                 while len(exampleSetFiles) > 0 and len(D) < n_examples_to_load:
                     data = ExampleSet.load(os.path.join(exampleSetDir, exampleSetFiles.pop()))
-                    D += data.train if isTraining else data.test
+                    D += data.test if isValidation else data.train
 
-                if isTraining: random.shuffle(D)
+                if not isValidation: random.shuffle(D)
 
-                #format our data
                 X = format_inputs(D)
-                X /= 255.0
 
                 #create output bins
                 labels = np.array([defineCategory(ex.angle) for ex in D])
 
-                if isTraining:
+                if not isValidation:
                     for i in range(len(X)):
                         if random.random() < 0.4: # 40% of images are flipped
                             X[i] = cv2.flip(X[i], 1)
                             labels[i] = labels[i][::-1]
-                        # print(y[i])
 
                 for i in range(0, len(X), batch_size):
                     xs = X[i: i + batch_size]
                     ys = labels[i: i + batch_size]
                     yield (xs, ys)
 
-    """
-    keras.preprocessing.image.ImageDataGenerator(featurewise_center=False, samplewise_center=False,
-        featurewise_std_normalization=False, samplewise_std_normalization=False, zca_whitening=False,
-        zca_epsilon=1e-06, rotation_range=0.0, width_shift_range=0.0, height_shift_range=0.0,
-        brightness_range=None, shear_range=0.0, zoom_range=0.0, channel_shift_range=0.0, fill_mode='nearest',
-        cval=0.0, horizontal_flip=False, vertical_flip=False, rescale=None, preprocessing_function=None,
-        data_format=None, validation_split=0.0)
-    """
-    # augmented_data_generator = ImageDataGenerator()
 
-        # augmented_data_generator.fit(xTrain)
-        # model.fit_generator(augmented_data_generator.flow(xTrain, yTrain, batch_size = batch_size),
-        #     epochs = 1,
-        #     verbose = 1,
-        #     validation_data=(xTest, yTest))
-
-
-    model.fit_generator(batch_generator(True), steps_per_epoch=n_training_examples//batch_size + 1,
+    model.fit_generator(batch_generator(), steps_per_epoch=n_training_examples//batch_size + 1,
                         epochs=epochs, verbose=1)
 
     model.save(name)
 
-    loss, acc = model.evaluate_generator(batch_generator(False), steps=n_test_examples//batch_size + 1)
+    loss, acc = model.evaluate_generator(batch_generator(True), steps=n_test_examples//batch_size + 1)
     print "validation loss:", loss, "| validation accuracy:", acc
 
     print "elapsed time:", time.time() - startTime
