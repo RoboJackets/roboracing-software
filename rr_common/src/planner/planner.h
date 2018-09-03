bd6ef182e309eafac38719a1c108bb7aae1591c2
Evan Bretl <evanbretl4@gmail.com>
2018-09-03 03:07:38 -0400

    finishes overhauling planner, minus some inevitable tweaks

--- rr_common/src/planner/planner.h
+++ rr_common/src/planner/planner.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <random>
+#include <tuple>
+
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/point_types.h>
 
@@ -15,12 +17,18 @@ struct Pose {
   double x;
   double y;
   double theta;
+
+  Pose(double x, double y, double theta) : x(x), y(x), theta(theta) {}
+  Pose() : x(0), y(0), theta(0) {}
 };
 
 struct PathPoint {
   Pose pose;
   double steer;
   double speed;
+
+  PathPoint(Pose pose, double steer, double speed) : pose(pose), steer(steer), speed(speed) {}
+  PathPoint() : pose(), steer(0), speed(0) {}
 };
 
 struct PlannedPath {
@@ -30,10 +38,10 @@ struct PlannedPath {
 };
 
 struct CollisionBox {
-  double lengthFront;  // distance from origin to front edge
-  double lengthBack;  // ditto for back edge, etc.
-  double widthLeft;
-  double widthRight;
+  double length_front;  // distance from origin to front edge
+  double length_back;  // ditto for back edge, etc.
+  double width_left;
+  double width_right;
 };
 
 class Planner
@@ -41,32 +49,57 @@ class Planner
 public:
 
   struct Params {
-    int n_path_segments;
-    int n_control_samples;
-    int smoothing_array_size;
-    double distance_increment;
-    double max_speed;
+    // robot
     double wheel_base;
-    double path_similarity_cutoff;
-    double max_relative_cost;
-    double obstacle_search_radius;
+    double lateral_accel;
+    CollisionBox collision_box;
+
+    // path generation
+    int n_path_segments;
     std::vector<double> segment_distances;
     std::vector<double> steer_limits;
     std::vector<double> steer_stddevs;
-    CollisionBox collision_box;
+
+    // path filtering
+    double obstacle_search_radius;
+    double path_similarity_cutoff;
+    double max_relative_cost;
+    double k_dist;
+    double k_speed;
+
+    // CPU usage tradeoff
+    int n_control_samples;
+    double distance_increment;
+
+    // control
+    int smoothing_array_size;
+    double max_speed;
   };
 
 
-  Planner(const Params &params);
+  Planner(const Params& params);
 
   ~Planner() = default;
 
-  PlannedPath Plan(const KdTreeMap &kd_tree_map);
+  PlannedPath Plan(const KdTreeMap& kd_tree_map);
 
 private:
 
+  /*
+   * steeringSample: get a random steering value from a normal
+   * distribution. Each path stage has a potentially different bell
+   * curve. If the value is out of bounds, try again
+   * Params:
+   * stage - the path stage or control vector dimension
+   */
   double SampleSteering(int stage);
 
+  /*
+   * SteeringToSpeed: Calculate a desired speed from a steering angle based on a maximum
+   * acceptable lateral acceleration. See https://www.desmos.com/calculator/7nh83g8afj
+   * Params:
+   * steer_angle - proposed steering value to publish
+   */
   double SteeringToSpeed(double steer_angle);
 
   /*
@@ -76,25 +109,54 @@ private:
    * steer_angle - steering angle in radians centered at 0
    * pose - (x,y,theta)
    */
-  Pose StepKinematics(const Pose &pose, double steer_angle);
+  Pose StepKinematics(const Pose& pose, double steer_angle);
 
-  std::vector<PathPoint> RollOutPath(const std::vector<double> &control);
+  /*
+   * RollOutPath: roll out a trajectory through the world using a given control vector.
+   * Params:
+   * - control - vector of dimension N_PATH_SEGMENTS with a steering value for 
+                 each stage/dimension
+   */
+  std::vector<PathPoint> RollOutPath(const std::vector<double>& control);
 
   /*
    * GetCollisionDistance: calculate the shortest distance from any part of the robot to any
    * point in the map.
    * Params:
-   * - pose - Future pose relative to current pose. The robot's initial state is (0, 0, 0)
-   * - kdtree - nearest-neighbors-searchable map
+   * pose - Future pose relative to current pose. The robot's initial state is (0, 0, 0)
+   * kdtree - nearest-neighbors-searchable map
    * Returns:
-   * - bool, collision detected
-   * - double, clearing distance to nearest obstacle
+   * bool - collision detected
+   * double - clearing distance to nearest obstacle
    */
-  std::tuple<bool, double> GetCollisionDistance(const Pose &pose, const KdTreeMap &kd_tree_map);
+  std::tuple<bool, double> GetCollisionDistance(const Pose& pose, const KdTreeMap& kd_tree_map);
 
-  std::tuple<bool, double> GetCost(const std::vector<double> &control, const KdTreeMap &kd_tree_map);
+  /*
+   * GetCost: calculate the total cost of a path.
+   * cost = path integral 1 / (k_dist * obstacle_distance + k_speed * speed)
+   * Params:
+   * path - list of (pose, steering, speed) tuples
+   * kdtree - nearest-neighbors-searchable map
+   * Returns:
+   * bool - collision detected
+   * double - cost
+   */
+  std::tuple<bool, double> GetCost(const std::vector<PathPoint>& path, const KdTreeMap& kd_tree_map);
 
-  std::vector<int> GetLocalMinima(const std::vector<PlannedPath> &paths);
+  /*
+   * GetLocalMinima: A "clustering" function. Given a set of paths, find local cost
+   * minima in the space of control vectors and return these indices from the input matrix.
+   * Params:
+   * paths - list of PlannedPaths
+   * Return:
+   * list of indices of selected paths
+   */
+  std::vector<int> GetLocalMinima(const std::vector<std::reference_wrapper<PlannedPath>>& paths);
+
+  /* 
+   * FilterOutput: Apply a filter to the outputs of this planner over time
+   */
+  double FilterOutput(double this_steer);
 
 
   // algorithm parameters
@@ -104,7 +166,7 @@ private:
   std::vector<double> prev_steering_angles_;
   int prev_steering_angles_index_;
 
-  std::vector<std::normal_distribution<double>> steering_gaussians_;
+  std::vector<std::normal_distribution<float> > steering_gaussians_;
   std::mt19937 rand_gen_;
 };
 
@@ -112,4 +174,4 @@ private:
 }  // namespace planning
 }  // namespace rr
 
-#endif //RR_COMMON_PLANNER_H
+#endif  // RR_COMMON_PLANNER_H
--- rr_common/src/planner/planner_node.cpp
+++ rr_common/src/planner/planner_node.cpp
@@ -2,13 +2,79 @@
 
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
+#include <geometry_msgs/PoseStamped.h>
+
 #include <rr_platform/speed.h>
 #include <rr_platform/steering.h>
 
 #include "planner.h"
 
+
+std::unique_ptr<rr::planning::Planner> planner;
+
+ros::Publisher speed_pub;
+ros::Publisher steer_pub;
+ros::Publisher path_pub;
+
+
 void mapCallback(const sensor_msgs::PointCloud2ConstPtr& map) {
-  
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl_conversions::toPCL(*map, pcl_pc2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
+
+  if (cloud->empty()) {
+    // Do not publish new commands
+    ROS_WARN("environment map pointcloud is empty");
+    return;
+  }
+
+  pcl::KdTreeFLANN<pcl::PointXYZ> kdtree(false);
+  kdtree.setInputCloud(cloud);
+
+  rr::planning::PlannedPath plan = planner->Plan(kdtree);
+
+  rr_platform::speedPtr speedMSG(new rr_platform::speed);
+  rr_platform::steeringPtr steerMSG(new rr_platform::steering);
+  steerMSG->angle = plan.path[0].steer;
+  speedMSG->speed = plan.path[0].speed;
+  steerMSG->header.stamp = ros::Time::now();
+  speedMSG->header.stamp = ros::Time::now();
+  speed_pub.publish(speedMSG);
+  steer_pub.publish(steerMSG);
+
+  if(path_pub.getNumSubscribers() > 0) {
+    nav_msgs::Path pathMsg;
+    
+    for (auto path_point : plan.path) {
+      geometry_msgs::PoseStamped ps;
+      ps.pose.position.x = path_point.pose.x;
+      ps.pose.position.y = path_point.pose.y;
+      pathMsg.poses.push_back(ps);
+    }
+
+    pathMsg.header.frame_id = "base_footprint";
+    path_pub.publish(pathMsg);
+  }
+}
+
+std::vector<double> getDoubleListParam(const ros::NodeHandle& nhp, const std::string& name, char delim) {
+  std::string listAsString;
+  nhp.getParam(name, listAsString);
+
+  std::vector<double> out;
+
+  std::stringstream ss(listAsString);
+  std::string s;
+  while (std::getline(ss, s, delim)) {
+    out.push_back(std::stod(s));
+  }
+
+  return out;
 }
 
 int main(int argc, char** argv)
@@ -18,33 +84,44 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::NodeHandle nhp("~");
 
-  // nhp.param("N_PATH_SEGMENTS", N_PATH_SEGMENTS, 2);
-  // nhp.param("N_CONTROL_SAMPLES", N_CONTROL_SAMPLES, 200);
-  // nhp.param("SEGMENT_DISTANCES", segmentDistancesStr, string("3 5"));
-  // nhp.param("STEER_LIMITS", steerLimitsStr, string("0.4 0.4"));
-  // nhp.param("STEER_STDDEVS", steerStdDevsStr, string("0.2 0.2"));
-  // nhp.param("DISTANCE_INCREMENT", DISTANCE_INCREMENT, 0.2f);
-  // nhp.param("MAX_SPEED", MAX_SPEED, 0.5f);
-  // nhp.param("WHEEL_BASE", WHEEL_BASE, 0.37f); //TODO get from tf tree
-  // nhp.param("COLLISION_DIST_FRONT", robot_collision_box.lengthFront, 0.3f);
-  // nhp.param("COLLISION_DIST_BACK", robot_collision_box.lengthBack, 0.3f);
-  // nhp.param("COLLISION_DIST_SIDE", robot_collision_box.widthLeft, 0.3f);
-  // nhp.param("COLLISION_PENALTY", COLLISION_PENALTY, 1000.0f);
-  // nhp.param("PATH_SIMILARITY_CUTOFF", PATH_SIMILARITY_CUTOFF, 0.05f);
-  // nhp.param("MAX_RELATIVE_COST", MAX_RELATIVE_COST, 2.0f);
-  // nhp.param("SMOOTHING_ARRAY_SIZE", SMOOTHING_ARRAY_SIZE, 20);
-  // nhp.param("OBSTACLE_SEARCH_RADIUS", OBSTACLE_SEARCH_RADIUS, 2.0f);
+  rr::planning::Planner::Params params;
 
-  nhp.getParam("input_cloud_topic", obstacleCloudTopic);
-  auto map_sub = nh.subscribe(obstacleCloudTopic, 1, mapCallback);
+  nhp.getParam("wheel_base", params.wheel_base);
+  nhp.getParam("lateral_accel", params.lateral_accel);
+  nhp.getParam("collision_dist_front", params.collision_box.length_front);
+  nhp.getParam("collision_dist_back", params.collision_box.length_back);
+  nhp.getParam("collision_dist_side", params.collision_box.width_left);
+  params.collision_box.width_right = params.collision_box.width_left;
+
+  nhp.getParam("n_path_segments", params.n_path_segments);
+  params.segment_distances = getDoubleListParam(nhp, "segment_distances", ' ');
+  params.steer_limits = getDoubleListParam(nhp, "steer_limits", ' ');
+  params.steer_stddevs = getDoubleListParam(nhp, "steer_stddevs", ' ');
+
+  nhp.getParam("obstacle_search_radius", params.obstacle_search_radius);
+  nhp.getParam("path_similarity_cutoff", params.path_similarity_cutoff);
+  nhp.getParam("max_relative_cost", params.max_relative_cost);
+  nhp.getParam("k_dist", params.k_dist);
+  nhp.getParam("k_speed", params.k_speed);
+
+  nhp.getParam("n_control_samples", params.n_control_samples);
+  nhp.getParam("distance_increment", params.distance_increment);
+
+  nhp.getParam("smoothing_array_size", params.smoothing_array_size);
+  nhp.getParam("max_speed", params.max_speed);
+
+  std::string obstacle_cloud_topic;
+  nhp.getParam("input_cloud_topic", obstacle_cloud_topic);
+
+  planner.reset(new rr::planning::Planner(params));
+
+  auto map_sub = nh.subscribe(obstacle_cloud_topic, 1, mapCallback);
   speed_pub = nh.advertise<rr_platform::speed>("plan/speed", 1);
   steer_pub = nh.advertise<rr_platform::steering>("plan/steering", 1);
   path_pub = nh.advertise<nav_msgs::Path>("plan/path", 1);
 
-  auto planner = rr::planning::Planner();
-
-  ROS_INFO("planner initialized");
-
+  ROS_INFO("Planner initialized");
   ros::spin();
+
   return 0;
 }
