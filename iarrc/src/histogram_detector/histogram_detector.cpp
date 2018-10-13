ca5f478c32557adc8fd7b4b5fc6009eaf0b5cd7a
Btdubs <Brianco1998@gmail.com>
2018-10-13 00:16:10 -0400

    Added more modes for testing with launch params

--- iarrc/src/histogram_detector/histogram_detector.cpp
+++ iarrc/src/histogram_detector/histogram_detector.cpp
@@ -6,9 +6,24 @@
 #include <opencv2/highgui/highgui.hpp>
 
 ros::Publisher pub;
+ros::Publisher debug_pub;
 
+int hsv_select = 0; //0 = hue, 1 = sat, 2 = value
 
-int thresholdValue = 10;
+int threshold = 10;
+
+int threshold_only_value = 200;
+bool threshold_only = false;
+
+int rectangle_x = 500;
+int rectangle_y = 520;
+int rectangle_width = 200;
+int rectangle_height = 100;
+
+int bins = 255;//180; //360;
+
+cv::Mat output;
+
 
 cv::Mat calculateHistogram(cv::Mat image, int bins){
 	bool uniform = true; bool accumulate = false;
@@ -17,7 +32,7 @@ cv::Mat calculateHistogram(cv::Mat image, int bins){
 	float range[] = { 0, bins } ; //the upper boundary is exclusive. HUE from 0 - 360
 	const float* histRange = { range };
 	int channels [] = {0};
-
+	cv::Mat histogram;
 	cv::calcHist(&image,1,channels,cv::Mat(),histogram, 1 ,&histSize,&histRange,uniform,accumulate);
 	return histogram;
 }
@@ -33,50 +48,105 @@ void img_callback(const sensor_msgs::ImageConstPtr& msg) {
 	std::vector<cv::Mat> frame_hsv_planes;
 	cv::split(frame_hsv, frame_hsv_planes);
 
+	//blurring to assist in removing line from road mask...eh
+	//cv::GaussianBlur(frame_hsv_planes[2], frame_hsv_planes[2], cv::Size(9,9), 2.0);
 
-	int rectangle_x = 0;
-	int rectangle_y = 0;
-	int rectangle_width = 300;
-	int rectangle_height = 100;
-	cv::Mat image_rectangle_hue = frame_hsv_planes[0](cv::Rect(rectangle_x,rectangle_y,rectangle_width, rectangle_height));
+	if (!threshold_only) {
+		cv::Mat image_rectangle = frame_hsv_planes[hsv_select](cv::Rect(rectangle_x,rectangle_y,rectangle_width, rectangle_height));
 
-	cv::Mat road_histogram;
-	int bins = 360;
-	road_histogram = calculateHistogram(image_rectangle_hue, bins);
+		cv::Mat road_histogram;
 
-	cv::normalize(road_histogram, road_histogram, 0, 255, cv::NORM_MINMAX, -1, cv::Mat());
-	cv::Mat backProj;
-	float hue_range[] = { 0, bins };
-	const float* ranges = { hue_range }
-	cv::calcBackProject( &frame_hsv_planes[0], 1, 0, road_histogram, backProj, &ranges, 1, true );
+		road_histogram = calculateHistogram(image_rectangle, bins);
 
-	cv::Mat output = backProj;
-	//cv::Mat output;
-	//cv::threshold(backproj, output, thresholdValue, 255, cv::THRESH_BINARY);
+		cv::normalize(road_histogram, road_histogram, 0, 255, cv::NORM_MINMAX, -1, cv::Mat());
+		cv::Mat backProj;
+		float hue_range[] = { 0, bins };
+		const float* ranges = { hue_range };
+		cv::calcBackProject( &frame_hsv_planes[hsv_select], 1, 0, road_histogram, backProj, &ranges, 1, true );
 
-	//publish image
+		output = backProj;
+		cv::threshold(backProj, output, threshold, 255, cv::THRESH_BINARY);
+
+
+		//morphology to to close up holes
+		auto kernel0 = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(7,7));
+		cv::morphologyEx(output,output,cv::MORPH_CLOSE,kernel0);
+		//auto kernel1 = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(3,3));
+		//cv::morphologyEx(output, output, cv::MORPH_OPEN,kernal1);
+
+		//make white the lines
+		cv::bitwise_not(output,output);
+
+	} else {
+		//straight up value based thresholding
+		cv::threshold(frame_hsv_planes[2], output, threshold_only_value, 255, cv::THRESH_BINARY);
+		auto kernel_small = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(3,3));
+		cv::morphologyEx(output, output, cv::MORPH_OPEN,kernel_small);
+		//#TODO: will need some very small morphology openning to remove stray pixels, and proper body removal
+	}
+
+
+	//remove the body kinda
+	cv::rectangle(output,
+			cv::Point(390,650),
+			cv::Point(400 + 410,output.rows),
+			cv::Scalar(0,0,0),CV_FILLED);
+
+	//remove sky kinda
+	cv::rectangle(output,
+			cv::Point(0,0),
+			cv::Point(output.cols,output.rows / 3 + 100),
+			cv::Scalar(0,0,0),CV_FILLED);
+
+
+
+	//publish mask image
   sensor_msgs::Image outmsg;
   cv_ptr->image = output;
   cv_ptr->encoding = "mono8";
   cv_ptr->toImageMsg(outmsg);
 
   pub.publish(outmsg);
+
+
+
+	//publish image with rectangle for debugging
+	cv::rectangle(frame,
+			cv::Point(rectangle_x,rectangle_y),
+			cv::Point(rectangle_x + rectangle_width,rectangle_y+rectangle_height),
+			cv::Scalar(255,0,0),5);
+
+
+	sensor_msgs::Image outmsg2;
+	cv_ptr->image = frame;
+	cv_ptr->encoding = "bgr8";
+	cv_ptr->toImageMsg(outmsg2);
+
+	debug_pub.publish(outmsg2);
 }
 
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "road_detector");
+	ros::init(argc, argv, "histogram_detector");
 
 	ros::NodeHandle nh;
-	//ros::NodeHandle nhp("~");
-	//nhp.param("road_threshold_bootstrap", road_threshold_bootstrap);//, 0.5f);//#Defaults are done at the top. This should be changed and below uncommented #TODO
+	ros::NodeHandle nhp("~");
+	nhp.param("threshold", threshold, 10);
+	nhp.param("hsv_select", hsv_select, 0);
+	nhp.param("bins", bins, 255);
 
+	nhp.param("rectangle_x", rectangle_x, 500);
+	nhp.param("rectangle_y", rectangle_y, 520);
+	nhp.param("rectangle_width", rectangle_width, 200);
+	nhp.param("rectangle_height", rectangle_height, 100);
 
-
+	nhp.param("threshold_only", threshold_only, false);
+	nhp.param("threshold_only_value", threshold_only_value, 200);
 
 
   pub = nh.advertise<sensor_msgs::Image>("/histogram_detector", 1); //test publish of image
-	auto img_sub = nh.subscribe("/camera/image_rect", 1, img_callback);
+	debug_pub = nh.advertise<sensor_msgs::Image>("/histogram_debug", 1); //test publish of image
+	auto img_sub = nh.subscribe("/camera/image_color_rect_flipped", 1, img_callback);
 
 	ros::spin();
 	return 0;
