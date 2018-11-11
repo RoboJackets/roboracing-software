c649487153c910f61af520d7f4d56e1236341422
Evan Bretl <evanbretl4@gmail.com>
2018-11-11 17:39:41 -0500

    makes fake data slightly more difficult

--- rr_ml/nodes/semantic_segmentation/gen_fake_data.py
+++ rr_ml/nodes/semantic_segmentation/gen_fake_data.py
@@ -12,18 +12,36 @@ if "-n" in sys.argv:
 else:
     n_imgs = 100
 
+
+DIR = os.path.dirname(__file__)
+
+colors = [(100, 100, 100), (90, 100, 110), (100, 110, 90)]
+n_categories = len(colors)
+circle_size_range = (20, 50)
+
+local_noise_range = (-20, 20)
+global_noise_range = (-60, 60)
+
+
+def random_feature_circle(img, color):
+    max_r = circle_size_range[1]
+    rows, cols = img.shape[:2]
+    img = cv2.circle(img,
+               (random.randint(max_r, cols - max_r), random.randint(max_r, rows - max_r)),
+               random.randint(*circle_size_range),
+               color,
+               thickness=-1)
+    return img
+
+
 for n in range(n_imgs):
     img = np.zeros((180, 320, 3), dtype=np.uint8)
     rows, cols, channels = img.shape
-    n_categories = 3
-
-    colors = [(200, 100, 50), (50, 100, 200), (60, 90, 210)]
 
     img[:,:,:] = colors[0]
-    cv2.circle(img, (random.randint(50, 270), random.randint(50, 130)), random.randint(20, 50), colors[1],
-               thickness=-1)
-    cv2.circle(img, (random.randint(50, 270), random.randint(50, 130)), random.randint(20, 50), colors[2],
-               thickness=-1)
+
+    img = random_feature_circle(img, colors[1])
+    img = random_feature_circle(img, colors[2])
 
     def get_mask_for(color):
         mask = (img == color)  # mask 3D
@@ -34,14 +52,18 @@ for n in range(n_imgs):
     label[get_mask_for(colors[1])] = 1
     label[get_mask_for(colors[2])] = 2
 
-    img = (img + np.random.randint(-50, 50, img.shape)).astype(np.uint8)
+    local_noise_mat = np.random.randint(local_noise_range[0], local_noise_range[1], img.shape)
+    global_noise_mat = np.ones(img.shape, dtype=np.int16) * np.random.randint(*global_noise_range)
+    img = (img + local_noise_mat + global_noise_mat).astype(np.uint8)
 
-    if not os.path.exists("fake_data"):
-        os.makedirs("fake_data")
+    data_dir = os.path.join(DIR, "fake_data")
 
-    cv2.imwrite("fake_data/test_img_%d.png" % n, img)
-    cv2.imwrite("fake_data/test_label_%d.png" % n, label)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    cv2.imwrite(os.path.join(data_dir, "test_img_%d.png" % n), img)
+    cv2.imwrite(os.path.join(data_dir, "test_label_%d.png" % n), label)
 
     # cv2.imshow("data", img)
     # cv2.imshow("label", label * 50)
-    # cv2.waitKey(10)
+    # cv2.waitKey(0)
