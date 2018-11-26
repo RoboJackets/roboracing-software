033179ba67d125ade5ccb7a6e62b5677ada9f225
Evan Bretl <evanbretl4@gmail.com>
2018-11-26 00:26:46 -0500

    model can memorize a small but tricky iarrc lines dataset

--- rr_ml/nodes/semantic_segmentation/model_utils.py
+++ rr_ml/nodes/semantic_segmentation/model_utils.py
@@ -15,42 +15,46 @@ class UNetModelUtils(object):
 
     # significant credit to https://github.com/divamgupta/image-segmentation-keras
     def make_unet_model(self):
-        inputs = keras.layers.Input((self.in_height, self.in_width, self.n_channels))
+        L = keras.layers
 
-        conv1 = keras.layers.Conv2D(16, 3, activation='relu', padding='same')(inputs)
-        conv1 = keras.layers.Dropout(0.2)(conv1)
-        conv1 = keras.layers.Conv2D(16, 3, activation='relu', padding='same')(conv1)
+        input_layer = L.Input((self.in_height, self.in_width, self.n_channels))
 
-        pool1 = keras.layers.MaxPooling2D(pool_size=2)(conv1)
+        conv1 = L.Conv2D(16, 3, activation='relu', padding='same')(input_layer)
+        conv1 = L.Dropout(0.2)(conv1)
+        conv1 = L.Conv2D(16, 3, activation='relu', padding='same')(conv1)
 
-        conv2 = keras.layers.Conv2D(24, 3, activation='relu', padding='same')(pool1)
-        conv2 = keras.layers.Dropout(0.2)(conv2)
-        conv2 = keras.layers.Conv2D(24, 3, activation='relu', padding='same')(conv2)
+        pool1 = L.MaxPooling2D(pool_size=2)(conv1)
 
-        pool2 = keras.layers.MaxPooling2D(pool_size=2)(conv2)
+        conv2 = L.Conv2D(24, 3, activation='relu', padding='same')(pool1)
+        conv2 = L.Dropout(0.2)(conv2)
+        conv2 = L.Conv2D(24, 3, activation='relu', padding='same')(conv2)
 
-        conv3 = keras.layers.Conv2D(32, 3, activation='relu', padding='same')(pool2)
-        conv3 = keras.layers.Dropout(0.2)(conv3)
-        conv3 = keras.layers.Conv2D(32, 3, activation='relu', padding='same')(conv3)
+        pool2 = L.MaxPooling2D(pool_size=2)(conv2)
 
-        up1 = keras.layers.UpSampling2D(size=2)(conv3)
-        up1 = keras.layers.Concatenate(axis=3)([up1, conv2])
+        conv3 = L.Conv2D(32, 3, activation='relu', padding='same')(pool2)
+        conv3 = L.Dropout(0.2)(conv3)
+        conv3 = L.Conv2D(32, 3, activation='relu', padding='same')(conv3)
 
-        conv4 = keras.layers.Conv2D(24, 3, activation='relu', padding='same')(up1)
-        conv4 = keras.layers.Dropout(0.2)(conv4)
-        conv4 = keras.layers.Conv2D(24, 3, activation='relu', padding='same')(conv4)
+        up1 = L.UpSampling2D(size=2)(conv3)
+        up1 = L.Concatenate(axis=3)([up1, conv2])
 
-        up2 = keras.layers.UpSampling2D(size=2)(conv4)
-        up2 = keras.layers.Concatenate(axis=3)([up2, conv1])
+        conv4 = L.Conv2D(24, 3, activation='relu', padding='same')(up1)
+        conv4 = L.Dropout(0.2)(conv4)
+        conv4 = L.Conv2D(24, 3, activation='relu', padding='same')(conv4)
 
-        conv5 = keras.layers.Conv2D(16, 3, activation='relu', padding='same')(up2)
-        conv5 = keras.layers.Dropout(0.2)(conv5)
-        conv5 = keras.layers.Conv2D(16, 3, activation='relu', padding='same')(conv5)
+        up2 = L.UpSampling2D(size=2)(conv4)
+        up2 = L.Concatenate(axis=3)([up2, conv1])
 
-        conv6 = keras.layers.Conv2D(self.n_classes, 1, padding='same')(conv5)
-        conv7 = keras.layers.Activation('softmax')(conv6)
+        conv5 = L.Conv2D(16, 3, activation='relu', padding='same')(up2)
+        conv5 = L.Dropout(0.2)(conv5)
+        conv5 = L.Conv2D(16, 3, activation='relu', padding='same')(conv5)
 
-        model = keras.Model(inputs=inputs, outputs=conv7)
+        conv6 = L.Conv2D(self.n_classes, 1, padding='same')(conv5)
+        conv7 = L.Activation('softmax')(conv6)
+
+        # output = L.Reshape((self.in_width * self.in_height, self.n_classes))(conv6)
+
+        model = keras.Model(inputs=input_layer, outputs=conv7)
         return model
 
     def load_input(self, fname):
@@ -65,10 +69,12 @@ class UNetModelUtils(object):
             for c in range(img.shape[1]):
                 chan = img[r, c]
                 label_img[r, c, chan] = 1.0
-        return cv2.resize(label_img, (self.in_width, self.in_height))
+        label_img_rsz = cv2.resize(label_img, (self.in_width, self.in_height))
+        # return label_img_rsz.reshape(-1, self.n_classes)
+        return label_img_rsz
 
     def prediction_to_image(self, y):
-        colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0)]
+        colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0)]
         my = np.argmax(y, axis=-1)
         img = np.ndarray((self.in_height, self.in_width, 3), dtype=np.uint8)
         for i, color in enumerate(colors):
@@ -85,3 +91,12 @@ def IOU_score(y1, y2):
     total = np.sum(mask)
     right = np.sum(np.equal(argmax_img1, argmax_img2)[mask])
     return float(right) / total
+
+
+# def metric_max_uncommon(y_true, y_pred):
+#     K = keras.backend
+#     start = K.zeros_like(K.shape(y_pred))
+#     size = K.shape(y_pred)
+#     start[-1] = 1
+#     size[-1] -= 1
+#     return K.max(K.slice(y_pred, start, size))
--- rr_ml/nodes/semantic_segmentation/train.py
+++ rr_ml/nodes/semantic_segmentation/train.py
@@ -11,11 +11,16 @@ import rospy
 from . import model_utils
 
 osp = os.path
+K = keras.backend
 
-batch_size = 1
+batch_size = 5
 
 
 def data_gen(helper, input_files, label_files):
+    z = list(zip(input_files, label_files))
+    random.shuffle(z)
+    input_files, label_files = zip(*z)
+
     b = 0
     example_input = helper.load_input(input_files[0])
     example_label = helper.load_label(label_files[0])
@@ -38,6 +43,23 @@ def data_gen_wrapper(helper, input_files, label_files, epochs):
             yield x
 
 
+def weighted_focal_loss(gamma, non_background_weight):
+    def loss(target, output):
+        weights = [non_background_weight] * K.int_shape(output)[-1]
+        weights[0] = 1
+        weights = K.constant(weights)
+
+        output /= K.sum(output, axis=-1, keepdims=True)
+        eps = K.epsilon()
+        output = K.clip(output, eps, 1. - eps)
+        return -K.sum(K.pow(1. - output, gamma) * target * K.log(output) * weights, axis=-1)
+    return loss
+
+
+def max_output_non_background(y_true, y_pred):
+    return K.mean(K.max(y_pred[:,:,:,1:], axis=[1,2,3]))
+
+
 def main(acceptable_image_formats):
     rospy.init_node("train_segmentation")
 
@@ -57,24 +79,26 @@ def main(acceptable_image_formats):
 
     files = list(zip(input_files, label_files))
     random.shuffle(files)
-    split = int(len(input_files) * 0.75)
-    training_files = files[:split]
-    validate_files = files[split:]
+    # split = int(len(input_files) * 0.75)
+    # training_files = files[:split]
+    # validate_files = files[split:]
+    training_files = files[:]
+    validate_files = files[:]
 
     training_input_files, training_label_files = zip(*training_files)
     validate_input_files, validate_label_files = zip(*validate_files)
 
-    unet_helper = model_utils.UNetModelUtils(input_shape=(216, 384, 3), n_classes=3)
+    unet_helper = model_utils.UNetModelUtils(input_shape=(144, 256, 3), n_classes=4)
 
     try:
         model = keras.models.load_model(model_path)
         print "loaded model from disk"
     except IOError:
         model = unet_helper.make_unet_model()
-        model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
         print "no model found on disk, created a new one"
-        time.sleep(1.0)
 
+    model.compile(loss=weighted_focal_loss(2, 30), optimizer='adam', metrics=['accuracy', max_output_non_background])
+    time.sleep(2.0)
     model.summary()
 
     if not validate_only:
@@ -84,12 +108,12 @@ def main(acceptable_image_formats):
 
         if not osp.exists(osp.dirname(model_path)):
             os.makedirs(osp.dirname(model_path))
-        keras.models.save_model(model, model_path)
+        model.save(model_path, include_optimizer=False)
         print "saved to disk"
 
-        loss, accuracy = model.evaluate_generator(data_gen(unet_helper, validate_input_files, validate_label_files),
-                                                  steps=len(validate_files) // batch_size, verbose=1)
-        print "validation loss", loss, "accuracy", accuracy
+        metrics = model.evaluate_generator(data_gen(unet_helper, validate_input_files, validate_label_files),
+                                           steps=len(validate_files) // batch_size, verbose=1)
+        print "metrics", metrics
 
     scores = []
     pred_times = []
@@ -101,6 +125,9 @@ def main(acceptable_image_formats):
         y = model.predict(x[None])[0]
         t_pred = time.time() - t_pred_0
         pred_times.append(t_pred)
+
+        maxes = [np.max(y[...,i]) for i in range(y.shape[-1])]
+        print "max per category:", maxes
 
         iou_score = model_utils.IOU_score(y, y_)
         scores.append(iou_score)
