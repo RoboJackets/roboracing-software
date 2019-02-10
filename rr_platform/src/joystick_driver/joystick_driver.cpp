bd0f09b5b6d389e3735ea2dc2d28aa3cf253c9d3
RJ-RR-03 <support@robojackets.org>
2019-02-10 18:21:33 -0500

    changes to help test the reversing firmware

--- rr_platform/src/joystick_driver/joystick_driver.cpp
+++ rr_platform/src/joystick_driver/joystick_driver.cpp
@@ -11,9 +11,12 @@ double angle_max;
 double speed_max;
 
 void JoystickCB(const sensor_msgs::Joy::ConstPtr& msg) {
+    double input_forward = (1 - msg->axes[5]) / 2.;
+    double input_reverse = (1 - msg->axes[2]) / 2.;
+
     rr_platform::speed sp_cmd;
     rr_platform::steering st_cmd;
-    sp_cmd.speed = speed_max * ((-1*msg->axes[5] + 1.)/2.);
+    sp_cmd.speed = speed_max * (input_forward - input_reverse);
     st_cmd.angle = -(angle_max * msg->axes[0]);
 
     speed_publisher.publish(sp_cmd);
