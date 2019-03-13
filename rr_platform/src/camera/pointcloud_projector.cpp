f99c2b14a43cdc96b650d5d763c6d47ddb114110
Evan Bretl <evanbretl4@gmail.com>
2019-03-13 05:22:09 -0400

    nodelet-ifies the new pointcloud projector

--- rr_platform/src/camera/pointcloud_projector.cpp
+++ rr_platform/src/camera/pointcloud_projector.cpp
@@ -1,68 +1,78 @@
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <pluginlib/class_list_macros.h>
+#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+
 #include "CameraGeometry.h"
 
-rr::CameraGeometry cam_geom;
-ros::Publisher pointcloud_pub;
 
-void image_cb(const sensor_msgs::ImageConstPtr& msg) {
-  const cv::Mat& cv_img = cv_bridge::toCvCopy(msg, "mono8")->image;
-  pcl::PointCloud<pcl::PointXYZ> cloud;
+class PointCloudProjector : public nodelet::Nodelet {
+private:
+  rr::CameraGeometry cam_geom_;
+  ros::Publisher pointcloud_pub_;
+  image_transport::Subscriber detection_image_sub_;
 
-  for (int r = 0; r < cv_img.rows; r++) {
-    for (int c = 0; c < cv_img.cols; c++) {
-      if (cv_img.at<uint8_t>(r, c) > 0) {
-        auto [in_front, point] = cam_geom.ProjectToWorld(r, c);
-        if (in_front) {
-          cloud.push_back(pcl::PointXYZ(point.x, point.y, 0));
+
+  void ImageCallback(const sensor_msgs::ImageConstPtr& msg) {
+    const cv::Mat cv_img = cv_bridge::toCvShare(msg, "mono8")->image;
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    for (int r = 0; r < cv_img.rows; r++) {
+      for (int c = 0; c < cv_img.cols; c++) {
+        if (cv_img.at<uint8_t>(r, c) > 0) {
+          auto [in_front, point] = cam_geom_.ProjectToWorld(r, c);
+          if (in_front) {
+            cloud.push_back(pcl::PointXYZ(point.x, point.y, 0));
+          }
         }
       }
     }
+
+    sensor_msgs::PointCloud2Ptr out(new sensor_msgs::PointCloud2);
+    pcl::toROSMsg(cloud, *out);
+    out->header.frame_id = "base_footprint";
+    pointcloud_pub_.publish(out);
   }
 
-  sensor_msgs::PointCloud2 out;
-  pcl::toROSMsg(cloud, out);
-  out.header.frame_id = "base_footprint";
-  pointcloud_pub.publish(out);
-}
+  void onInit() override {
+    auto node_handle = getNodeHandle();
+    auto nh_private = getPrivateNodeHandle();
 
+    image_transport::ImageTransport image_transport(node_handle);
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "image_transform_test");
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_private("~");
+    std::string camera_info_topic;
+    std::string camera_tf_frame;
+    std::string image_topic_in;
+    std::string pointcloud_topic_out;
+    double load_timeout;
 
-  std::string camera_info_topic;
-  std::string camera_tf_frame;
-  std::string image_topic_in;
-  std::string pointcloud_topic_out;
-  double load_timeout;
+    bool all_defined = true;
+    all_defined &= nh_private.getParam("camera_info_topic", camera_info_topic);
+    all_defined &= nh_private.getParam("camera_tf_frame", camera_tf_frame);
+    all_defined &= nh_private.getParam("image_topic_in", image_topic_in);
+    all_defined &= nh_private.getParam("pointcloud_topic_out", pointcloud_topic_out);
+    all_defined &= nh_private.getParam("load_timeout", load_timeout);
 
-  bool all_defined = true;
-  all_defined &= nh_private.getParam("camera_info_topic", camera_info_topic);
-  all_defined &= nh_private.getParam("camera_tf_frame", camera_tf_frame);
-  all_defined &= nh_private.getParam("image_topic_in", image_topic_in);
-  all_defined &= nh_private.getParam("pointcloud_topic_out", pointcloud_topic_out);
-  all_defined &= nh_private.getParam("load_timeout", load_timeout);
+    if (!all_defined) {
+      NODELET_WARN("Pointcloud projector is missing roslaunch params");
+    }
 
-  if (!all_defined) {
-    ROS_WARN("Pointcloud projector is missing roslaunch params");
+    // load camera geometry
+    cam_geom_.LoadInfo(node_handle, camera_info_topic, camera_tf_frame, load_timeout);
+
+    detection_image_sub_ = image_transport.subscribe(image_topic_in, 1, &PointCloudProjector::ImageCallback, this);
+
+    pointcloud_pub_ = node_handle.advertise<sensor_msgs::PointCloud2>(pointcloud_topic_out, 1);
+
+    NODELET_INFO("Loaded pointcloud projector nodelet");
   }
+};
 
-  // load camera geometry
-  cam_geom.LoadInfo(nh, camera_info_topic, camera_tf_frame, load_timeout);
-
-  auto sub = nh.subscribe(image_topic_in, 1, image_cb);
-
-  pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/test_cloud", 1);
-
-  ros::spin();
-
-  return 0;
-}
+PLUGINLIB_EXPORT_CLASS(PointCloudProjector, nodelet::Nodelet);
