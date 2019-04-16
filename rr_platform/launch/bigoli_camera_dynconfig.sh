62ce9ec64db7022337ec3519a2e408c81c65eb24
Evan Bretl <evanbretl4@gmail.com>
2019-04-15 22:30:45 -0400

    fixes Sedani launch files

--- rr_platform/launch/bigoli_camera_dynconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# There is no sleeping/waiting in launch files, so
-# unfortunately we're left with this. The dynamic
-# reconfigure settings should be applied after all
-# the defaults have been applied
-
-sleep 10
-roslaunch rr_platform bigoli_camera_dynconfig.launch
