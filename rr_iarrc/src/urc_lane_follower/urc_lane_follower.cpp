bff3d95931872ff66deb57b34a6f5fd2274e970d
Brian Cochran <brianco1998@gmail.com>
2019-06-23 19:45:43 -0400

    adds weird steer control

--- rr_iarrc/src/urc_lane_follower/urc_lane_follower.cpp
+++ rr_iarrc/src/urc_lane_follower/urc_lane_follower.cpp
@@ -10,12 +10,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <rr_platform/speed.h>
+#include <rr_platform/steering.h>
+
 using namespace std;
 
 cv_bridge::CvImagePtr cv_ptr;
 ros::Publisher image_pub;
+ros::Publisher speed_pub;
+ros::Publisher steer_pub;
 
-cv::Mat kernel(int x, int y);
+rr_platform::speed speed_message;
+rr_platform::steering steer_message;
+
 
 cv::Mat mergeImagesSideBySide(cv::Mat leftImg, cv::Mat rightImg) {
     int outputRows = leftImg.rows + rightImg.rows;
@@ -29,8 +36,8 @@ cv::Mat mergeImagesSideBySide(cv::Mat leftImg, cv::Mat rightImg) {
     return merged;
 }
 
-
-double calcLineAngle(cv::Mat &binaryImg) {
+//Outputs angle in radians
+double calcLineAngle(cv::Mat &binaryImg, cv::Mat &colorImg) {
     cv::Mat edges;
     int ddepth = CV_8UC1;
     cv::Laplacian(binaryImg, edges, ddepth); //use edges to get better Hough results
@@ -54,10 +61,13 @@ double calcLineAngle(cv::Mat &binaryImg) {
         cv::Point p1(l[0], l[1]);
         cv::Point p2(l[2], l[3]);
 
+        //debug
+        cv::line( colorImg, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+
         //calc angle and decide if it is a stop bar
         double dx = p1.x - p2.x;
         double dy = p1.y - p2.y;
-        double currAngle = std::fabs(atan2(dy, dx)) * 180/CV_PI;//in degrees
+        double currAngle = std::fabs(atan2(dy, dx)); //in radians
 
         double currLength = std::sqrt(std::pow(dx, 2) + std::pow(dy, 2));
         totalWeight += currLength;
@@ -66,6 +76,20 @@ double calcLineAngle(cv::Mat &binaryImg) {
     weightedAngle /= totalWeight; //handle divide by zero (no lines in image)
 
     return weightedAngle;
+}
+
+
+cv::Mat findLineBinary(cv::Mat image) {
+    cv::Mat gray;
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    int ddepth = CV_8UC1;
+    cv::Laplacian(gray, gray, ddepth); convertScaleAbs( gray, gray );
+    int thresh = 2;//25;
+    cv::threshold( gray, gray, thresh, 255, cv::THRESH_BINARY );
+    //cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(15,15));
+    cv::morphologyEx(gray, gray, cv::MORPH_CLOSE, kernel );
+    return gray;
 }
 
 
@@ -81,52 +105,50 @@ double calcLineAngle(cv::Mat &binaryImg) {
  */
 void img_callback(const sensor_msgs::ImageConstPtr& leftMsg, const sensor_msgs::ImageConstPtr& rightMsg) {
     //Convert msg to Mat image
-    cv_ptr = cv_bridge::toCvCopy(leftMsg, "mono8");
+    cv_ptr = cv_bridge::toCvCopy(leftMsg, "bgr8");
     cv::Mat leftFrame = cv_ptr->image;
-    cv_ptr = cv_bridge::toCvCopy(rightMsg, "mono8");
+    cv_ptr = cv_bridge::toCvCopy(rightMsg, "bgr8");
     cv::Mat rightFrame = cv_ptr->image;
 
-    //cv::threshold( leftFrame, leftFrame, 130, 255, cv::THRESH_BINARY );
-    //cv::threshold( rightFrame, rightFrame, 130, 255, cv::THRESH_BINARY );
-    //cv::morphologyEx()
-    //cv::Canny(leftFrame, leftFrame, 20, 100);
-    //cv::Canny(rightFrame, rightFrame, 20, 100);
-    int ddepth = CV_8UC1;
-    cv::Laplacian(leftFrame, leftFrame, ddepth); convertScaleAbs( leftFrame, leftFrame );
-    cv::Laplacian(rightFrame, rightFrame, ddepth); convertScaleAbs( rightFrame, rightFrame );
-    cv::threshold( rightFrame, rightFrame, 25, 255, cv::THRESH_BINARY );
-    cv::threshold( leftFrame, leftFrame, 25, 255, cv::THRESH_BINARY );
-    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
-    cv::morphologyEx(rightFrame, rightFrame, cv::MORPH_CLOSE, kernel );
-    cv::morphologyEx(leftFrame, leftFrame, cv::MORPH_CLOSE, kernel );
+    cv::rotate(leftFrame, leftFrame, cv::ROTATE_90_COUNTERCLOCKWISE);
+    cv::rotate(rightFrame, rightFrame, cv::ROTATE_90_CLOCKWISE);
+    const double angleOffset = CV_PI / 2; //since it is 90 degrees rotated
 
+    cv::Mat leftLine = findLineBinary(leftFrame);
+    cv::Mat rightLine = findLineBinary(rightFrame);
 
-    double leftAngle = calcLineAngle(leftFrame);
-    double rightAngle = calcLineAngle(rightFrame);
-    
+    double leftAngle = calcLineAngle(leftLine, leftFrame);
+    double rightAngle = calcLineAngle(rightLine, rightFrame);
+
+    double errorTolerance = 16.0; //allowable angle difference
+    if (leftAngle - rightAngle > errorTolerance) {
+        ROS_WARN_STREAM("URC Lane Follower: Angle mismatch");
+    }
+
+    double angleAvg = (leftAngle + rightAngle) / 2; //#TODO: change to radians
+    double kP = 0.01;
+    double goal = 1.0;
+    double steering = ( goal - (angleAvg - angleOffset) ) * kP;
+    double speed = 1.0;
+
+    auto now = ros::Time::now();
+
+    speed_message.speed = speed;
+    speed_message.header.stamp = now;
+
+    steer_message.angle = steering;
+    steer_message.header.stamp = now;
+
+    speed_pub.publish(speed_message);
+    steer_pub.publish(steer_message);
+
     //debugging stuff
     cv::Mat debug = mergeImagesSideBySide(leftFrame, rightFrame);
-    cv::cvtColor(debug, debug, cv::COLOR_GRAY2BGR);
 
-    //##################
-    std::vector<cv::Vec4i> lines; // will hold the results of the detection
-    double rho = 1; //distance resolution
-    double theta = CV_PI/180; //angular resolution (in radians) pi/180 is one degree res
-    int threshold = 10;
-    double minLineLength = 0;
-    double maxLineGap = 6;
-
-    cv::HoughLinesP(leftFrame, lines, rho, theta, threshold, minLineLength, maxLineGap );
-    for( size_t i = 0; i < lines.size(); i++ )
-      {
-        cv::Vec4i l = lines[i];
-        cv::line( debug, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
-      }
-
-    //#################
- 
-    cv::putText(debug, to_string(leftAngle), cv::Point(20,100), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,0,255), 2);
-    cv::putText(debug, to_string(rightAngle), cv::Point(20,150), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,0,255), 2);
+    cv::putText(debug, "Left: " + to_string(leftAngle), cv::Point(20,100), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,255,0), 2);
+    cv::putText(debug, "Right: " + to_string(rightAngle), cv::Point(20,150), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,255,0), 2);
+    cv::putText(debug, "Avg: " + to_string(angleAvg), cv::Point(20,200), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,255,0), 2);
+    cv::putText(debug, "Steer: " + to_string(steering), cv::Point(20,250), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar(0,255,0), 2);
 
 
     if (image_pub.getNumSubscribers() > 0) {
@@ -143,15 +165,19 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle nh;
     ros::NodeHandle nhp("~");
-    std::string subscription_node;
-    //nhp.param("perfect_lines_min_cut", perfect_lines_min_cut, 200);
-    //nhp.param("subscription_node", subscription_node, std::string("/camera/image_color_rect"));
+    std::string leftCamera_sub_name;
+    std::string rightCamera_sub_name;
+    nhp.param("camera_left_subscription", leftCamera_sub_name, std::string("/camera_left/image_color_rect"));
+    nhp.param("camera_right_subscription", rightCamera_sub_name, std::string("/camera_right/image_color_rect"));
+
 
     image_pub = nh.advertise<sensor_msgs::Image>("/urc_lane_follower", 1); //publish debug image
+    speed_pub = nh.advertise<rr_platform::speed>("plan/speed", 1);
+    steer_pub = nh.advertise<rr_platform::steering>("plan/steering", 1);
 
 
-    message_filters::Subscriber<sensor_msgs::Image> leftCamera_sub(nh, "/camera_left/image_mono_rect", 1);
-    message_filters::Subscriber<sensor_msgs::Image> rightCamera_sub(nh, "/camera_right/image_mono_rect", 1);
+    message_filters::Subscriber<sensor_msgs::Image> leftCamera_sub(nh, leftCamera_sub_name, 1);
+    message_filters::Subscriber<sensor_msgs::Image> rightCamera_sub(nh, rightCamera_sub_name, 1);
 
     typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
     // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10) //#TODO: change?
@@ -160,8 +186,4 @@ int main(int argc, char** argv) {
 
     ros::spin();
     return 0;
-}
-
-cv::Mat kernel(int x, int y) {
-    return cv::getStructuringElement(cv::MORPH_RECT,cv::Size(x,y));
 }
