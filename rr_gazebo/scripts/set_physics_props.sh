19f464b94a2903740f7b96fb479d066763f422f4
Evan Bretl <evanbretl4@gmail.com>
2019-09-03 00:57:55 -0400

    undo macaroni physics props changes

--- rr_gazebo/scripts/set_physics_props.sh
+++ rr_gazebo/scripts/set_physics_props.sh
@@ -3,4 +3,4 @@
 # set gazebo physics step size so that it uses less CPU resources
 
 sleep 5
-gz physics --step-size 0.0015 --update-rate 667 --iters 30
+gz physics --step-size 0.0025 --update-rate 400 --iters 30
