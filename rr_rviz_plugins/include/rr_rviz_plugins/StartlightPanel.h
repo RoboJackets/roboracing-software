1f83601c0afe0c4023d70f7f266032047f4918a0
NicoBartholomai <nfbartholomai@gmail.com>
2019-11-17 17:57:49 -0500

    StartlightPanel no longer breakes on runtime.

--- rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
+++ rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
@@ -1,28 +1,62 @@
 //
 // Created by nico on 11/3/19.
 //
+//
+//#ifndef CATKIN_WS_STARTLIGHTPANEL_H
+//#define CATKIN_WS_STARTLIGHTPANEL_H
+//
+//#include <ros/ros.h>
+//#include <rviz/panel.h>
+//#include <std_msgs/Bool.h>
+//#include <rr_msgs/speed.h>
+//#include <QLabel> //<-- not sure about this one
+//
+//namespace rr_rviz_plugins {
+//    class StartlightPanel : public rviz::Panel {
+//        Q_OBJECT
+//    public:
+//        StartlightPanel(QWidget *parent = 0);
+//
+//    private:
+//        ros::Subscriber start_detector;
+//        void paintEvent(QPaintEvent *event) override;
+//        static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/);
+//        void speed_callback(const rr_msgs::speedConstPtr &msg, QLabel *label);
+//    };
+//}
 
 #ifndef CATKIN_WS_STARTLIGHTPANEL_H
 #define CATKIN_WS_STARTLIGHTPANEL_H
 
 #include <ros/ros.h>
+#include <rr_msgs/race_reset.h>
 #include <rviz/panel.h>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QtWidgets/QLabel>
 #include <std_msgs/Bool.h>
-#include <rr_msgs/speed.h>
-#include <QLabel> //<-- not sure about this one
 
+/*
+ * All of our panels need to be under the rr_rviz_plugins namespace.
+ */
 namespace rr_rviz_plugins {
+
     class StartlightPanel : public rviz::Panel {
-        Q_OBJECT
+    Q_OBJECT
     public:
         StartlightPanel(QWidget *parent = 0);
 
-    private:
+    protected:
+        QPushButton *reset_btn;
+        ros::NodeHandle nh;
+        ros::Publisher reset_pub;
         ros::Subscriber start_detector;
-        void paintEvent(QPaintEvent *event) override;
-        static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/);
-        void speed_callback(const rr_msgs::speedConstPtr &msg, QLabel *label);
+
+    private slots:
+//        void resetCallback();
+        static void startlightCallback(std_msgs::Bool msg);
     };
-}
+
+}  // namespace rr_rviz_plugins
 
 #endif //CATKIN_WS_STARTLIGHTPANEL_H
--- rr_rviz_plugins/src/StartlightPanel.cpp
+++ rr_rviz_plugins/src/StartlightPanel.cpp
@@ -1,51 +1,110 @@
 //
 // Created by nico on 11/3/19.
 //
+//#include <pluginlib/class_list_macros.h>
+//#include <rr_rviz_plugins/StartlightPanel.h>
+//#include <rr_rviz_plugins/ExamplePanel.h>
+//#include <std_msgs/Bool.h>
+//#include <QVBoxLayout>
+//#include <QPainter>
+//
+//namespace rr_rviz_plugins {
+//    static bool isGreen = false;
+//    static bool recievedSignal = false;
+//    StartlightPanel::StartlightPanel(QWidget *parent)
+//    : rviz::Panel(parent) {
+//
+//        ros::NodeHandle nh;
+//        auto *label = new QLabel("No Message");
+//        start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, startlight_callback);
+//
+//        //Creates painter to make the circle
+//        QPainter painter;
+//        //Makes the pen that the painter uses
+//        QPen myPen;
+//
+//        //sets the color of the pen
+//        if (isGreen) {
+//            myPen.setColor(Qt::green);
+//        } else {
+//            myPen.setColor(Qt::red);
+//        }
+//        if (!recievedSignal) {
+//            myPen.setColor(Qt::gray);
+//        }
+//
+//        //makes a layout
+//        QVBoxLayout *layout = new QVBoxLayout;
+//        //draws the circle
+//        painter.drawEllipse(0, 0, 100, 100);
+//        setLayout(layout);
+//    }
+//    static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/){
+//        //auto text = std::to_string(msg.data);
+//        //label->setText(text.c_str());
+//        recievedSignal = true;
+//        isGreen = msg.data != 0;
+//    }
+////This is literally just example panel
+//} // end of rr_rviz_plugins
+
 #include <pluginlib/class_list_macros.h>
 #include <rr_rviz_plugins/StartlightPanel.h>
-#include <rr_rviz_plugins/ExamplePanel.h>
 #include <std_msgs/Bool.h>
 #include <QVBoxLayout>
-#include <QPainter>
+#include <QtWidgets/QLabel>
+#include <QtGui/QPainter>
 
 namespace rr_rviz_plugins {
-    static bool isGreen = false;
-    static bool recievedSignal = false;
-    StartlightPanel::StartlightPanel(QWidget *parent)
-    : rviz::Panel(parent) {
 
-        ros::NodeHandle nh;
+    bool isGreen = false;
+    bool receivedSignal = false;
+    StartlightPanel::StartlightPanel(QWidget *parent)
+            : rviz::Panel(parent)  // Base class constructor
+    {
+//        reset_btn = new QPushButton("Reset!");
+//        reset_pub = nh.advertise<rr_msgs::race_reset>("/reset_detected", 0);
+//        connect(reset_btn, SIGNAL(released()), this, SLOT(resetCallback()));
         auto *label = new QLabel("No Message");
-        start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, startlight_callback);
+        start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, startlightCallback);
 
         //Creates painter to make the circle
         QPainter painter;
-        //Makes the pen that the painter uses
-        QPen myPen;
 
-        //sets the color of the pen
+        //Makes the pen that the painter uses
+        QPen pen;
+
+        //Sets the color of the pen
         if (isGreen) {
-            myPen.setColor(Qt::green);
+            pen.setColor(Qt::green);
         } else {
-            myPen.setColor(Qt::red);
+            pen.setColor(Qt::red);
         }
-        if (!recievedSignal) {
-            myPen.setColor(Qt::gray);
+        if(!receivedSignal) {
+            pen.setColor(Qt::gray);
         }
 
         //makes a layout
         QVBoxLayout *layout = new QVBoxLayout;
+
         //draws the circle
         painter.drawEllipse(0, 0, 100, 100);
+//        layout->addWidget(reset_btn);
+        layout->addWidget(label);
         setLayout(layout);
     }
-    static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/){
-        //auto text = std::to_string(msg.data);
-        //label->setText(text.c_str());
-        recievedSignal = true;
+
+//    void StartlightPanel::resetCallback() {
+//        rr_msgs::race_reset reset;
+//        reset_pub.publish(reset);
+//    }
+
+    void StartlightPanel::startlightCallback(const std_msgs::Bool msg){
+//        auto text = std::to_string(msg.data);
+//        label->setText(text.c_str());
+        receivedSignal = true;
         isGreen = msg.data != 0;
     }
-//This is literally just example panel
-} // end of rr_rviz_plugins
+}  // namespace rr_rviz_plugins
 
-PLUGINLIB_EXPORT_CLASS(rr_rviz_plugins::StartlightPanel, rviz::Panel);
+PLUGINLIB_EXPORT_CLASS(rr_rviz_plugins::StartlightPanel, rviz::Panel)
