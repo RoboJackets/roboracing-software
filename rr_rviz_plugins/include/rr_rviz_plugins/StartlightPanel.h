ae26ef99caa8809243ac15ff386ba6ed07b8a295
NicoBartholomai <nfbartholomai@gmail.com>
2019-11-23 15:38:56 -0500

    cleaned up some comments and changed the position and size of the circle

--- rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
+++ rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
@@ -1,30 +1,3 @@
-//
-// Created by nico on 11/3/19.
-//
-//
-//#ifndef CATKIN_WS_STARTLIGHTPANEL_H
-//#define CATKIN_WS_STARTLIGHTPANEL_H
-//
-//#include <ros/ros.h>
-//#include <rviz/panel.h>
-//#include <std_msgs/Bool.h>
-//#include <rr_msgs/speed.h>
-//#include <QLabel> //<-- not sure about this one
-//
-//namespace rr_rviz_plugins {
-//    class StartlightPanel : public rviz::Panel {
-//        Q_OBJECT
-//    public:
-//        StartlightPanel(QWidget *parent = 0);
-//
-//    private:
-//        ros::Subscriber start_detector;
-//        void paintEvent(QPaintEvent *event) override;
-//        static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/);
-//        void speed_callback(const rr_msgs::speedConstPtr &msg, QLabel *label);
-//    };
-//}
-
 #ifndef CATKIN_WS_STARTLIGHTPANEL_H
 #define CATKIN_WS_STARTLIGHTPANEL_H
 
@@ -37,24 +10,19 @@
 #include <std_msgs/Bool.h>
 #include <QtGui/QPaintEvent>
 
-/*
- * All of our panels need to be under the rr_rviz_plugins namespace.
- */
 namespace rr_rviz_plugins {
 
     class StartlightPanel : public rviz::Panel {
     Q_OBJECT
     public:
-        StartlightPanel(QWidget *parent = 0);
+        explicit StartlightPanel(QWidget *parent = 0);
 
     protected:
-//        QPushButton *reset_bpainter.setBrush(Qt::green);tn{};
         ros::NodeHandle nh;
         ros::Publisher reset_pub;
         ros::Subscriber start_detector;
 
     private slots:
-//        void resetCallback();
         static void startlightCallback(std_msgs::Bool msg);
         void paintEvent(QPaintEvent *e);
     };
--- rr_rviz_plugins/src/StartlightPanel.cpp
+++ rr_rviz_plugins/src/StartlightPanel.cpp
@@ -1,52 +1,6 @@
 //
 // Created by nico on 11/3/19.
 //
-//#include <pluginlib/class_list_macros.h>
-//#include <rr_rviz_plugins/StartlightPanel.h>
-//#include <rr_rviz_plugins/ExamplePanel.h>
-//#include <std_msgs/Bool.h>
-//#include <QVBoxLayout>
-//#include <QPainter>
-//
-//namespace rr_rviz_plugins {
-//    static bool isGreen = false;
-//    static bool recievedSignal = false;
-//    StartlightPanel::StartlightPanel(QWidget *parent)
-//    : rviz::Panel(parent) {
-//
-//        ros::NodeHandle nh;
-//        auto *label = new QLabel("No Message");
-//        start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, startlight_callback);
-//
-//        //Creates painter to make the circle
-//        QPainter painter;
-//        //Makes the pen that the painter uses
-//        QPen myPen;
-//
-//        //sets the color of the pen
-//        if (isGreen) {
-//            myPen.setColor(Qt::green);
-//        } else {
-//            myPen.setColor(Qt::red);
-//        }
-//        if (!recievedSignal) {
-//            myPen.setColor(Qt::gray);
-//        }
-//
-//        //makes a layout
-//        QVBoxLayout *layout = new QVBoxLayout;
-//        //draws the circle
-//        painter.drawEllipse(0, 0, 100, 100);
-//        setLayout(layout);
-//    }
-//    static void startlight_callback(const std_msgs::Bool msg/*, QLabel *label*/){
-//        //auto text = std::to_string(msg.data);
-//        //label->setText(text.c_str());
-//        recievedSignal = true;
-//        isGreen = msg.data != 0;
-//    }
-////This is literally just example panel
-//} // end of rr_rviz_plugins
 #include <pluginlib/class_list_macros.h>
 #include <rr_rviz_plugins/StartlightPanel.h>
 #include <std_msgs/Bool.h>
@@ -59,43 +13,20 @@ namespace rr_rviz_plugins {
     bool isGreen = false;
     bool receivedSignal = false;
     QLabel *label;
-    static QRect *rectangle = new QRect(0, 0, 100, 100);
     StartlightPanel::StartlightPanel(QWidget *parent)
             : rviz::Panel(parent)  // Base class constructor
     {
-//        reset_btn = new QPushButton("Reset!");
-//        reset_pub = nh.advertise<rr_msgs::race_reset>("/reset_detected", 0);
-//        connect(reset_btn, SIGNAL(released()), this, SLOT(resetCallback()));
-        label = new QLabel("No Message");
+        label = new QLabel("        No Message");
+        label->setMinimumHeight(30);
+        label->setMinimumWidth(130);
         start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, startlightCallback);
-
-        //Creates painter to make the circle
-////        QPaintEvent screen;
-//        QPainter painter;
-//
-//        //Makes the pen that the painter uses
-//        QPen pen;
-//
-//        //Sets the color of the pen
-//        if (isGreen) {
-//            pen.setColor(Qt::green);
-//        } else {
-//            pen.setColor(Qt::red);
-//        }
-//        if(!receivedSignal) {
-//            pen.setColor(Qt::gray);
-//        }
-//
-//        //makes a layout
         auto *layout = new QVBoxLayout;
         QWidget::repaint();
-//
-//        //draws the circle
-//        painter.drawEllipse(0, 0, 10, 10);
-//        layout->addWidget(reset_btn);
+
         layout->addWidget(label);
         setLayout(layout);
     }
+    //draws the circle
     void StartlightPanel::paintEvent(QPaintEvent *e) {
         QPainter painter(this);
         if (isGreen) {
@@ -109,23 +40,18 @@ namespace rr_rviz_plugins {
             painter.setPen(Qt::gray);
             painter.setBrush(Qt::gray);
         }
-        painter.drawEllipse(6, 6, 20, 20);
+        int radius = 21;
+        painter.drawEllipse(16, 12, radius, radius);
         QWidget::paintEvent(e);
     }
-//    void StartlightPanel::resetCallback() {
-//        rr_msgs::race_reset reset;
-//        reset_pub.publish(reset);
-//    }
 
     void StartlightPanel::startlightCallback(const std_msgs::Bool msg){
-//        auto text = std::to_string(msg.data);
-//        label->setText(text.c_str());
         receivedSignal = true;
         isGreen = msg.data != 0;
         if (isGreen) {
-            label->setText("Go!");
+            label->setText("        Go!");
         } else {
-            label->setText("Stop!");
+            label->setText("        Stop!");
         }
     }
 }  // namespace rr_rviz_plugins
