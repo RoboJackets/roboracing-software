7da639ed2d60e0f3da3380b939deee9cb5352f6b
Eric <elriinc@gmail.com>
2019-12-02 21:01:29 -0500

    added box visualization and changed launch variables to arg

--- rr_common/src/follower/follower.h
+++ rr_common/src/follower/follower.h
@@ -11,7 +11,9 @@
 #include <std_msgs/Float64.h>
 #include <string>
 #include "flann/flann.hpp"
-
+#include <geometry_msgs/PolygonStamped.h>
+#include <geometry_msgs/Point32.h>
+#include <vector>
 float MIN_FRONT_VISION;
 float MAX_FRONT_VISION;
 float GOAL_DIST;
@@ -24,6 +26,7 @@ std::string topic_from_plant;
 std::string setpoint_topic;
 std::string topic_from_controller;
 
-ros::Publisher speed_pub, steer_pub, pid_speed_pub, pid_setpoint_pub;
+
+ros::Publisher speed_pub, steer_pub, pid_speed_pub, pid_setpoint_pub, visonBox_pub;
 
 #endif  // RR_COMMON_FOLLOWER_H
