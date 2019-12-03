ed7c8f178d76d66ee9bf26295c536c2725ea8f0d
NicoBartholomai <nfbartholomai@gmail.com>
2019-12-02 20:51:42 -0500

    Added another circle and made it look like a stoplight

--- rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
+++ rr_rviz_plugins/include/rr_rviz_plugins/StartlightPanel.h
@@ -4,30 +4,29 @@
 #include <ros/ros.h>
 #include <rr_msgs/race_reset.h>
 #include <rviz/panel.h>
+#include <std_msgs/Bool.h>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QtWidgets/QLabel>
-#include <std_msgs/Bool.h>
 #include <QtGui/QPaintEvent>
+#include <QtWidgets/QLabel>
 
 namespace rr_rviz_plugins {
 
-    class StartlightPanel : public rviz::Panel {
+class StartlightPanel : public rviz::Panel {
     Q_OBJECT
-    public:
-        explicit StartlightPanel(QWidget *parent = nullptr);
+  public:
+    explicit StartlightPanel(QWidget *parent = nullptr);
 
-    protected:
-        ros::NodeHandle nh;
-        ros::Subscriber start_detector;
+  protected:
+    ros::NodeHandle nh;
+    ros::Subscriber start_detector;
 
-    private slots:
-//        void startlightCallback(const std_msgs::BoolConstPtr &msg, QWidget *parent);
-        static void startlightCallback(const std_msgs::Bool msg);
-        static void timerCallback(const ros::TimerEvent& e);
-        void paintEvent(QPaintEvent *e) override;
-    };
+  private slots:
+    static void startlightCallback(const std_msgs::Bool msg);
+    static void timerCallback(const ros::TimerEvent &e);
+    void paintEvent(QPaintEvent *e) override;
+};
 
 }  // namespace rr_rviz_plugins
 
-#endif //CATKIN_WS_STARTLIGHTPANEL_H
+#endif  // CATKIN_WS_STARTLIGHTPANEL_H
--- rr_rviz_plugins/src/StartlightPanel.cpp
+++ rr_rviz_plugins/src/StartlightPanel.cpp
@@ -5,75 +5,92 @@
 #include <rr_rviz_plugins/StartlightPanel.h>
 #include <std_msgs/Bool.h>
 #include <QVBoxLayout>
-#include <QtWidgets/QLabel>
-#include <QtGui/QPainter>
 #include <QtGui/QPaintEvent>
+#include <QtGui/QPainter>
+#include <QtWidgets/QLabel>
 
 namespace rr_rviz_plugins {
-    bool isGreen = false;
-    bool receivedSignal = false;
-    QLabel *label;
-    ros::Time current;
-    ros::Time callbackTime = ros::Time::now();
-    ros::Timer timer;
-    StartlightPanel::StartlightPanel(QWidget *parent)
-            : rviz::Panel(parent)  // Base class constructor
-    {
-        label = new QLabel("        No Message");
-        label->setFixedHeight(30);
-        label->setFixedHeight(30);
-        label->setGeometry(QRect(0, 0, 30, 30));
-        start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1,
-                StartlightPanel::startlightCallback);
-        timer = nh.createTimer(ros::Duration(1), StartlightPanel::timerCallback);
-        auto *layout = new QVBoxLayout;
-        layout->addWidget(label);
-        setLayout(layout);
+bool isGreen = false;
+bool receivedSignal = false;
+QLabel *label;
+ros::Time current;
+ros::Time callbackTime = ros::Time::now();
+ros::Timer timer;
+StartlightPanel::StartlightPanel(QWidget *parent)
+      : rviz::Panel(parent)  // Base class constructor
+{
+    auto *layout = new QVBoxLayout;
+    label = new QLabel("              No Message");
+    label->setGeometry(QRect(80, 0, 30, 30));
+    start_detector = nh.subscribe<std_msgs::Bool>("/start_detected", 1, StartlightPanel::startlightCallback);
+    timer = nh.createTimer(ros::Duration(1), StartlightPanel::timerCallback);
+    layout->addWidget(label);
+    setLayout(layout);
+    this->setFixedHeight(50);
+}
+
+// draws the stoplight
+void StartlightPanel::paintEvent(QPaintEvent *e) {
+    int radius = 21;
+    int posX = 16;
+    int posY = 12;
+
+    // Background rectangle
+    QPainter painter1(this);
+    painter1.setBrush(Qt::black);
+    painter1.setPen(Qt::black);
+    painter1.drawRect(posX - 1, posY - 1, 2 * radius + 5, radius + 2);
+
+    // Left circle
+    QPainter painter(this);
+    if (isGreen) {
+        painter.setPen(Qt::green);
+        painter.setBrush(Qt::green);
+    } else {
+        painter.setPen(Qt::gray);
+        painter.setBrush(Qt::gray);
     }
-
-    //draws the circle
-    void StartlightPanel::paintEvent(QPaintEvent *e) {
-        QPainter painter(this);
-
-        if (isGreen) {
-            painter.setPen(Qt::green);
-            painter.setBrush(Qt::green);
-        } else {
-            painter.setPen(Qt::red);
-            painter.setBrush(Qt::red);
-        }
-        if (!receivedSignal) {
-            painter.setPen(Qt::gray);
-            painter.setBrush(Qt::gray);
-        }
-
-        int radius = 21;
-        painter.drawEllipse(16, 12, radius, radius);
-        QWidget::paintEvent(e);
+    if (!receivedSignal) {
+        painter.setPen(Qt::gray);
+        painter.setBrush(Qt::gray);
     }
+    painter.drawEllipse(posX, posY, radius, radius);
 
-    void StartlightPanel::startlightCallback(const std_msgs::Bool msg){
-
-        callbackTime = ros::Time::now();
-        receivedSignal = true;
-        isGreen = msg.data != 0;
-
-        if (isGreen) {
-            label->setText("        Go!");
-        } else {
-            label->setText("        Stop!");
-        }
+    // Right circle
+    if (!isGreen) {
+        painter.setPen(Qt::red);
+        painter.setBrush(Qt::red);
+    } else {
+        painter.setPen(Qt::gray);
+        painter.setBrush(Qt::gray);
     }
-
-    void StartlightPanel::timerCallback(const ros::TimerEvent& e) {
-        current = ros::Time::now();
-
-        if (current - callbackTime > ros::Duration(1)) {
-            receivedSignal = false;
-            label->setText("        No Message");
-        }
-
+    if (!receivedSignal) {
+        painter.setPen(Qt::gray);
+        painter.setBrush(Qt::gray);
     }
+    painter.drawEllipse(posX + radius + 3, posY, radius, radius);
+
+    QWidget::paintEvent(e);
+}
+
+void StartlightPanel::startlightCallback(const std_msgs::Bool msg) {
+    callbackTime = ros::Time::now();
+    receivedSignal = true;
+    isGreen = msg.data != 0;
+    if (isGreen) {
+        label->setText("              Go!");
+    } else {
+        label->setText("              Stop!");
+    }
+}
+
+void StartlightPanel::timerCallback(const ros::TimerEvent &e) {
+    current = ros::Time::now();
+    if (current - callbackTime > ros::Duration(1)) {
+        receivedSignal = false;
+        label->setText("              No Message");
+    }
+}
 }  // namespace rr_rviz_plugins
 
 PLUGINLIB_EXPORT_CLASS(rr_rviz_plugins::StartlightPanel, rviz::Panel)
