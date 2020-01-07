61b28fe326e60fdfe407d46a8d3a8a021342801b
NicoBartholomai <nfbartholomai@gmail.com>
2020-01-06 19:59:40 -0500

    Added a line that points in the direction of the heading

--- rr_rviz_plugins/src/ChassisPanel.cpp
+++ rr_rviz_plugins/src/ChassisPanel.cpp
@@ -11,6 +11,7 @@
 namespace rr_rviz_plugins {
     bool received_signal = false;
     int mph;
+    float hding;
     QLabel *speed_label;
     QLabel *heading_label;
     QLabel *msg_label;
@@ -38,12 +39,15 @@ namespace rr_rviz_plugins {
 // draws the images
     void ChassisPanel::paintEvent(QPaintEvent *e) {
         //initial positions / dimensions for the speedometer
+        const QRect & rct = e->rect();
         int x_pos = 1;
         int y_pos = 7;
         int width = 30;
         int height = 40;
         QRectF rect = QRectF(x_pos,y_pos,width,height);
         QPainter painter(this);
+        painter.eraseRect(rct);
+        painter.setRenderHint(QPainter::Antialiasing);
         //sets the color and draws the speedometer
         painter.setBrush(Qt::gray);
         painter.setPen(Qt::gray);
@@ -57,8 +61,8 @@ namespace rr_rviz_plugins {
         //the angle the speedometer needle needs to point in
         auto angle = (((top_speed - mph) * M_PI) / top_speed);
         //determines x and y height of the needle
-        auto x_comp = cos(angle) * width / 2;
-        auto y_comp = sin(angle) * height / 2;
+        float x_comp = cos(angle) * width / 2;
+        float y_comp = sin(angle) * height / 2;
         //the shape and direction of the needle
         QLineF needle = QLineF(width / 2,
                 y_pos + height / 2 - 1,
@@ -74,12 +78,36 @@ namespace rr_rviz_plugins {
         height = 25;
         //determines the rectangle shape the arrow will fit in
         rect = QRectF(x_pos,y_pos,width,height);
+        painter.setPen(Qt::white);
+        painter.setBrush(Qt::white);
+        painter.drawRect(rect);
         painter.setBrush(Qt::black);
         painter.setPen(Qt::black);
-        painter.drawRect(rect);
+
+//        painter.drawRect(rect);
+        //For the directional arrow to show the heading
+        //the x component of the origin
+        int x_org = x_pos + width / 2;
+        int y_org = y_pos + height / 2;
+        //The size of the arrow
+        int arr_size = 10; //this is actually  half the length of the arrow
+
+        y_comp = sin(hding) * (float) arr_size;
+        x_comp = cos(hding) * (float) arr_size;
+
+        QLineF arrow_shaft = QLineF(x_org - x_comp, y_org + y_comp, x_org + x_comp, y_org - y_comp);
+
+        painter.drawLine(arrow_shaft);
+
+        painter.setPen(Qt::white);
+        painter.setBrush(Qt::white);
 
 
-        //paints the event
+        //For the arrowhead
+        //the angle it juts out at
+        float pivot = M_PI / 6;
+
+        //actually paints everything from above
         QWidget::paintEvent(e);
     }
 
@@ -87,8 +115,9 @@ namespace rr_rviz_plugins {
         chassisUpdateTime = ros::Time::now();
         received_signal = true;
         mph = msg.speed_mps;
+        hding = msg.steer_rad;
         auto speed = "      " + std::to_string(msg.speed_mps) + " m/s";
-        auto heading = std::to_string(msg.steer_rad) + "rad";
+        auto heading = "      " + std::to_string(msg.steer_rad) + " rad";
         auto message = msg.state;
         speed_label->setText(speed.c_str());
         heading_label->setText(heading.c_str());
@@ -100,6 +129,7 @@ namespace rr_rviz_plugins {
         if (now - chassisUpdateTime > ros::Duration(.5)) { //latency for detecting if its no longer sending a message
             received_signal = false;
             mph = 0;
+            hding = 0;
             speed_label->setText("      No Message");
             heading_label->setText("      No Message");
             msg_label->setText("No Message");
