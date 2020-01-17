1a2d38d8275c2b24b04695108936f03bc1b1a511
Daniel <dmartin20576@gmail.com>
2020-01-17 18:46:53 +0800

    Added comments

--- rr_gazebo/scripts/box_trajectory_controller.py
+++ rr_gazebo/scripts/box_trajectory_controller.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+"""
+Measures the response time of the evgp car by moving a box in
+front of the car then measuring how long it takes the lidar scan, map,
+and planned path to reflect the update.
+
+@author Daniel Martin
+
+"""
+
 
 from __future__ import print_function
 
@@ -14,7 +23,6 @@ from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Path
 import numpy as np
-
 
 scan_empty_ = True
 map_empty_ = True
@@ -45,6 +53,7 @@ def make_request(traj_point, model_name):
     return request
 
 def scan_callback(scan):
+    # Scan is empty when all values detected are either within the car's footprint or infinity
     scan_empty = np.all(np.logical_or(np.array(scan.ranges) < car_inscribed_radius_, np.isinf(scan.ranges)))
 
     global scan_empty_, scan_pop_time_
@@ -54,6 +63,7 @@ def scan_callback(scan):
 
 
 def map_callback(map):
+    # Map is empty when there are only zero values
     map_empty = np.count_nonzero(map.data) == 0
 
     global map_empty_, map_pop_time_
@@ -62,6 +72,7 @@ def map_callback(map):
     map_empty_ = map_empty
 
 def path_callback(path):
+    # Path is straight when there are not obstacles in front of it
     path_straight = abs(path.poses[-1].pose.position.y) <= path_y_divergence_threshold_
 
     global path_straight_, path_pop_time_
@@ -90,9 +101,9 @@ def main():
     path_y_divergence_threshold_ = rospy.get_param("~path_y_divergence_threshold", 5)
     wait_time = rospy.get_param("~wait_time_between_states", 1)
 
+    # Convert pose string to list of floats
     world_full_obstacle_pose = map(float, world_full_obstacle_pose.split(" "))
     world_empty_obstacle_pose = map(float, world_empty_obstacle_pose.split(" "))
-
 
     rospy.Subscriber(lidar_scan, LaserScan, scan_callback)
     rospy.Subscriber(costmap_topic, OccupancyGrid, map_callback)
@@ -112,9 +123,6 @@ def main():
 
         print("Waiting for all topics to declare state: WORLD_EMPTY...")
         while not scan_empty_ or not map_empty_ and not path_straight_:
-            # print("Car is not planning straight when map is empty. Make sure there is a high bias for straightness")
-            # print("Map should be empty in state: WORLD_EMPTY, but it is not.")
-            # print("Check that car_inscribed_radius captures the car and world_empty_obstacle_pose is far enough away.")
             time.sleep(.1)
 
         # Moving object forwards and timing reaction
