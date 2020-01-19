c6e3c4da33484139fd7919c3f3df9d0b0df132fb
NicoBartholomai <nfbartholomai@gmail.com>
2020-01-19 17:02:23 -0500

    removed unnecessary comments

--- rr_rviz_plugins/include/rr_rviz_plugins/ChassisPanel.h
+++ rr_rviz_plugins/include/rr_rviz_plugins/ChassisPanel.h
@@ -1,36 +1,3 @@
-////
-//// Created by root on 12/20/19.
-////
-//
-//#ifndef SRC_CHASSISPANEL_H
-//#define SRC_CHASSISPANEL_H
-//
-//#include <ros/ros.h>
-//#include <rviz/panel.h>
-//#include <QVBoxLayout>
-//#include <QtGui/QPaintEvent>
-//#include <QtWidgets/QLabel>
-//#include <rr_msgs/chassis_state.h>
-//
-// namespace rr_rviz_plugins {
-// class ChassisPanel : public rviz::Panel {
-//    Q_OBJECT
-// public:
-//    explicit ChassisPanel(QWidget *parent = nullptr);
-//
-// protected:
-//    ros::NodeHandle nh;
-//    ros::Subscriber chassis_sub;
-//
-// private slots:
-//    static void chassisStateCallback(rr_msgs::chassis_state &msg); //need to find type of message later
-//    void paintEvent(QPaintEvent *e) override;
-//};
-//
-//} // namespace rr_rviz_plugins
-//
-//#endif //SRC_CHASSISPANEL_H
-
 #ifndef CATKIN_WS_CHASSISPANEL_H
 #define CATKIN_WS_CHASSISPANEL_H
 
