dc6f673971bdb8692a8e212dcba5c2df524eab6f
Srihari Subramanian <sri.sub000@gmail.com>
2020-08-02 14:56:13 -0400

    Add apriltag package

---
+++ rr_evgp/src/april_tag_detector/april_tag_detector.cpp
@@ -0,0 +1,15 @@
+//
+// Created by sriharis123 on 3/9/20.
+//
+
+#include "april_tag_detector.h"
+#include <ros/ros.h>
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "april_tag_detector");
+
+    ros::NodeHandle nh;
+
+    //ros::Subscriber tf_sub = nh.subscribe()
+}
