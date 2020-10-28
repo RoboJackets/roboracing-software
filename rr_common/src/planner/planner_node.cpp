d6d632ce459542ead99442ff7f9d86c526d34862
Nico Bartholomai <nfbartholomai@gmail.com>
2020-10-27 21:11:02 -0400

    Updates cost function

--- rr_common/src/planner/planner_node.cpp
+++ rr_common/src/planner/planner_node.cpp
@@ -71,7 +71,9 @@ void processMap() {
         const auto& path = rollout.path;
 
         std::vector<double> map_costs = g_map_cost_interface->DistanceCost(path);
-        double global_path_costs = g_global_path_cost->CalculateCost(path);
+//        double global_path_costs = g_global_path_cost->CalculateCost(path);
+        double global_path_costs = 0;
+        ROS_ERROR_STREAM("HELLO");
         double cost = 0;
         double inflator = 1;
         double gamma = 1.01;
@@ -83,12 +85,12 @@ void processMap() {
                 cost += k_speed_ * std::pow(max_speed - path[i].speed, 2);
                 cost += k_steering_ * std::abs(path[i].steer);
                 cost += k_angle_ * std::abs(path[i].pose.theta);
-                cost += k_global_path_cost_ *  global_path_costs;
             } else {
                 cost += collision_penalty_ * (path.size() - i);
                 break;
             }
         }
+        cost += k_global_path_cost_ * global_path_costs;
         return cost / inflator;
     };
 
