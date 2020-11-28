c7518335603baa7b3a7045bbcab15ea1cbcacbf5
Charlie Jenkins <cjenkins72@gatech.edu>
2020-11-27 20:42:19 -0800

    Add pointcloud box to represent car with april tag.

--- rr_evgp/src/april_tag_detection/tag_detection.cpp
+++ rr_evgp/src/april_tag_detection/tag_detection.cpp
@@ -8,34 +8,39 @@ std::vector<pcl::PointXYZ> opponent_points;
 
 pcl::PointCloud<pcl::PointXYZ> draw_opponents(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg);
 
-void draw_opponent(pcl::PointCloud<pcl::PointXYZ>& cloud, const int &id, pcl::PointXYZ &april_tag_center);
+void draw_opponent(pcl::PointCloud<pcl::PointXYZ> &cloud, const int &id, pcl::PointXYZ &april_tag_center);
 
 pcl::PointCloud<pcl::PointXYZ> draw_opponents(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg) {
     pcl::PointCloud<pcl::PointXYZ> opponent_cloud;
 //    opponent_points.clear();
     auto msgs = msg->detections;
-    for (const auto& message : msgs) { //Iterate through all discovered April Tags
-        pcl::PointXYZ opponent_point = pcl::PointXYZ(message.pose.pose.pose.position.x,
-                                                     message.pose.pose.pose.position.y,
-                                                     message.pose.pose.pose.position.z);
+    for (const auto &message : msgs) {  // Iterate through all discovered April Tags
+        pcl::PointXYZ opponent_point = pcl::PointXYZ(
+              message.pose.pose.pose.position.x, message.pose.pose.pose.position.y, message.pose.pose.pose.position.z);
         draw_opponent(opponent_cloud, message.id[0], opponent_point);
     }
     return opponent_cloud;
 }
 
-void publishPointCloud(pcl::PointCloud<pcl::PointXYZ>& cloud) {
+void publishPointCloud(pcl::PointCloud<pcl::PointXYZ> &cloud) {
     sensor_msgs::PointCloud2 outmsg;
     pcl::toROSMsg(cloud, outmsg);
-    outmsg.header.frame_id = "base_footprint";
+    outmsg.header.frame_id = "camera_optical";
     pub_pointcloud.publish(outmsg);
 }
 
-void draw_opponent(pcl::PointCloud<pcl::PointXYZ>& cloud, const int &id, pcl::PointXYZ &april_tag_center) {
-    int side_length = 10;
-    int start_x = april_tag_center.x - side_length / 2;
-    for (int x = 0; x < side_length; x++) {
-        for (int y = 0; y < side_length; y++) {
-            cloud.push_back(pcl::PointXYZ(april_tag_center.z, april_tag_center.x * -1, april_tag_center.y * -1));
+void draw_opponent(pcl::PointCloud<pcl::PointXYZ> &cloud, const int &id, pcl::PointXYZ &april_tag_center) {
+    int side_length = 5;
+    int num_points = 10;
+    double ratio = (double)side_length / num_points;
+    float start_y = april_tag_center.y - (float)side_length / 2;
+    float start_x = april_tag_center.x - (float)side_length / 2;
+    for (int x = 0; x < num_points; x++) {
+        for (int y = 0; y < num_points; y++) {
+            for (int z = 0; z < num_points; z++) {
+                cloud.push_back(pcl::PointXYZ((float)(start_x + x * ratio), (float)(start_y + y * ratio),
+                                              (float)(april_tag_center.z + z * ratio)));
+            }
         }
     }
 }
@@ -45,12 +50,12 @@ void callback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg) {
     auto msgs = msg->detections;
     pcl::PointCloud<pcl::PointXYZ> cloud = draw_opponents(msg);
     publishPointCloud(cloud);
-    for (const auto& message : msgs) { //Iterate through all discovered April Tags
+    for (const auto &message : msgs) {  // Iterate through all discovered April Tags
         ROS_INFO_STREAM((message.id[0]));
     }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     ros::init(argc, argv, "tag_detection");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("tag_detections", 1, callback);
