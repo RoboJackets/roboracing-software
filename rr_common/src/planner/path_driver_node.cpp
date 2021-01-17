88facb5439d11bf71f3645b8129551d8e5f53e9a
Nico Bartholomai <nfbartholomai@gmail.com>
2021-01-17 17:29:29 -0500

    updates path_point function

--- rr_common/src/planner/path_driver_node.cpp
+++ rr_common/src/planner/path_driver_node.cpp
@@ -5,76 +5,59 @@
 #include <rr_msgs/speed.h>
 #include <rr_msgs/steering.h>
 #include <rr_msgs/drive_path.h>
-
-ros::Publisher speed_pub;
-ros::Publisher steer_pub;
+ros::Publisher speed_pub, steer_pub;
 ros::Subscriber control_sub;
 std::vector<rr_msgs::control_vector> curr_drive_path;
 std::mutex mtx;
-ros::Time curr_path_time;
+ros::Time recv_drive_path;
 unsigned int curr_index;
-
 void drive_path_callback(const rr_msgs::drive_path::ConstPtr& msg) {
-    mtx.lock();
-    curr_path_time = ros::Time::now();
-    curr_drive_path = msg->control_vectors; // this should make a copy
-    curr_index = 0;
-    mtx.unlock();
-//    ROS_INFO("callback finished");
-}
-
-rr_msgs::control_vector get_path_point(ros::Time t) {
-    //get the right control vector given the time that has passed.
-    rr_msgs::control_vector prev_vec;
-    rr_msgs::control_vector curr_vec;
-    ros::Time path_time;
-    mtx.lock();
-    unsigned int path_size = curr_drive_path.size();
-    mtx.unlock();
-    // not sure how to handle if curr_drive_path changes mid loop
-    for (unsigned int i = 0; i < path_size;) {
-        prev_vec = curr_vec;
+    if (!msg->control_vectors.empty()) {
         mtx.lock();
-        if (i < curr_drive_path.size()) {
-            curr_vec = curr_drive_path[i];
-        }
-        path_time = curr_path_time;
-        i = curr_index++;
+        recv_drive_path = ros::Time::now();
+        curr_drive_path = msg->control_vectors;
+        curr_index = 0;
         mtx.unlock();
-        if (path_time + ros::Duration(curr_vec.time) > t) {
-            return prev_vec;
+    }
+}
+std::pair<double, double> get_path_point(ros::Time t) {
+    mtx.lock();
+    std::vector<rr_msgs::control_vector> local_drive_path = curr_drive_path;
+    unsigned int local_index = curr_index;
+    mtx.unlock();
+//    auto send_control = local_drive_path[curr_index];
+    rr_msgs::control_vector send_control;
+    for (unsigned int i = local_index; i < local_drive_path.size(); i++) {
+        if (i == local_drive_path.size() - 1 || t < recv_drive_path + ros::Duration(local_drive_path[i + 1].time)) {
+            send_control = local_drive_path[i];
+            //why no mtx for this current index?
+            mtx.lock();
+            curr_index = i;
+            mtx.unlock();
+            break;
         }
     }
-    return curr_vec;
+    return std::make_pair(send_control.speed, send_control.angle);
 }
-
 int main(int argc, char **argv) {
     ros::init(argc, argv, "path_driver");
     ros::NodeHandle nh;
-    curr_path_time = ros::Time::now();
+    recv_drive_path = ros::Time::now();
     speed_pub = nh.advertise<rr_msgs::speed>("speed", 1);
     steer_pub = nh.advertise<rr_msgs::steering>("steering", 1);
     control_sub = nh.subscribe<rr_msgs::drive_path>("plan/drive_path", 1, drive_path_callback);
-
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
     rr_msgs::speed curr_speed;
     rr_msgs::steering curr_angle;
-    ros::Rate r(15); // 10hz
+    ros::Rate r(100); // 10hz
     while(ros::ok()) {
+        ros::spinOnce();
         auto now = ros::Time::now();
-//        ros::spinOnce();
-        rr_msgs::control_vector vec = get_path_point(ros::Time::now());
+        std::tie(curr_speed.speed, curr_angle.angle) = get_path_point(now);
         curr_speed.header.stamp = now;
         curr_angle.header.stamp = now;
-
-        curr_speed.speed = vec.speed;
-        curr_angle.angle = vec.angle;
-
         speed_pub.publish(curr_speed);
         steer_pub.publish(curr_angle);
         r.sleep();
     }
-
     return 0;
 }
