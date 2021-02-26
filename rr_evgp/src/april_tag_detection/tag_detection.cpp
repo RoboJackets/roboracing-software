59503dbbdb39cc0104ab08aeeaed77b93095c715
Daniel <dmartin20576@gmail.com>
2021-02-26 01:34:01 -0500

    fix base_footprint guess

--- rr_evgp/src/april_tag_detection/tag_detection.cpp
+++ rr_evgp/src/april_tag_detection/tag_detection.cpp
@@ -8,9 +8,8 @@
 
 tag_detection::tag_detection(ros::NodeHandle *nh, const std::string &camera_frame, const std::string &pointcloud,
                              const std::string &tag_detections_topic, const std::string &destination_frame,
-                             const std::string &tag_detection_markers, double x_offset,
-                             double y_offset, double px_per_m, double width, double height,
-                             std::vector<april_robot> robots) {
+                             const std::string &tag_detection_markers, double x_offset, double y_offset,
+                             double px_per_m, double width, double height, std::vector<april_robot> robots) {
     this->camera_frame = camera_frame;
     this->destination_frame = destination_frame;
     this->width = width;
@@ -30,7 +29,7 @@ tag_detection::tag_detection(ros::NodeHandle *nh, const std::string &camera_fram
         }
     }
 
-    //Storing as an instance variable keeps subscriber alive
+    // Storing as an instance variable keeps subscriber alive
     sub_detections = nh->subscribe(tag_detections_topic, 1, &tag_detection::callback, this);
     pub_pointcloud = nh->advertise<sensor_msgs::PointCloud2>(pointcloud, 1);
     pub_markers = nh->advertise<visualization_msgs::Marker>(tag_detection_markers, 0);
@@ -39,12 +38,12 @@ tag_detection::tag_detection(ros::NodeHandle *nh, const std::string &camera_fram
 void tag_detection::callback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg) {
     opponent_cloud.clear();
     auto msgs = msg->detections;
-    std::vector<std::vector<std::pair<int, geometry_msgs::Pose>>> tag_groups(5); //TODO: Add as param
-    for (const auto &message : msgs) {  // Iterate through all discovered April Tags
-        int car_number = message.id[0] / 10; // By the specs, car 1 has tags 10,11,12... car 2 has 20,21,22...
+    std::vector<std::vector<std::pair<int, geometry_msgs::Pose>>> tag_groups(5);  // TODO: Add as param
+    for (const auto &message : msgs) {        // Iterate through all discovered April Tags
+        int car_number = message.id[0] / 10;  // By the specs, car 1 has tags 10,11,12... car 2 has 20,21,22...
         tag_groups[car_number].push_back(std::pair(message.id[0], message.pose.pose.pose));
-//        message.id[0]
-//        draw_opponent(message.id[0], message.pose.pose.pose);
+        //        message.id[0]
+        //        draw_opponent(message.id[0], message.pose.pose.pose);
     }
     draw_opponents(&tag_groups);
     publishPointCloud(opponent_cloud);
@@ -103,37 +102,70 @@ void tag_detection::draw_opponent(int id, geometry_msgs::Pose april_camera_msg) 
 }
 
 void tag_detection::draw_opponents(std::vector<std::vector<std::pair<int, geometry_msgs::Pose>>> *real_tags) {
-    tf::StampedTransform camera;
-    tf_listener.lookupTransform(this->destination_frame, this->camera_frame, ros::Time(0), camera);
-    for (const auto &robot: *real_tags) {
+    // w := our base footprint (world), o := optical camera, a := april image, l := april link,
+    // b := their base footprint
+    // x_T_y := Transfrom from y to x
+    
+    tf::StampedTransform w_T_o;
+    tf_listener.lookupTransform(this->destination_frame, this->camera_frame, ros::Time(0), w_T_o);
+    for (const auto &robot : *real_tags) {
         tf::Pose robot_pose;
-        robot_pose.setRotation(tf::Quaternion::getIdentity());
 
-        for (const auto &tag: robot) {
-            tf::Pose april_w;
-            tf::poseMsgToTF(tag.second, april_w);
+        for (const auto &tag : robot) {
+            tf::Pose o_T_a;
+            tf::poseMsgToTF(tag.second, o_T_a);
 
-            //Update location in respects to camera
-            tf::Pose back_pose = camera * april_w;
+            tf::Transform a_T_l;
+            a_T_l.setRotation(tf::createQuaternionFromRPY(0, M_PI / 2, M_PI / 2));
+            // April tag has different rotation from april link
 
-            tf::StampedTransform tag_transformation;
+            // Target frame is rightmost digit + _april
+            // eg: April tag id = 34, target frame is "4_april"
+            std::string april_link = std::to_string(tag.first % 10) + "_april";
+            tf::StampedTransform l_T_b;
+            tf_listener.lookupTransform(april_link, "base_footprint", ros::Time(0), l_T_b);
 
-            //Target frame is rightmost digit + _april
-            //eg: April tag id = 34, target frame is "4_april"
-            tf_listener.lookupTransform(std::to_string(tag.first % 10) + "_april", "5_april", ros::Time(0),
-                                        tag_transformation);
+            // Update location
+            tf::Pose p_w = w_T_o * o_T_a * a_T_l * l_T_b;
 
-            back_pose.setRotation(tag_transformation.getRotation() * back_pose.getRotation());
-            back_pose.setOrigin(tag_transformation.getOrigin() + back_pose.getOrigin());
+            // To make your box outline point the right dirction,
+            // Not needed if you make the outline different dimension
+            tf::Transform random_T;
+            random_T.setRotation(tf::createQuaternionFromRPY(0, 0, -M_PI / 2));
 
-            robot_pose = back_pose;
+            robot_pose = p_w * random_T;
+
+            ROS_INFO_STREAM(april_link << " " << robot_pose.getOrigin().getX()
+                                << " " << robot_pose.getOrigin().getY() << " " << robot_pose.getOrigin().getZ());
+
+            // Just to viz points for debugging
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = "base_footprint";
+            marker.header.stamp = ros::Time();
+            marker.ns = "";
+            marker.id = 1;
+            marker.type = visualization_msgs::Marker::SPHERE;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = robot_pose.getOrigin().getX();
+            marker.pose.position.y = robot_pose.getOrigin().getY();
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.x = 0.1;
+            marker.scale.y = 0.1;
+            marker.scale.z = 0.1;
+            marker.color.a = 1.0;
+            marker.color.g = 1.0;
+            pub_markers.publish(marker);
+
+            pcl::PointCloud<pcl::PointXYZ> car_outline;
+            Eigen::Affine3d affine3d;
+            tf::transformTFToEigen(robot_pose, affine3d);
+            pcl::transformPointCloud(pcl_outline, car_outline, affine3d);
+            opponent_cloud += (car_outline);
         }
-
-        pcl::PointCloud<pcl::PointXYZ> car_outline;
-        Eigen::Affine3d affine3d;
-        tf::transformTFToEigen(camera * robot_pose, affine3d);
-        pcl::transformPointCloud(pcl_outline, car_outline, affine3d);
-        opponent_cloud += (car_outline);
     }
 }
 
@@ -142,7 +174,7 @@ tf::Pose tag_detection::poseAverage(std::vector<tf::Pose> poses) {
     tf::Quaternion averagedQuaternions;
     Eigen::Matrix4Xf quaternionMatrix;
 
-    for(int i = 0; i < poses.size(); i++) {
+    for (int i = 0; i < poses.size(); i++) {
         averagedVectors += poses[i].getOrigin();
         quaternionMatrix(0, i) = poses[i].getRotation().getW();
         quaternionMatrix(1, i) = poses[i].getRotation().getX();
