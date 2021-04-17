03aea5f26b5c35c2fa24e379244da031ca31892c
Charlie Jenkins <cjenkins72@gatech.edu>
2021-04-16 22:55:44 -0400

    Made progress on cone filtering

--- rr_evgp/src/cone_detection/cone_connection.cpp
+++ rr_evgp/src/cone_detection/cone_connection.cpp
@@ -10,44 +10,69 @@ static int final_index1 = 0;
 static int final_index2 = 0;
 static std::vector<geometry_msgs::Pose> wall1;
 static std::vector<geometry_msgs::Pose> wall2;
+static geometry_msgs::Pose car_pose;
+static float close_distance = 0.25;
 
-static double distance(const geometry_msgs::Pose &p1, const geometry_msgs::Pose &p2) {
-    return pow((p2.position.x - p1.position.x), 2) +
-           pow((p2.position.y - p1.position.y), 2) +
-           pow((p2.position.z - p1.position.z), 2);
+static double distance(const geometry_msgs::Point &p1, const geometry_msgs::Point &p2) {
+    return pow((p2.x - p1.x), 2) +
+           pow((p2.y - p1.y), 2) +
+           pow((p2.z - p1.z), 2);
 }
 
 static bool close(const geometry_msgs::Point &p1, const geometry_msgs::Point &p2) {
-
+    return distance(p1, p2) < close_distance;
 }
 
+/*
+ * Modifies p1 to be the average of itself and p2
+ */
+static inline void average_pose(geometry_msgs::Pose &p1, const geometry_msgs::Pose &p2) {
+    p1.position.x = (p1.position.x + p2.position.x) / 2;
+    p1.position.y = (p1.position.y + p2.position.y) / 2;
+    p1.position.z = (p1.position.z + p2.position.z) / 2;
+}
+
+// Relies on the elements that are closes to the car being at the front of the array
+// Must be sorted by distance to car
 void callback(const geometry_msgs::PoseArray &cone_array) {
     std::vector<geometry_msgs::Pose> local_wall1;
     std::vector<geometry_msgs::Pose> local_wall2;
 
     wall1.push_back(cone_array.poses[0]);
     for (geometry_msgs::Pose cone : cone_array.poses) {
-        if (distance(cone, wall1[0]) < cone_distance) {
+        if (distance(cone.position, wall1[0].position) < cone_distance) {
             wall1.push_back(cone);
         } else {
             wall2.push_back(cone);
         }
     }
 
-    if (distance(local_wall1[local_wall1.size() - 1], wall1[final_index1]) <
-        distance(local_wall1[local_wall1.size() - 1], wall2[final_index2])) {
+    if (distance(local_wall1[local_wall1.size() - 1].position, wall1[final_index1].position) <
+        distance(local_wall1[local_wall1.size() - 1].position, wall2[final_index2].position)) {
         // local_wall 1 is closer to global wall1
 
         // Iterate backwards through global wall1 starting at final_index1
         // until reach value that is close to 0th item in local wall1
-        long start_wall1;
-        for (auto it = wall1.rbegin() + static_cast<int>(wall1.size() - final_index1); it != wall1.rend(); ++it) {
-            if (close(it->position, wall1[0].position)) {
-                start_wall1 = std::distance(begin(wall1), it.base()) - 1; // get current index
+        ulong start_wall1;
+        for (ulong i = wall1.size(); i > 0; --i) {
+            if (close(wall1[i].position, local_wall1[0].position)) {
+                start_wall1 = i; // get current index
             }
         }
 
         // Now iterate forwards and update overlapping cone points
+        for (auto it = local_wall1.begin();
+             it < local_wall1.end() && start_wall1 < wall1.size() - 1; it++, start_wall1++) {
+            double dist;
+            if (close(it->position, wall1[start_wall1].position)) {
+                average_pose(wall1[start_wall1], *it.base());
+            } else if (distance(car_pose.position, wall1[start_wall1].position) <
+                       (dist = distance(car_pose.position, it->position))) {
+                if (dist < distance(car_pose.position, wall1[start_wall1 + 1].position)) {
+                    wall1.insert(start_wall1 + 1, it.base());
+                }
+            }
+        }
 
         // Iterate backwards through global wall2 until reach value
         // that is close to 0th item in local wall2
