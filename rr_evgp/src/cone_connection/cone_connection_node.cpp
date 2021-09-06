a6693454d1ff1d47f522a936973f6df4eb5f2a04
Charlie Jenkins <thecharlesjenkins@gmail.com>
2021-09-05 20:13:56 -0400

    Now publishing occupancy grid

--- rr_evgp/src/cone_connection/cone_connection_node.cpp
+++ rr_evgp/src/cone_connection/cone_connection_node.cpp
@@ -1,61 +1,37 @@
 #include "cone_connection_node.h"
 
-#include <nav_msgs/OccupancyGrid.h>
 #include <parameter_assertions/assertions.h>
 #include <pcl/PCLPointCloud2.h>
-#include <pcl/point_cloud.h>
 #include <pcl/search/kdtree.h>
-#include <pcl/segmentation/extract_clusters.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/PointCloud2.h>
 #include <std_msgs/String.h>
 
-#include <pcl/impl/point_types.hpp>
+#include <random>
 #include <unordered_set>
 
 #include "opencv2/core.hpp"
 #include "ros/ros.h"
 
-/**
- * @author Charles Jenkins
- * Helpful guide to understand this file: http://wiki.ros.org/costmap_2d/Tutorials/Creating%20a%20New%20Layer
- */
-
-ConeConnection::ConeConnection() {
-    // Initialize parameters
-    ros::NodeHandle nh;
-    ros::NodeHandle private_nh("~");
-
+ConeConnection::ConeConnection(ros::NodeHandle &nh, ros::NodeHandle &pnh) {
     // Initialize class variables
-    max_x_ = 0;
-    max_y_ = 0;
-    min_x_ = 0;
-    min_y_ = 0;
     distance_between_walls = 18;
     distance_between_cones = distance_between_walls / 3;
-    cluster_tolerance_ = 0.5;
-    min_cluster_size_ = 1;
-    max_cluster_size_ = 10;
 
     std::string walls_topic;
-    assertions::param(private_nh, "walls_topic", walls_topic, std::string("/global_walls_topic"));
+    assertions::param(pnh, "walls_topic", walls_topic, std::string("/global_walls_topic"));
     pub_walls = nh.advertise<nav_msgs::OccupancyGrid>(walls_topic, 1);
 
     std::string cone_connection_status_topic;
-    assertions::param(private_nh, "cone_connection_status", cone_connection_status_topic,
+    assertions::param(pnh, "cone_connection_status", cone_connection_status_topic,
                       std::string("/cone_connection_status"));
     cone_connection_status = nh.advertise<std_msgs::String>(cone_connection_status_topic, 1);
 
-    assertions::param(private_nh, "distance_between_walls", distance_between_walls, 18);
-    assertions::param(private_nh, "distance_between_cones", distance_between_cones, distance_between_walls / 3);
-
-    assertions::param(private_nh, "cluster_tolerance", cluster_tolerance_, 0.5);
-    assertions::param(private_nh, "min_cluster_size", min_cluster_size_, 1);
-    assertions::param(private_nh, "max_cluster_size", max_cluster_size_, 10);
+    assertions::param(pnh, "distance_between_walls", distance_between_walls, 18);
+    assertions::param(pnh, "distance_between_cones", distance_between_cones, distance_between_walls / 3);
 
     std::string cones_topic;
-    assertions::param(private_nh, "cones_topic", cones_topic, std::string("/cones_topic"));
-    cones_subscriber = nh.subscribe(cones_topic, 1, &ConeConnection::updateMap);
+    assertions::param(pnh, "cones_topic", cones_topic, std::string("/cones_topic"));
+    cones_subscriber = nh.subscribe(cones_topic, 1, &ConeConnection::updateMap, this);
 }
 
 static inline double distance(const pcl::PointXYZ &p1, const pcl::PointXYZ &p2) {
@@ -91,71 +67,96 @@ void ConeConnection::bsline(GridPosition start, GridPosition end, nav_msgs::Occu
 }
 
 void ConeConnection::clustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud,
-                                nav_msgs::OccupancyGrid &occupancyGrid) {
+                                nav_msgs::OccupancyGrid &occupancyGrid, const int radius) {
+    static int id = 0;
+    id++;
     // **CLUSTERING**
     // Link: https://pcl.readthedocs.io/en/latest/cluster_extraction.html
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     // Add all points to the tree
     tree->setInputCloud(cloud);
-    // visited set
 
     const double pnt_origin_x = .5 * (occupancyGrid.info.height);
     const double pnt_origin_y = .5 * (occupancyGrid.info.width);
-    //  col = int((pnt.y - pnt_origin) / resolution)
-    const int K = 4;
 
-    std::unordered_set<pcl::PointXYZ *> visited;
-    std::queue<pcl::PointXYZ *> queue;
-    queue.push(&cloud->at(0));
-    visited.insert(&cloud->at(0));
+
+    std::unordered_set<pcl::PointXYZ> visited;
+    std::queue<pcl::PointXYZ> queue;
+    queue.push(cloud->at(0));
+    visited.insert(cloud->at(0));
 
     auto occupancyPosition = [&](const pcl::PointXYZ &point) {
-        GridPosition ret{ .row = int((point.x - pnt_origin_x) / occupancyGrid.info.resolution),
-                          .col = int((point.y - pnt_origin_y) / occupancyGrid.info.resolution) };
+        GridPosition ret{ .row = int((pnt_origin_x)-point.x * occupancyGrid.info.resolution),
+                          .col = int((pnt_origin_y)-point.y * occupancyGrid.info.resolution) };
         return ret;
     };
 
     while (!queue.empty()) {
-        pcl::PointXYZ *curr = queue.front();                   // Grab value from queue
-        GridPosition currPosition = occupancyPosition(*curr);  // Get position of curr on graph
-        queue.pop();                                           // Remove value from queue
+        std::string queueSize = "Queue Size: " + std::to_string(queue.size());
+        ROS_INFO_STREAM(queueSize);
+        pcl::PointXYZ curr = queue.front();                   // Grab value from queue
+        GridPosition currPosition = occupancyPosition(curr);  // Get position of curr on graph
+        queue.pop();                                          // Remove value from queue
 
-        std::vector<int> nearbyPoints(K);
-        std::vector<float> nearbyPointsSquaredDistance(K);
-        int close = tree->nearestKSearch(*curr, K, nearbyPoints, nearbyPointsSquaredDistance);
+        std::vector<int> nearbyPoints;
+        std::vector<float> nearbyPointsSquaredDistance;
+        int close = tree->radiusSearch(curr, radius, nearbyPoints, nearbyPointsSquaredDistance, 10);
 
+        ROS_INFO_STREAM(std::to_string(id) + " Close points: " + std::to_string(close));
         // Is a cone that should be added
         if (close > 0) {
-            for (auto nearby = nearbyPoints.begin(); nearby < nearbyPoints.end(); nearby++) {
-                pcl::PointXYZ *c = &cloud->at(*nearby.base());
-                if (visited.find(c) != visited.end()) {
+            int addedToQueue = 0;
+            for (auto nearby : nearbyPoints) {
+                pcl::PointXYZ c = cloud->at(nearby);
+                if (visited.find(c) == visited.end()) {
                     // Draw line on occupancy grid to each neighbor
-                    bsline(occupancyPosition(*c), currPosition, occupancyGrid);
+                    bsline(occupancyPosition(c), currPosition, occupancyGrid);
                     // Add neighbors to visited and queue
                     visited.insert(c);
                     queue.push(c);
+                    ROS_INFO_STREAM(std::to_string(id) + " Pushing onto queue: " + std::to_string(++addedToQueue) + " Visited Size: " + std::to_string(visited.size()));
                 }
+                ROS_INFO_STREAM(std::to_string(id) + " Point: " + std::to_string(c.x) + ", " + std::to_string(c.y) +
+                                ", " + std::to_string(c.z));
             }
+            ROS_INFO_STREAM(std::to_string(id) + " Finished for loop");
+            ROS_INFO_STREAM(std::to_string(id) + " Queue Size: " + std::to_string(queue.size()));
         }
     }
+    ROS_INFO_STREAM(std::to_string(id) + " exited");
 }
 
 // main callback function
-void ConeConnection::updateMap(const sensor_msgs::PointCloud2ConstPtr& cloud_msg) {
+void ConeConnection::updateMap(const sensor_msgs::PointCloud2ConstPtr &cloud_msg) {
     // Convert from sensor_msgs::PointCloud2 -> pcl::PCLPointCloud2 -> pcl::PointCloud<PointXYZ>
     pcl::PCLPointCloud2::Ptr pcl_pc2(new pcl::PCLPointCloud2);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl_conversions::toPCL(*cloud_msg, *pcl_pc2);
     pcl::fromPCLPointCloud2(*pcl_pc2, *cloud);
 
-//    std::vector<geometry_msgs::Pose> centroids;
-//    walls_ = linkWalls(centroids);
-
     // Initialize occupancy grid
     nav_msgs::OccupancyGrid occupancyGrid;
     occupancyGrid.info.height = 350;
     occupancyGrid.info.width = 350;
-    clustering(cloud, occupancyGrid);
 
+    // info.resolution scales grid size to the provided meters
+    occupancyGrid.info.resolution = 0.25;
+    std::vector<signed char> grid(occupancyGrid.info.height * occupancyGrid.info.width);
+    occupancyGrid.data = grid;
+    clustering(cloud, occupancyGrid, static_cast<int>(distance_between_cones * 1.5));
 
+    std_msgs::String msg;
+    msg.data = "Publishing grid";
+    cone_connection_status.publish(msg);
+    pub_walls.publish(occupancyGrid);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "cone_connection_node");
+    ros::NodeHandle nh;
+    ros::NodeHandle nhp("~");
+    ConeConnection coneConnection(nh, nhp);
+
+    ros::spin();
+    return 0;
 }
--- rr_evgp/src/cone_connection/cone_connection_node.h
+++ rr_evgp/src/cone_connection/cone_connection_node.h
@@ -12,12 +12,11 @@
 #include <geometry_msgs/PoseArray.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <ros/publisher.h>
 #include <ros/subscriber.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <tf/transform_listener.h>
-
-#include <pcl/impl/point_types.hpp>
 
 namespace std {
 template <>
@@ -38,29 +37,6 @@ struct equal_to<pcl::PointXYZ> {
 
 class ConeConnection {
   private:
-    class Node {
-      public:
-        geometry_msgs::Pose value;
-        Node *next;
-        Node *prev;
-    };
-
-    class LinkedList {
-      public:
-        Node *head;
-        int size;
-    };
-
-    struct GraphNode {
-        geometry_msgs::Pose value;
-        std::vector<GraphNode> children;
-    };
-
-    struct Graph {
-        GraphNode *head;
-        int size;
-    };
-
     struct GridPosition {
         int row;
         int col;
@@ -68,35 +44,21 @@ class ConeConnection {
 
     std::vector<tf::Pose> points;
 
-    double max_x_;
-    double max_y_;
-    double min_x_;
-    double min_y_;
     int distance_between_walls;
     int distance_between_cones;
 
-    std::vector<LinkedList> walls_;
-
     ros::Publisher pub_walls;
     ros::Publisher cone_connection_status;
-    ros::Publisher image;
-    nav_msgs::MapMetaData mapMetaData;
-    ros::Subscriber global_costmap_;
     ros::Subscriber cones_subscriber;
     std::unique_ptr<tf::TransformListener> listener;
-    ros::Publisher distance_map_pub;
 
-    double cluster_tolerance_;
-    int min_cluster_size_, max_cluster_size_;
-
-    static void updateMap(const sensor_msgs::PointCloud2ConstPtr &cloud_msg);
-    static void clustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, nav_msgs::OccupancyGrid &occupancyGrid);
+    void updateMap(const sensor_msgs::PointCloud2ConstPtr &cloud_msg);
+    void clustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, nav_msgs::OccupancyGrid &occupancyGrid,
+                           int radius);
 
     static void bsline(GridPosition start, GridPosition end, nav_msgs::OccupancyGrid &grid);
 
-    ConeConnection();
-
   public:
-    ~ConeConnection();
+    ConeConnection(ros::NodeHandle &nh, ros::NodeHandle &pnh);
 };
 #endif  // RR_EVGP_CONE_CONNECTION_NODE_H
