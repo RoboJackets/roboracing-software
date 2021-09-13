7425c093af08bd15624cc85092ff0a649d2ede3a
Charlie Jenkins <thecharlesjenkins@gmail.com>
2021-09-13 16:53:09 -0400

    Get cone connection to work using DDA algorithm

--- rr_evgp/src/cone_connection/cone_connection_node.cpp
+++ rr_evgp/src/cone_connection/cone_connection_node.cpp
@@ -26,7 +26,7 @@ ConeConnection::ConeConnection(ros::NodeHandle &nh, ros::NodeHandle &pnh) {
                       std::string("/cone_connection_status"));
     cone_connection_status = nh.advertise<std_msgs::String>(cone_connection_status_topic, 1);
 
-    assertions::param(pnh, "distance_between_walls", distance_between_walls, 18);
+    assertions::param(pnh, "distance_between_walls", distance_between_walls, 15);
     assertions::param(pnh, "distance_between_cones", distance_between_cones, distance_between_walls / 3);
 
     std::string cones_topic;
@@ -46,79 +46,126 @@ static inline double distance(const pcl::PointXYZ &p1, const pcl::PointXYZ &p2) 
  * @param grid OccupancyGrid to place points
  */
 void ConeConnection::bsline(GridPosition start, GridPosition end, nav_msgs::OccupancyGrid &grid) {
-    int column_diff, row_diff, p;
-    column_diff = end.col - start.col;
-    row_diff = end.row - start.row;
-    p = 2 * (row_diff) - (column_diff);
-    while (start.col <= end.col) {
-        if (p < 0) {
-            start.col = start.col + 1;
-            start.row = start.row;
-            p = p + 2 * (row_diff);
-        } else {
-            start.col = start.col + 1;
-            start.row = start.row + 1;
-            p = p + 2 * (row_diff - column_diff);
-        }
-        ROS_INFO_STREAM("Close points: (" + std::to_string(start.col) + ", "+ std::to_string(start.row) + "), (" + std::to_string(end.col) + ", "+ std::to_string(end.row) + ")");
-        // Set deadly object at provided row/column
-        grid.data[start.row * grid.info.width + start.col] = 100;
+    //    ROS_INFO_STREAM("BSline: (" + std::to_string(start.col) + ", "+ std::to_string(start.row) + "), (" +
+    //    std::to_string(end.col) + ", "+ std::to_string(end.row) + ")");
+    grid.data[start.row * grid.info.width + start.col] = -start.row % 90 - 10;
+    grid.data[end.row * grid.info.width + end.col] = -start.row % 90 - 10;
+
+    //    int column_diff, row_diff, p;
+    //    column_diff = end.col - start.col;
+    //    row_diff = end.row - start.row;
+    //    p = 2 * (row_diff) - (column_diff);
+    //    while (start.col <= end.col) {
+    //        if (p < 0) {
+    //            start.col = start.col + 1;
+    //            start.row = start.row;
+    //            p = p + 2 * (row_diff);
+    //        } else {
+    //            start.col = start.col + 1;
+    //            start.row = start.row + 1;
+    //            p = p + 2 * (row_diff - column_diff);
+    //        }
+    ////        ROS_INFO_STREAM("Close points: (" + std::to_string(start.col) + ", "+ std::to_string(start.row) + "), ("
+    ///+ std::to_string(end.col) + ", "+ std::to_string(end.row) + ")");
+    //        // Set deadly object at provided row/column
+    //    }
+
+    //    grid.data[((int) ((start.row + end.row) / 2)) * grid.info.width + ((int) ((end.col + end.col) / 2))] = 126;
+    int row = end.row;
+    int col = end.col;
+    for (int i = 0; i < 10; i++) {
+        row = ((int)((start.row + row) / 2));
+        col = ((int)((start.col + col) / 2));
+        grid.data[row * grid.info.width + row] = 126;
+    }
+}
+
+int abs(int n) {
+    return ((n > 0) ? n : (n * (-1)));
+}
+
+// DDA Function for line generation
+void DDA(int X0, int Y0, int X1, int Y1, nav_msgs::OccupancyGrid &occupancyGrid) {
+    // calculate dx & dy
+    int dx = X1 - X0;
+    int dy = Y1 - Y0;
+
+    // calculate steps required for generating pixels
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    // calculate increment in x & y for each steps
+    float Xinc = dx / (float)steps;
+    float Yinc = dy / (float)steps;
+
+    // Put pixel for each step
+    float X = X0;
+    float Y = Y0;
+    for (int i = 0; i <= steps; i++) {
+        occupancyGrid.data[floor(X) * occupancyGrid.info.width + floor(Y) - 1] = 126;  // put pixel at (X,Y)
+        X += Xinc;  // increment in x at each step
+        Y += Yinc;  // increment in y at each step
     }
 }
 
 void ConeConnection::clustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud,
                                 nav_msgs::OccupancyGrid &occupancyGrid, const int radius) {
-    static int id = 0;
-    id++;
     // **CLUSTERING**
     // Link: https://pcl.readthedocs.io/en/latest/cluster_extraction.html
     pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
     // Add all points to the tree
     tree->setInputCloud(cloud);
 
-    const double pnt_origin_x = .5 * (occupancyGrid.info.height);
-    const double pnt_origin_y = .5 * (occupancyGrid.info.width);
+    int car_r = .5 * (occupancyGrid.info.height);
+    int car_c = .5 * (occupancyGrid.info.width);
+    double car_x = car_r * occupancyGrid.info.resolution;  // m / cell
+    double car_y = car_c * occupancyGrid.info.resolution;
 
-
-    std::unordered_set<pcl::PointXYZ> visited;
+    std::unordered_set<pcl::PointXYZ> unvisited;
     std::queue<pcl::PointXYZ> queue;
     queue.push(cloud->at(0));
-    visited.insert(cloud->at(0));
 
+    for (pcl::PointXYZ point : cloud->points) {
+        unvisited.insert(point);
+    }
+
+    //    at 5,5, lidar at -1,1 from car
+    //    so lidar 4,6
     auto occupancyPosition = [&](const pcl::PointXYZ &point) {
-        GridPosition ret{ .row = int((pnt_origin_x)-point.x * occupancyGrid.info.resolution),
-                          .col = int((pnt_origin_y)-point.y * occupancyGrid.info.resolution) };
+        int r = std::clamp((car_y + point.y) / occupancyGrid.info.resolution, 0.0, (double)occupancyGrid.info.height);
+        int c = std::clamp((car_x + point.x) / occupancyGrid.info.resolution, 0.0, (double)occupancyGrid.info.width);
+        GridPosition ret{ .row = r, .col = c };
         return ret;
     };
 
-    while (!queue.empty()) {
-        pcl::PointXYZ curr = queue.front();                   // Grab value from queue
-        GridPosition currPosition = occupancyPosition(curr);  // Get position of curr on graph
-        queue.pop();                                          // Remove value from queue
+    while (!unvisited.empty()) {
+        queue.push(*unvisited.begin());
+        unvisited.erase(queue.back());
+        while (!queue.empty()) {
+            pcl::PointXYZ curr = queue.front();                   // Grab value from queue
+            GridPosition currPosition = occupancyPosition(curr);  // Get position of curr on graph
+            queue.pop();                                          // Remove value from queue
 
-        std::vector<int> nearbyPoints;
-        std::vector<float> nearbyPointsSquaredDistance;
-        int close = tree->radiusSearch(curr, radius, nearbyPoints, nearbyPointsSquaredDistance, 10);
+            std::vector<int> nearbyPoints;
+            std::vector<float> nearbyPointsSquaredDistance;
+            int close = tree->radiusSearch(curr, radius, nearbyPoints, nearbyPointsSquaredDistance, 100);
 
-        // Is a cone that should be added
-        if (close > 0) {
-            ROS_INFO_STREAM("CURRENT POINT: (" + curr.x + ", " + currPosition.col);
-            int addedToQueue = 0;
-            for (auto nearby : nearbyPoints) {
-                pcl::PointXYZ c = cloud->at(nearby);
-                if (visited.find(c) == visited.end()) {
-                    // Draw line on occupancy grid to each neighbor
-                    bsline(occupancyPosition(c), currPosition, occupancyGrid);
-                    // Add neighbors to visited and queue
-                    visited.insert(c);
-                    queue.push(c);
-//                    ROS_INFO_STREAM(std::to_string(id) + " Pushing onto queue: " + std::to_string(++addedToQueue) + " Visited Size: " + std::to_string(visited.size()));
+            // Is a cone that should be added
+            if (close > 0) {
+                int addedToQueue = 0;
+                for (auto nearby : nearbyPoints) {
+                    pcl::PointXYZ c = cloud->at(nearby);
+                    if (unvisited.find(c) != unvisited.end()) {
+                        GridPosition start = occupancyPosition(c);
+                        if ((start.col > car_c + 1 || start.col < car_c - 1) && (start.row > car_r + 1 || start.row < car_r - 1)) {
+                            // Draw line on occupancy grid to each neighbor
+                            DDA(start.row, start.col, currPosition.row, currPosition.col, occupancyGrid);
+                            // Add neighbors to visited and queue (only if outside of car footprint)
+                            queue.push(c);
+                        }
+                        unvisited.erase(c);
+                   }
                 }
-//                ROS_INFO_STREAM(std::to_string(id) + " Point: " + std::to_string(c.x) + ", " + std::to_string(c.y) +
-//                                ", " + std::to_string(c.z));
             }
-//            ROS_INFO_STREAM(std::to_string(id) + " Finished for loop");
-//            ROS_INFO_STREAM(std::to_string(i ) + " Queue Size: " + std::to_string(queue.size()));
         }
     }
 }
@@ -133,14 +180,23 @@ void ConeConnection::updateMap(const sensor_msgs::PointCloud2ConstPtr &cloud_msg
 
     // Initialize occupancy grid
     nav_msgs::OccupancyGrid occupancyGrid;
-    occupancyGrid.info.height = 350;
-    occupancyGrid.info.width = 350;
-
     // info.resolution scales grid size to the provided meters
     occupancyGrid.info.resolution = 0.25;
+    occupancyGrid.info.height = 350;
+    occupancyGrid.info.width = 350;
+    geometry_msgs::Pose origin;
+    double car_x = -.5 * occupancyGrid.info.height * occupancyGrid.info.resolution;  // m / cell
+    double car_y = -.5 * occupancyGrid.info.width * occupancyGrid.info.resolution;
+
+    origin.position.x = car_x;
+    origin.position.y = car_y;
+
+    occupancyGrid.info.origin = origin;
+    occupancyGrid.header.frame_id = "base_footprint";
+
     std::vector<signed char> grid(occupancyGrid.info.height * occupancyGrid.info.width);
     occupancyGrid.data = grid;
-    clustering(cloud, occupancyGrid, static_cast<int>(distance_between_cones * 1.5));
+    clustering(cloud, occupancyGrid, static_cast<int>(distance_between_cones * 1.15));
 
     std_msgs::String msg;
     msg.data = "Publishing grid";
