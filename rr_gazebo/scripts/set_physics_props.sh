e3f484145b5920480c1be9bac3becd13897d7ea9
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 18:53:27 +0800

    Delete rr_gazebo directory

--- rr_gazebo/scripts/set_physics_props.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# set gazebo physics step size so that it uses less CPU resources
-
-sleep 5
-gz physics --step-size 0.0025 --update-rate 400 --iters 30
--- rr_gazebo/scripts/spawn_macaroni_platform.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sleep 3
-
-roslaunch rr_gazebo spawn_macaroni_platform.launch x:=$1 y:=$2 heading:=$3
