0b76d5dbcb859908bc32d221bfcf26a50286641d
Ishan Desai <idesai33@gatech.edu>
2022-01-24 00:01:44 +0000

    fix format

--- rr_rviz_plugins/pid_plugins/pid_node.cpp
+++ rr_rviz_plugins/pid_plugins/pid_node.cpp
@@ -1,11 +1,7 @@
-#include <pluginlib/class_list_macros.hpp>
 #include "pid_node.hpp"
 
-void PidNode::processMessage(rr_msgs::msg::Pid) {
-    
-}
+#include <pluginlib/class_list_macros.hpp>
 
-
-
+void PidNode::processMessage(rr_msgs::msg::Pid) {}
 
 PLUGINLIB_EXPORT_CLASS(myplugin::Pid, rviz_common::Display)
--- rr_rviz_plugins/pid_plugins/pid_node.hpp
+++ rr_rviz_plugins/pid_plugins/pid_node.hpp
@@ -2,18 +2,15 @@
 #define PIDNODE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
-#include <rviz_common/display.hpp>
 #include <rr_msgs/msg/pid.hpp>
+#include <rviz_common/display.hpp>
 
-namespace rr_rviz_plugins
-{
-    class PidNode : public rviz_common::RosTopicDisplay<rr_msgs::msg::Pid>
-    {
-
-        public:
-            PidNode(rclcpp::Node & node);
-            override void processMessage(rr_msgs::msg::Pid);
-    };
-}
+namespace rr_rviz_plugins {
+class PidNode : public rviz_common::RosTopicDisplay<rr_msgs::msg::Pid> {
+  public:
+    PidNode(rclcpp::Node& node);
+    override void processMessage(rr_msgs::msg::Pid);
+};
+}  // namespace rr_rviz_plugins
 
 #endif
