f0b9e3833903f7a6e8cb9588b93795e142bc44fe
Cody <codynelson03@gmail.com>
2022-01-30 17:22:07 -0500

    changed conversion method

--- rr_util/src/ground_segmentation/ground_segmentation.cpp
+++ rr_util/src/ground_segmentation/ground_segmentation.cpp
@@ -6,19 +6,17 @@
 GroundSegmentation::GroundSegmentation() : Node("ground_segmentation") {
     pcl_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("/ground_segmentation", rclcpp::SystemDefaultsQoS());
     velodyne_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
-          "/velodyne_points", rclcpp::SensorDataQoS(),
+          "/velodyne_points2", rclcpp::SensorDataQoS(),
           std::bind(&GroundSegmentation::PointCloudCallback, this, std::placeholders::_1));
 }
 
 void GroundSegmentation::PointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-    pcl::PCLPointCloud2 pcl_cloud = pcl::PCLPointCloud2();
+    pcl::PointCloud<pcl::_PointXYZI> pcl_cloud{};
+    pcl::fromROSMsg(*msg, pcl_cloud);
 
-    pcl_conversions::moveToPCL(*msg, pcl_cloud);
 
-    sensor_msgs::msg::PointCloud2 result = sensor_msgs::msg::PointCloud2();
-
-    pcl_conversions::moveFromPCL(pcl_cloud, result);
-
+    sensor_msgs::msg::PointCloud2 result{};
+    pcl::toROSMsg(pcl_cloud, result);
     pcl_pub_->publish(result);
 }
 
