44cebc2cda08ced39053bbfc4e13d73a24487906
Cody <codynelson03@gmail.com>
2022-01-30 16:59:19 -0500

    copies to pcl point cloud

--- rr_util/src/ground_segmentation/ground_segmentation.cpp
+++ rr_util/src/ground_segmentation/ground_segmentation.cpp
@@ -1,7 +1,9 @@
 #include "ground_segmentation.hpp"
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
 
 GroundSegmentation::GroundSegmentation() : Node("ground_segmentation") {
-    pcl_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("/ground_segmentation", rclcpp::SensorDataQoS());
+    pcl_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("/ground_segmentation", rclcpp::SystemDefaultsQoS());
     velodyne_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
         "/velodyne_points", rclcpp::SensorDataQoS(),
         std::bind(&GroundSegmentation::PointCloudCallback, this, std::placeholders::_1)
@@ -9,7 +11,15 @@ GroundSegmentation::GroundSegmentation() : Node("ground_segmentation") {
 }
 
 void GroundSegmentation::PointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-    pcl_pub_->publish(*msg);
+    pcl::PCLPointCloud2 pcl_cloud = pcl::PCLPointCloud2();
+
+    pcl_conversions::moveToPCL(*msg, pcl_cloud);
+
+    sensor_msgs::msg::PointCloud2 result = sensor_msgs::msg::PointCloud2();
+
+    pcl_conversions::moveFromPCL(pcl_cloud, result);
+
+    pcl_pub_->publish(result);
 }
 
 int main(int argc, char** argv) {
