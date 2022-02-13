177d1662a1c3df2ca60f115a9b94277a7448e0cb
NatashaSetiadi <natset456@gmail.com>
2022-02-13 23:02:10 +0000

    fix format

--- rr_rviz_plugins/src/SteeringDirection.cpp
+++ rr_rviz_plugins/src/SteeringDirection.cpp
@@ -1,18 +1,20 @@
+#include "SteeringDirection.h"
+
+#include <QVBoxLayout>
 #include <pluginlib/class_list_macros.hpp>
 #include <rviz_common/panel.hpp>
-#include "SteeringDirection.h"
-#include <QVBoxLayout>
+
 #include "rr_msgs/msg/steering.hpp"
 
 namespace rr_rviz_plugins {
-    QLabel *label;
-    rclcpp::Publisher<rr_msgs::msg::Steering>::SharedPtr steering_pub_;
-    rclcpp::Subscription<rr_msgs::msg::Steering>::SharedPtr steering_sub_;
-    std::shared_ptr<rclcpp::Node> node;
-    SteeringDirection::SteeringDirection(QWidget * parent) : rviz_common::Panel(parent){
-        node = std::make_shared<rclcpp::Node>("SteeringDirection");
-        float curr_direction;
-        steering_pub_ = node->create_publisher<rr_msgs::msg::Steering>("/steering", rclcpp::SystemDefaultsQoS());
+QLabel *label;
+rclcpp::Publisher<rr_msgs::msg::Steering>::SharedPtr steering_pub_;
+rclcpp::Subscription<rr_msgs::msg::Steering>::SharedPtr steering_sub_;
+std::shared_ptr<rclcpp::Node> node;
+SteeringDirection::SteeringDirection(QWidget *parent) : rviz_common::Panel(parent) {
+    node = std::make_shared<rclcpp::Node>("SteeringDirection");
+    float curr_direction;
+    steering_pub_ = node->create_publisher<rr_msgs::msg::Steering>("/steering", rclcpp::SystemDefaultsQoS());
         steering_sub_ = node->create_subscription<rr_msgs::msg::Steering>(
               "/steering", rclcpp::SystemDefaultsQoS(),
               std::bind(&SteeringDirection::direction_callback, this, std::placeholders::_1);
@@ -21,16 +23,14 @@ namespace rr_rviz_plugins {
         setLayout(layout);
         rclcpp::spin(node);
         rclcpp::shutdown();
-    }
-    
-    void SteeringDirection::direction_callback(const rr_msgs::msg::Steering &msg) {
-        auto text = std::to_string(msg->Steering) + " m/s";
-        // Set the contents of the label.
-        label->setText(text.c_str());
-    }
-    void displayGUI(){
-
-    }
 }
+
+void SteeringDirection::direction_callback(const rr_msgs::msg::Steering &msg) {
+    auto text = std::to_string(msg->Steering) + " m/s";
+    // Set the contents of the label.
+    label->setText(text.c_str());
+}
+void displayGUI() {}
+}  // namespace rr_rviz_plugins
 
 PLUGINLIB_EXPORT_CLASS(rr_rviz_plugins::SteeringDirection, rviz_common::Panel)
--- rr_rviz_plugins/src/SteeringDirection.h
+++ rr_rviz_plugins/src/SteeringDirection.h
@@ -1,23 +1,22 @@
 #ifndef RR_RVIZ_PLUGIN_STEERINGDIRECTION_H
 #define RR_RVIZ_PLUGIN_STEERINGDIRECTION_H
 
-#include <rclcpp/rclcpp.hpp>
-#include <pluginlib/class_list_macros.hpp>
 #include <QPushButton>
-#include <rviz_common/panel.hpp>
 #include <QVBoxLayout>
 #include <QtGui/QPaintEvent>
 #include <QtWidgets/QLabel>
+#include <pluginlib/class_list_macros.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rviz_common/panel.hpp>
 
 namespace rr_rviz_plugins {
 
 class SteeringDirection : public rviz_common::Panel {
-  Q_OBJECT
+    Q_OBJECT
   public:
     explicit SteeringDirection(QWidget *parent = nullptr);
-
 };
 
-}
+}  // namespace rr_rviz_plugins
 
 #endif
