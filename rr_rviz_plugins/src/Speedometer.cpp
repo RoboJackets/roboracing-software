6641a512497202d363b243ba36b32af238d8a396
sophiazlin <sophialin00000@gmail.com>
2022-02-15 02:32:18 +0000

    fix format

--- rr_rviz_plugins/src/Speedometer.cpp
+++ rr_rviz_plugins/src/Speedometer.cpp
@@ -6,20 +6,20 @@ namespace rr_rviz_plugins {
 
 rclcpp::Subscription<rr_msgs::msg::Speed>::SharedPtr speed_subscriber;
 float current_speed;
-QLabel* label;
+QLabel *label;
 rclcpp::Publisher<rr_msgs::msg::Speed>::SharedPtr speed_pub_;
 rclcpp::Subscription<rr_msgs::msg::Speed>::SharedPtr speed_sub_;
 std::shared_ptr<rclcpp::Node> node;
 
-Speedometer::Speedometer(QWidget *parent) : rviz_common::Panel(parent)  // Base class constructor
+Speedometer::Speedometer(QWidget *parent)
+      : rviz_common::Panel(parent)  // Base class constructor
 {
     // Initialize a label for displaying some data
     label = new QLabel("0 m/s");
     node = std::make_shared<rclcpp::Node>("Speed");
     speed_pub_ = node->create_publisher<rr_msgs::msg::Speed>("/speed", rclcpp::SystemDefaultsQoS());
     speed_sub_ = node->create_subscription<rr_msgs::msg::Speed>(
-                "/speed", rclcpp::SystemDefaultsQoS(),
-                std::bind(&Speedometer::speedCallback, this, std::placeholders::_1));
+          "/speed", rclcpp::SystemDefaultsQoS(), std::bind(&Speedometer::speedCallback, this, std::placeholders::_1));
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(label);
@@ -37,12 +37,12 @@ void Speedometer::speedCallback(const rr_msgs::msg::Speed::SharedPtr msg) {
 }
 
 // create image of speedometer
-void Speedometer::paintEvent(QPaintEvent *event)  {
-    //draw a speedometer
+void Speedometer::paintEvent(QPaintEvent *event) {
+    // draw a speedometer
     float max_speed = 40;
     float adjusted_speed = std::min(max_speed, current_speed);
-    float angle = M_PI * (1.0 - current_speed / max_speed); // in radians
+    float angle = M_PI * (1.0 - current_speed / max_speed);  // in radians
 }
-}
+}  // namespace rr_rviz_plugins
 
 PLUGINLIB_EXPORT_CLASS(rr_rviz_plugins::Speedometer, rviz_common::Panel)
