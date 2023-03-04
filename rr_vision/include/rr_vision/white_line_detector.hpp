c38475c8882576877ed9441915ea496e3b810e5e
idale0001 <idale0001@gmail.com>
2023-03-04 20:07:26 +0000

    fix format

--- rr_vision/include/rr_vision/white_line_detector.hpp
+++ rr_vision/include/rr_vision/white_line_detector.hpp
@@ -1,37 +1,36 @@
 #ifndef WHITE_LINE_DETECTOR_HPP_
 #define WHITE_LINE_DETECTOR_HPP_
 
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/opencv.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
-
 #include <rr_msgs/msg/speed.hpp>
 #include <rr_msgs/msg/steering.hpp>
+#include <sensor_msgs/msg/image.hpp>
 
-namespace white_line{
+namespace white_line {
 
-class WhitePixelNode : public rclcpp::Node
-{
-public:
-  WhitePixelNode(const rclcpp::NodeOptions & options);
-  
-private:
-  void process_image(const sensor_msgs::msg::Image::SharedPtr msg);
-  
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
-  /**
-  * @brief Publishes to /speed
-    */
-  rclcpp::Publisher<rr_msgs::msg::Speed>::SharedPtr speed_publisher;
-  /**
-  * @brief Publishes to /steering
-    */
-  rclcpp::Publisher<rr_msgs::msg::Steering>::SharedPtr steering_publisher;
-  int min_white;
+class WhitePixelNode : public rclcpp::Node {
+  public:
+    WhitePixelNode(const rclcpp::NodeOptions& options);
+
+  private:
+    void process_image(const sensor_msgs::msg::Image::SharedPtr msg);
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+    /**
+     * @brief Publishes to /speed
+     */
+    rclcpp::Publisher<rr_msgs::msg::Speed>::SharedPtr speed_publisher;
+    /**
+     * @brief Publishes to /steering
+     */
+    rclcpp::Publisher<rr_msgs::msg::Steering>::SharedPtr steering_publisher;
+    int min_white;
 };
 
-}
+}  // namespace white_line
 #endif  // WHITE_LINE_DETECTOR_HPP_
